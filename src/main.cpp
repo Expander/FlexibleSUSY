a96e015341600867e02da0d7040d74a272072145
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2012-10-22 13:28:53 +0200

    initial commit: Starting project `FlexibleSUSY', hosted ad http://flexiblesusy.hepforge.org/

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+// ====================================================================
+//
+// File:        main.cpp
+// Author:      Alexander Voigt
+// Description: main() routine
+//
+// This file is part of FlexibleSUSY.
+//
+// FlexibleSUSY is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// FlexibleSUSY is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with FlexibleSUSY.  If not, see
+// <http://www.gnu.org/licenses/>.
+// ====================================================================
+
+int main()
+{
+   return 0;
+}
