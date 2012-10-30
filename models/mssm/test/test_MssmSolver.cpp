50e2378274c86399689cfd0dbd9d522df18eb621
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2012-10-30 13:49:09 +0100

    add test case: test that the output of MssmSolver::lowOrg and MssmSoftsusy::lowOrg are the same

---
+++ models/mssm/test/test_MssmSolver.cpp
@@ -0,0 +1,42 @@
+#include "mssm_solver.h"
+#include "softsusy.h"
+
+#define BOOST_TEST_MODULE test_MssmSolver
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( test_MssmSolver )
+{
+   DoubleVector highScaleSoftPars(3);
+   const double m12 = 500., a0 = 0., m0 = 125.;
+   highScaleSoftPars(1) = m0;
+   highScaleSoftPars(2) = m12;
+   highScaleSoftPars(3) = a0;
+
+   const int signMu = 1;
+   const double tanBeta = 10.0;
+   const bool uni = true;
+   const double mxGuess = 1.0e16;
+
+   QedQcd oneset;
+   const double alphasMZ = 0.1187, mtop = 173.4, mbmb = 4.2;
+   oneset.setAlpha(ALPHAS, alphasMZ);
+   oneset.setPoleMt(mtop);
+   oneset.setMass(mBottom, mbmb);
+   oneset.toMz();
+
+   MssmSolver mssmSolver;
+   mssmSolver.setSoftHighScalePars(highScaleSoftPars);
+   mssmSolver.setSignMu(signMu);
+   mssmSolver.setTanBeta(tanBeta);
+   mssmSolver.setLowScaleBoundaryConditions(oneset);
+   mssmSolver.setGaugeUnification(uni);
+   mssmSolver.setMxGuess(mxGuess);
+   mssmSolver.setHighScaleBoundaryCondition(sugraBcs);
+   mssmSolver.lowOrg();
+
+   MssmSoftsusy softSusy;
+   softSusy.lowOrg(sugraBcs, mxGuess, highScaleSoftPars, signMu, tanBeta, oneset, uni);
+
+   BOOST_CHECK_EQUAL(mssmSolver.displayPhys(), softSusy.displayPhys());
+}
