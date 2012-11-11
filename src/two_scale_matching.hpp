f1a63f89f1b56c18756b8f7971b1e79041098477
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2012-11-11 16:53:26 +0100

    add virtual destructor and include general template

--- src/two_scale_matching.hpp
+++ src/two_scale_matching.hpp
@@ -2,12 +2,15 @@
 #ifndef TWO_SCALE_MATCHING_H
 #define TWO_SCALE_MATCHING_H
 
+#include "matching.hpp"
+
 class DoubleVector;
 class Two_scale;
 
 template<>
 class Matching<Two_scale> {
 public:
+   virtual ~Matching() {}
    virtual DoubleVector calcHighFromLowScaleParameters(const DoubleVector&) const = 0;
    virtual DoubleVector calcLowFromHighScaleParameters(const DoubleVector&) const = 0;
 };
