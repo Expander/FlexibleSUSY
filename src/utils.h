6b6908e0291d3fa7d7c1deb3e63bc23f974c78c8
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2012-12-02 13:39:44 +0100

    Remove unused POSIX headers. Now FlexibleSUSY compiles under MinGW.

--- src/utils.h
+++ src/utils.h
@@ -23,10 +23,6 @@
 #include <exception>
 /// Error handling for softsusy
 #include <csignal>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
