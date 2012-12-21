72f96f551e2a79f28c007da89d6dfc93ab1216d0
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2012-12-21 10:56:44 +0100

    extend test: now we test that the returned precision is not smaller than the specified minimum

--- test/test_running_precision.cpp
+++ test/test_running_precision.cpp
@@ -26,12 +26,15 @@ BOOST_AUTO_TEST_CASE( test_increasing_running_precision )
 {
    for (int i = 1; i < 10; ++i) {
       const double increasing_factor = static_cast<double>(i);
-      Two_scale_increasing_precision incr_prec(increasing_factor, 1.0e-12);
+      const double min_precision = 1.0e-12;
+      Two_scale_increasing_precision incr_prec(increasing_factor, min_precision);
 
       // test that all iterations yield expected precision
-      for (int iteration = 0; iteration < 10; ++iteration) {
+      for (int iteration = 0; iteration < 20; ++iteration) {
          double precision = incr_prec.get_precision(iteration);
          double expected_precision = std::pow(1./increasing_factor, iteration + 1);
+         if (expected_precision < min_precision)
+            expected_precision = min_precision;
          BOOST_CHECK_CLOSE(precision, expected_precision, 1.0e-5);
       }
    }
