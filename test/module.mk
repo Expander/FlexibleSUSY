2d417bbe1f7243a00fcb3474590c699adf536490
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2012-12-29 22:39:53 +0100

    Split boost and fortran libraries to remove linking of unused libraries

--- test/module.mk
+++ test/module.mk
@@ -42,25 +42,25 @@ clean::         clean-$(MODNAME)
 distclean::     distclean-$(MODNAME)
 
 $(DIR)/test_logger.x: $(DIR)/test_logger.o $(LIBFLEXI)
-		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(LIBS)
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(BOOSTLIBS)
 
 $(DIR)/test_mssm_solver.x: $(DIR)/test_mssm_solver.o $(LIBMSSM) $(LIBFLEXI)
-		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(LIBS)
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(FLIBS) $(BOOSTLIBS)
 
 $(DIR)/test_running_precision.x: $(DIR)/test_running_precision.o $(LIBFLEXI)
-		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(LIBS)
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(BOOSTLIBS)
 
 $(DIR)/test_sm_smcw_two_scale_integration.x: $(DIR)/test_sm_smcw_two_scale_integration.o $(LIBSMCW) $(LIBSM) $(LIBFLEXI)
-		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(LIBS)
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(BOOSTLIBS)
 
 $(DIR)/test_sm_two_scale.x: $(DIR)/test_sm_two_scale.o $(LIBSM) $(LIBFLEXI)
-		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(LIBS)
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(BOOSTLIBS)
 
 $(DIR)/test_two_scale_solver.x: $(DIR)/test_two_scale_solver.o $(LIBFLEXI)
-		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(LIBS)
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(BOOSTLIBS)
 
 %.x: %.o $(ALLLIB)
-		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(LIBS)
+		$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^ $(BOOSTLIBS)
 
 ALLDEP += $(TEST_DEP)
 ALLTST += $(TEST_EXE)
