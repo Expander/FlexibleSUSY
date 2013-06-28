08bb4e52bbf90f69c508dcab065612f9b05a7548
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-06-28 09:19:48 +0200

    Simplify the two-loop results and evaluate the Zeta functions

--- meta/TwoLoop.m
+++ meta/TwoLoop.m
@@ -13,23 +13,25 @@ GetDeltaMQCD[quark_, renScale_] :=
           ];
 
 GetDeltaMQCD[quark:SARAH`TopQuark, renScale_] :=
-    Module[{CF, CA, colorPosition, alphaStrong, mf, log},
+    Module[{CF, CA, colorPosition, alphaStrong, mf, log, result},
            colorPosition = Position[SARAH`Gauge, SARAH`color][[1,1]];
            CF = SA`Casimir[quark, colorPosition];
            CA = SA`Casimir[SARAH`VectorG, colorPosition];
            alphaStrong = SARAH`strongCoupling^2 / (4 Pi);
            mf = FlexibleSUSY`M[quark];
            log = Log[(mf^2) / (renScale^2)];
-           mf CF (alphaStrong / (4 Pi))^2 (-43 - 12 Zeta[2] + 26 log - 6 log^2
-                                           + CF (-59/8 + 30 Zeta[2] - 48 Log[2] Zeta[2]
-                                                 + 12 Zeta[3] + (3/2) log + (9/2) log^2)
-                                           + CA (1093/24 - 8 Zeta[2] + 24 Log[2] Zeta[2]
-                                                 - 6 Zeta[3] - (179/6) log + 11/2 log^2)
-                                          )
+           result = mf CF (alphaStrong / (4 Pi))^2 (
+               -43 - 12 Zeta[2] + 26 log - 6 log^2
+               + CF (-59/8 + 30 Zeta[2] - 48 Log[2] Zeta[2]
+                     + 12 Zeta[3] + (3/2) log + (9/2) log^2)
+               + CA (1093/24 - 8 Zeta[2] + 24 Log[2] Zeta[2]
+                     - 6 Zeta[3] - (179/6) log + 11/2 log^2)
+                                                   );
+           Simplify[result /. Zeta[a_] :> N[Zeta[a]]]
           ];
 
 GetDeltaMQCD[quark:SARAH`BottomQuark, renScale_] :=
-    Module[{CF, CA, colorPosition, alphaStrong, mf, mt, log, logMt},
+    Module[{CF, CA, colorPosition, alphaStrong, mf, mt, log, logMt, result},
            colorPosition = Position[SARAH`Gauge, SARAH`color][[1,1]];
            CF = SA`Casimir[quark, colorPosition];
            CA = SA`Casimir[SARAH`VectorG, colorPosition];
@@ -38,13 +40,15 @@ GetDeltaMQCD[quark:SARAH`BottomQuark, renScale_] :=
            mt = FlexibleSUSY`M[SARAH`TopQuark];
            log = Log[(mf^2) / (mt^2)];
            logMt = Log[(mf^2) / (^2)]
-           mf CF (alphaStrong / (4 Pi))^2 (-623/18 - 8 Zeta[2] + 26 log - 6 log^2
-                                           + CF (-59/8 + 30 Zeta[2] - 48 Log[2] Zeta[2]
-                                                 + 12 Zeta[3] + (3/2) log + (9/2) log^2)
-                                           + CA (1093/24 - 8 Zeta[2] + 24 Log[2] Zeta[2]
-                                                 - 6 Zeta[3] - (179/6) log + 11/2 log^2)
-                                           - 13/3 logMt + logMt^2
-                                          )
+           result = mf CF (alphaStrong / (4 Pi))^2 (
+               -623/18 - 8 Zeta[2] + 26 log - 6 log^2
+               + CF (-59/8 + 30 Zeta[2] - 48 Log[2] Zeta[2]
+                     + 12 Zeta[3] + (3/2) log + (9/2) log^2)
+               + CA (1093/24 - 8 Zeta[2] + 24 Log[2] Zeta[2]
+                     - 6 Zeta[3] - (179/6) log + 11/2 log^2)
+               - 13/3 logMt + logMt^2
+                                                   )
+           Simplify[result /. Zeta[a_] :> N[Zeta[a]]]
           ];
 
 End[];
