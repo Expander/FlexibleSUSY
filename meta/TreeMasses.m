5a60b260617cbe309a62683020c755af8da099c0
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-06-28 18:56:16 +0200

    Temporarily print a warning only if we have a tachyon.

--- meta/TreeMasses.m
+++ meta/TreeMasses.m
@@ -421,7 +421,8 @@ CreateDiagonalizationFunction[matrix_List, eigenVector_, mixingMatrixSymbol_] :=
               (* check for tachyons *)
               body = body <> "\nint min_element;\n" <>
                      "if (" <> ev <> ".min(min_element) < 0.)\n" <>
-                     IndentText["throw TachyonError(this, \"" <> ev <> "\", min_element);\n\n"];
+                     IndentText["WARNING(\"Particle is tachyonic: " <> ev <> "(\" << min_element << \")\");\n\n"];
+                     (* IndentText["throw TachyonError(this, \"" <> ev <> "\", min_element);\n\n"]; *)
               body = body <> ev <> " = " <> ev <> ".apply(ZeroSqrt);\n";
              ];
            Return[result <> IndentText[body] <> "}\n"];
