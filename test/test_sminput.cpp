d029db21fbb839efb77cb92483d525279c2b2001
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-08-07 16:21:26 +0200

    add test case which demonstates (and tests) iteration

--- test/test_sminput.cpp
+++ test/test_sminput.cpp
@@ -48,3 +48,75 @@ BOOST_AUTO_TEST_CASE( test_reading_sminput )
    BOOST_CHECK_EQUAL(m_s     , 1.040000000e-01);
    BOOST_CHECK_EQUAL(m_c     , 1.270000000e+00);
 }
+
+BOOST_AUTO_TEST_CASE( test_iteration_sminput )
+{
+   std::ifstream ifs("test/sminput.slha2");
+   const SLHAea::Coll input(ifs);
+
+   BOOST_REQUIRE(!input.empty());
+
+   double alpha_em = 0.0;
+   double G_mu     = 0.0;
+   double alpha_s  = 0.0;
+   double m_Z      = 0.0;
+   double m_b      = 0.0;
+   double m_t      = 0.0;
+   double m_tau    = 0.0;
+   double m_nu3    = 0.0;
+   double m_e      = 0.0;
+   double m_nu1    = 0.0;
+   double m_mu     = 0.0;
+   double m_nu2    = 0.0;
+   double m_d      = 0.0;
+   double m_u      = 0.0;
+   double m_s      = 0.0;
+   double m_c      = 0.0;
+
+   for (SLHAea::Block::const_iterator line = input.at("SMINPUTS").cbegin();
+        line != input.at("SMINPUTS").cend(); ++line) {
+      if (!line->is_data_line()) continue;
+
+      if (line->size() >= 2) {
+         const int key = SLHAea::to<int>((*line)[0]);
+         switch (key) {
+         case 1:  alpha_em = SLHAea::to<double>((*line)[1]); break;
+         case 2:  G_mu     = SLHAea::to<double>((*line)[1]); break;
+         case 3:  alpha_s  = SLHAea::to<double>((*line)[1]); break;
+         case 4:  m_Z      = SLHAea::to<double>((*line)[1]); break;
+         case 5:  m_b      = SLHAea::to<double>((*line)[1]); break;
+         case 6:  m_t      = SLHAea::to<double>((*line)[1]); break;
+         case 7:  m_tau    = SLHAea::to<double>((*line)[1]); break;
+         case 8:  m_nu3    = SLHAea::to<double>((*line)[1]); break;
+         case 11: m_e      = SLHAea::to<double>((*line)[1]); break;
+         case 12: m_nu1    = SLHAea::to<double>((*line)[1]); break;
+         case 13: m_mu     = SLHAea::to<double>((*line)[1]); break;
+         case 14: m_nu2    = SLHAea::to<double>((*line)[1]); break;
+         case 21: m_d      = SLHAea::to<double>((*line)[1]); break;
+         case 22: m_u      = SLHAea::to<double>((*line)[1]); break;
+         case 23: m_s      = SLHAea::to<double>((*line)[1]); break;
+         case 24: m_c      = SLHAea::to<double>((*line)[1]); break;
+         default: BOOST_CHECK(false); break;
+         }
+      } else {
+         BOOST_CHECK(false);
+      }
+   }
+
+   BOOST_CHECK_EQUAL(alpha_em, 1.279340000e+02);
+   BOOST_CHECK_EQUAL(G_mu    , 1.166370000e-05);
+   BOOST_CHECK_EQUAL(alpha_s , 1.176000000e-01);
+   BOOST_CHECK_EQUAL(m_Z     , 9.118760000e+01);
+   BOOST_CHECK_EQUAL(m_b     , 4.200000000e+00);
+   BOOST_CHECK_EQUAL(m_t     , 1.733000000e+02);
+   BOOST_CHECK_EQUAL(m_tau   , 1.777000000e+00);
+   BOOST_CHECK_EQUAL(m_nu3   , 0.000000000e+00);
+   BOOST_CHECK_EQUAL(m_e     , 5.109989020e-04);
+   BOOST_CHECK_EQUAL(m_nu1   , 0.000000000e+00);
+   BOOST_CHECK_EQUAL(m_mu    , 1.056583570e-01);
+   BOOST_CHECK_EQUAL(m_nu2   , 0.000000000e+00);
+   BOOST_CHECK_EQUAL(m_d     , 4.750000000e-03);
+   BOOST_CHECK_EQUAL(m_u     , 2.400000000e-03);
+   BOOST_CHECK_EQUAL(m_s     , 1.040000000e-01);
+   BOOST_CHECK_EQUAL(m_c     , 1.270000000e+00);
+}
