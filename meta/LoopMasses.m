0755a85099e6c5233cb45de7e624e459de2aa0c0
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-08-10 16:05:23 +0200

    set pole mass to zero in case of a tachyon

--- meta/LoopMasses.m
+++ meta/LoopMasses.m
@@ -55,7 +55,7 @@ Do1DimScalar[particleName_String, massName_String, selfEnergyFunction_String,
     If[tadpole == "", "", " + " <> tadpole] <> ";\n\n" <>
     "if (mass_sqr < 0.)\n" <>
     IndentText["problems.flag_tachyon(" <> particleName <> ");"] <> "\n\n" <>
-    "PHYSICAL(" <> massName <> ") = AbsSqrt(mass_sqr);\n";
+    "PHYSICAL(" <> massName <> ") = ZeroSqrt(mass_sqr);\n";
 
 Do1DimFermion[massName_String, selfEnergyFunctionS_String,
               selfEnergyFunctionPL_String, selfEnergyFunctionPR_String, momentum_String] :=
@@ -73,7 +73,7 @@ Do1DimVector[particleName_String, massName_String, selfEnergyFunction_String,
     "const double mass_sqr = Sqr(" <> massName <> ") - self_energy;\n\n" <>
     "if (mass_sqr < 0.)\n" <>
     IndentText["problems.flag_tachyon(" <> particleName <> ");"] <> "\n\n" <>
-    "PHYSICAL(" <> massName <> ") = AbsSqrt(mass_sqr);\n";
+    "PHYSICAL(" <> massName <> ") = ZeroSqrt(mass_sqr);\n";
 
 
 (* ********** fast diagonalization routines ********** *)
@@ -121,7 +121,7 @@ DoFastDiagonalization[particle_Symbol /; IsScalar[particle], tadpoles_List] :=
                        "\nint min_element;\n" <>
                        "if (" <> massName <> ".min(min_element) < 0.)\n" <>
                        IndentText["problems.flag_tachyon(" <> particleName <> ");"] <> "\n\n" <>
-                       "PHYSICAL(" <> massName <> ") = AbsSqrt(PHYSICAL(" <>
+                       "PHYSICAL(" <> massName <> ") = ZeroSqrt(PHYSICAL(" <>
                        massName <> "));\n";
               ,
               result = Do1DimScalar[particleName, massName, selfEnergyFunction, massName,
@@ -252,7 +252,7 @@ DoMediumDiagonalization[particle_Symbol /; IsScalar[particle], inputMomentum_, t
                             diagonalizationFunction <> "(M_1loop, " <> Utemp <> ", " <> Vtemp <> ", eigen_values);\n\n" <>
                             "if (eigen_values(es) < 0.)\n" <>
                             IndentText["problems.flag_tachyon(" <> particleName <> ");"] <> "\n\n" <>
-                            "PHYSICAL(" <> massName <> "(es)) = AbsSqrt(eigen_values(es));\n" <>
+                            "PHYSICAL(" <> massName <> "(es)) = ZeroSqrt(eigen_values(es));\n" <>
                             "if (es == 1) {\n" <>
                             IndentText["PHYSICAL(" <> U <> ") = " <> Utemp <> ";\n" <>
                                        "PHYSICAL(" <> V <> ") = " <> Vtemp <> ";\n"] <>
@@ -264,7 +264,7 @@ DoMediumDiagonalization[particle_Symbol /; IsScalar[particle], inputMomentum_, t
                             diagonalizationFunction <> "(M_1loop, " <> Utemp <> ", eigen_values);\n\n" <>
                             "if (eigen_values(es) < 0.)\n" <>
                             IndentText["problems.flag_tachyon(" <> particleName <> ");"] <> "\n\n" <>
-                            "PHYSICAL(" <> massName <> "(es)) = AbsSqrt(eigen_values(es));\n" <>
+                            "PHYSICAL(" <> massName <> "(es)) = ZeroSqrt(eigen_values(es));\n" <>
                             "if (es == 1)\n" <>
                             IndentText["PHYSICAL(" <> U <> ") = " <> Utemp <> ";\n"];
              ];
@@ -656,7 +656,7 @@ CreateRunningDRbarMassFunction[particle_] :=
               body = "const double p = m_pole;\n" <>
               "const double self_energy = Re(" <> selfEnergyFunction <> "(p));\n" <>
               "const double mass_sqr = Sqr(m_pole) + self_energy;\n\n" <>
-              "return AbsSqrt(mass_sqr);\n";
+              "return ZeroSqrt(mass_sqr);\n";
              ];
            Return[result <> IndentText[body] <> "}\n\n"];
           ];
