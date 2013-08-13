d6f7b10f02c8afa2a9128ec0420c77638d26ff17
Jae-hyeon Park <jhyeon@gmail.com>
2013-08-14 01:02:17 +0200

    Suppress warnings about function hiding
    See http://www.parashift.com/c++-faq-lite/hiding-rule.html

--- src/lattice_compound_constraint.hpp
+++ src/lattice_compound_constraint.hpp
@@ -29,6 +29,8 @@ public:
     virtual void operator()() { for (auto c: components) (*c)(); }
     virtual void relocate(const std::vector<size_t>& site_map)
     { for (auto c: components) c->relocate(site_map); }
+    using Constraint<Lattice>::init;
+    using Constraint<Lattice>::relocate;
 protected:
     virtual void activate() {}
     std::vector<Constraint<Lattice>*> components;
@@ -51,6 +53,7 @@ public:
     virtual void free_rows()
     { for (auto c: components) c->free_rows(); }
     virtual void operator()() { for (auto c: components) (*c)(); }
+    using Matching<Lattice>::init;
 protected:
     virtual void activate() {}
     std::vector<Matching<Lattice>*> components;
