0bab9771e735086faf04e0a7f42ba8d5e5107a5f
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-09-15 22:39:56 +0200

    add manual tex file with the rough structure layout

--- doc/module.mk
+++ doc/module.mk
@@ -4,13 +4,22 @@ MODNAME      := doc
 DOC_OUTPUT_DIR  := $(DIR)/html
 INDEX_PADE      := $(DOC_OUTPUT_DIR)/index.html
 DOXYFILE        := $(DIR)/Doxyfile
+MANUAL_PDF      := $(DIR)/flexiblesusy.pdf
+MANUAL_SRC      := \
+		$(DIR)/flexiblesusy.tex \
+		$(DIR)/chapters/overview.tex \
+		$(DIR)/chapters/quick_start.tex \
+		$(DIR)/chapters/usage.tex \
+		$(DIR)/chapters/output.tex
 
 .PHONY:         all-$(MODNAME) clean-$(MODNAME) distclean-$(MODNAME) \
 		$(INDEX_PADE) doc
 
 doc: all-$(MODNAME)
 
-all-$(MODNAME): $(INDEX_PADE)
+doc-pdf: $(MANUAL_PDF)
+
+all-$(MODNAME): $(INDEX_PADE) $(MANUAL_PDF)
 
 clean-$(MODNAME):
 
@@ -28,3 +37,6 @@ $(INDEX_PADE):
 		  echo "EXCLUDE = $(ALLDEP) $(META_SRC) $(TEMPLATES) \
 		        $(TEST_SRC) $(TEST_META)" \
 		) | doxygen -
+
+$(MANUAL_PDF): $(MANUAL_SRC)
+		pdflatex -output-directory $(DIR) $<
