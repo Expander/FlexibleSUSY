8cb6d98ccf783f68d19060115878a04bb1165ce4
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-09-18 09:34:16 +0200

    add test which checks the calculation of MX

--- test/test_NMSSM_high_scale_constraint.cpp
+++ test/test_NMSSM_high_scale_constraint.cpp
@@ -13,6 +13,9 @@
 #include "nmssmsoftsusy.h"
 #include "NMSSM_two_scale_model.hpp"
 #include "NMSSM_two_scale_high_scale_constraint.hpp"
+#include "snmssm_two_scale.hpp"
+#include "snmssm_parameter_point.hpp"
+#include "snmssm_two_scale_sugra_constraint.hpp"
 
 using namespace flexiblesusy;
 using namespace softsusy;
@@ -64,4 +67,32 @@ BOOST_AUTO_TEST_CASE( test_unification_condition )
    TEST_CLOSE(m.get_TYe(), input.Azero * m.get_Ye(), 1.0e-6);
    BOOST_CHECK_CLOSE_FRACTION(m.get_TLambdax(), input.Azero * m.get_Lambdax(), 1.0e-10);
    BOOST_CHECK_CLOSE_FRACTION(m.get_TKappa()  , input.Azero * m.get_Kappa()  , 1.0e-10);
+}
+
+BOOST_AUTO_TEST_CASE( test_mx_calculation )
+{
+   NMSSM<Two_scale> m;
+   NmssmSoftsusy softSusy;
+   NMSSM_input_parameters input;
+   setup_NMSSM(m, softSusy, input);
+   SNmssm<Two_scale> s(softSusy);
+
+   SNmssm_parameter_point pp;
+   pp.tanBeta = input.TanBeta;
+   pp.a0 = input.Azero;
+   pp.m12 = input.m12;
+   pp.m0 = input.m0;
+   pp.lambda = input.LambdaInput;
+
+   NMSSM_high_scale_constraint<Two_scale> NMSSM_sugra_constraint(input);
+   SNmssm_sugra_constraint snmssm_sugra_constraint(pp);
+
+   NMSSM_sugra_constraint.set_model(&m);
+   snmssm_sugra_constraint.set_model((Two_scale_model*)&s);
+
+   NMSSM_sugra_constraint.update_scale();
+   snmssm_sugra_constraint.update_scale();
+
+   BOOST_CHECK_CLOSE_FRACTION(NMSSM_sugra_constraint.get_scale(),
+                              snmssm_sugra_constraint.get_scale(), 1.0e-13);
 }
