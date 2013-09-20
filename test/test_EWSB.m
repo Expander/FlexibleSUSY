27969197ee9c01cf381da505aa1aa86828889c6a
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-09-20 17:28:18 +0200

    do not write the equations in terms of lists

--- test/test_EWSB.m
+++ test/test_EWSB.m
@@ -4,8 +4,8 @@ Needs["EWSB`", "EWSB.m"];
 Print["testing MSSM-like EWSB for Mu and BMu ..."];
 
 mssmEwsbEqs = {
-    {mu^2 + x^2 + x y + z + 5},
-    {Bmu  - x^2 + x y + z + 5}
+    mu^2 + x^2 + x y + z + 5,
+    Bmu  - x^2 + x y + z + 5
 };
 
 mssmEwsbOutputParameters = { mu, Bmu };
@@ -43,9 +43,9 @@ muEff = lambda s;
 BmuEff = Alambda + kappa s;
 
 nmssmEwsbEqs = {
-    { vu muEff^2 - vd muEff BmuEff },
-    { vd muEff^2 - vu muEff BmuEff },
-    { s (mS2 + X + kappa s Akappa + kappa^2 s^2) + Y }
+    vu (mHu2 + muEff^2 + lambda^2 vd^2 + g^2 (vu^2 - vd^2)) - vd muEff BmuEff,
+    vd (mHd2 + muEff^2 + lambda^2 vu^2 + g^2 (vd^2 - vu^2)) - vu muEff BmuEff,
+    s (mS2 + X + kappa s Akappa + kappa^2 s^2) + Y
 };
 
 nmssmEwsbOutputParameters = { s, kappa, mS2 };
@@ -57,10 +57,6 @@ TestEquality[EWSB`FindFreePhasesInEWSB[nmssmEwsbEqs, nmssmEwsbOutputParameters],
 
 nmssmFullSolution = EWSB`Private`FindSolution[nmssmEwsbEqs, nmssmEwsbOutputParameters];
 
-TestEquality[nmssmFullSolution,
-             {}];
-
-TestEquality[Solve[(# == 0)& /@ nmssmEwsbEqs, nmssmEwsbOutputParameters],
-             {}];
+TestEquality[Length[nmssmFullSolution], 2];
 
 PrintTestSummary[];
