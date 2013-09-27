87c75fe221a46adcd0f9d435f2f0c31b0f8b3d78
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-09-27 10:27:37 +0200

    Rewrite the benchmark test to avoid exit code problems with boost

--- test/test_benchmark.cpp
+++ test/test_benchmark.cpp
@@ -1,39 +1,34 @@
 
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_IGNORE_NON_ZERO_CHILD_CODE
-#define BOOST_TEST_MODULE test_benchmark
-
-#include <boost/test/unit_test.hpp>
-
 #include <iostream>
 #include <fstream>
 #include <string>
 
+#include "test.h"
 #include "slhaea.h"
 #include "stopwatch.hpp"
+#include "logger.hpp"
 
 int run_cmd(const std::string& cmd)
 {
    if (!system(NULL)) {
-      BOOST_MESSAGE("Error: command processor not available!");
+      ERROR("Error: command processor not available!");
       return -1;
    }
 
    const int status = system(cmd.c_str());
 
    if (status) {
-      BOOST_MESSAGE("Command \"" << cmd << "\" returned with exit code "
-                    << status);
+      VERBOSE_MSG("Command \"" << cmd << "\" returned with exit code "
+                  << status);
    }
 
    return status;
 }
 
-int run_point(const std::string& slha_file)
+int run_point(const std::string& slha_file, double& fs_time, double& ss_time)
 {
    int status;
    flexiblesusy::Stopwatch stopwatch;
-   double fs_time = 0., ss_time = 0.;
 
    stopwatch.start();
    status = run_cmd("./models/MSSM/run_MSSM.x --slha-input-file=" +
@@ -42,24 +37,18 @@ int run_point(const std::string& slha_file)
    fs_time = stopwatch.get_time_in_seconds();
 
    if (status) {
-      BOOST_MESSAGE("FlexibleSUSY returned exit code " << status);
-      return status;
+      VERBOSE_MSG("FlexibleSUSY returned exit code " << status);
    }
 
    stopwatch.start();
    status = run_cmd("./examples/run_softpoint.x leshouches < " +
                     slha_file + " > out.spc");
    stopwatch.stop();
-
-   if (status) {
-      BOOST_MESSAGE("Softsusy returned exit code " << status);
-      return status;
-   }
    ss_time = stopwatch.get_time_in_seconds();
 
-   BOOST_MESSAGE("\ttime: SS = " << ss_time << "s, FS = " << fs_time << "s"
-                 << ", rel. diff. = "
-                 << (fs_time - ss_time) * 100. / ss_time << "%");
+   if (status) {
+      VERBOSE_MSG("Softsusy returned exit code " << status);
+   }
 
    return 0;
 }
@@ -84,12 +73,14 @@ SLHAea::Coll create_point(double tanBeta)
    return coll;
 }
 
-BOOST_AUTO_TEST_CASE( test_tanbeta_scan )
+void test_tanbeta_scan()
 {
    const unsigned num_points = 100;
    const double tanBeta_start = 2.;
    const double tanBeta_stop = 80.;
    const double tanBeta_step = (tanBeta_stop - tanBeta_start) / num_points;
+
+   double fs_time_sum = 0., ss_time_sum = 0.;
 
    for (unsigned i = 0; i < num_points; i++) {
       const double tanBeta = tanBeta_start + i * tanBeta_step;
@@ -100,7 +91,32 @@ BOOST_AUTO_TEST_CASE( test_tanbeta_scan )
       ofs << coll;
       ofs.close();
 
-      BOOST_MESSAGE(">>> running point tan(beta) = " << tanBeta);
-      run_point(input_file);
+      double fs_time = 0., ss_time = 0.;
+
+      INFO(">>> running point tan(beta) = " << tanBeta);
+      run_point(input_file, fs_time, ss_time);
+
+      INFO("\ttime: SS = " << ss_time << "s, FS = " << fs_time << "s"
+           << ", rel. diff. = "
+           << (fs_time - ss_time) * 100. / ss_time << "%");
+
+      fs_time_sum += fs_time;
+      ss_time_sum += ss_time;
    }
+
+   fs_time_sum /= num_points;
+   ss_time_sum /= num_points;
+
+   INFO("Summary: average times (in seconds) \n"
+        "  FlexibleSUSY: " << fs_time_sum << '\n' <<
+        "  Softsusy    : " << ss_time_sum);
+
+   TEST_GREATER(ss_time_sum, 2. * fs_time_sum);
+}
+
+int main()
+{
+   test_tanbeta_scan();
+
+   return gErrors;
 }
