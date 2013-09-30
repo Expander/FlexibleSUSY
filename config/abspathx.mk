9993efc8a82fc02b201d44da246d080bfbc2bd4f
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-09-30 10:27:51 +0200

    add comments about abspathx module

--- config/abspathx.mk
+++ config/abspathx.mk
@@ -1,3 +1,16 @@
+# This module defines the `abspathx' wrapper function.
+#
+# The `abspathx' function takes interprets its argument as one file
+# name and returns the absolute path.  It handles spaces in file name
+# by
+#
+# 1. replacing all spaces in the file name by ?
+# 2. applying `abspath' to the result
+# 3. replacing all ? in the file name by spaces
+#
+# Important note: `abspathx' considers the argument to be *one* file
+# name, which might include spaces.  This implies that it does not
+# work with multiple file names which are separated by spaces or tabs.
 
 empty:=
 
