263b85568f77e17531811404fce7aa203018e848
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-10-24 19:25:15 +0200

    add test for full one-loop EWSB in the Z3 violating NMSSM

--- test/test_SMSSM_ewsb.cpp
+++ test/test_SMSSM_ewsb.cpp
@@ -128,3 +128,81 @@ BOOST_AUTO_TEST_CASE( test_SMSSM_one_loop_tadpoles )
    BOOST_CHECK_CLOSE_FRACTION(Re(tadpole_hh_2) / vu, tadpole_ss_2, 1.0e-12);
    BOOST_CHECK_CLOSE_FRACTION(Re(tadpole_hh_3) / vS, tadpole_ss_3, 1.0e-12);
 }
+
+BOOST_AUTO_TEST_CASE( test_SMSSM_one_loop_ewsb )
+{
+   SMSSM_input_parameters input;
+   input.m0       = 540.;
+   input.Azero    = -350.;
+   input.MSInput  = 290.;
+   input.BMSInput = 400.;
+   input.L1Input  = 300.;
+   SMSSM<Two_scale> m(input);
+   NmssmSoftsusy s;
+   const double precision = 1.0e-5;
+   setup_SMSSM(m, s, input);
+
+   softsusy::Z3 = false;
+   softsusy::numRewsbLoops = 1;
+
+   // initial guess
+   m.set_Kappa(0.1);
+   m.set_vS(5000.);
+   m.set_ms2(-Sqr(input.m0));
+   m.set_mHu2(-Sqr(input.m0));
+   m.set_mHd2(Sqr(input.m0));
+
+   s.setKappa(m.get_Kappa());
+   s.setSvev(m.get_vS());
+   s.setMsSquared(m.get_ms2());
+   s.setMh1Squared(m.get_mHd2());
+   s.setMh2Squared(m.get_mHu2());
+
+   s.calcDrBarPars();
+   m.calculate_DRbar_parameters();
+
+   // chech that there this is a valid point
+   if (m.get_problems().have_problem()) {
+      INFO("FlexibleSUSY problem detected: ");
+      m.get_problems().print();
+      INFO("");
+   }
+   BOOST_REQUIRE(!m.get_problems().have_problem());
+   BOOST_REQUIRE(!s.displayProblem().test());
+
+   const double mt = s.displayDrBarPars().mt;
+   const int signMu = 1;
+   const double vd = m.get_vd();
+   const double vu = m.get_vu();
+   const double vS = m.get_vS();
+
+   m.set_ewsb_iteration_precision(precision);
+   m.solve_ewsb_one_loop();
+
+   const Complex tadpole_hh_1(m.tadpole_hh(1));
+   const Complex tadpole_hh_2(m.tadpole_hh(2));
+   const Complex tadpole_hh_3(m.tadpole_hh(3));
+
+   BOOST_CHECK_SMALL(Im(tadpole_hh_1), 1.0e-12);
+   BOOST_CHECK_SMALL(Im(tadpole_hh_2), 1.0e-12);
+   BOOST_CHECK_SMALL(Im(tadpole_hh_3), 1.0e-12);
+
+   BOOST_CHECK_SMALL(m.get_ewsb_eq_vd() - Re(tadpole_hh_1), precision);
+   BOOST_CHECK_SMALL(m.get_ewsb_eq_vu() - Re(tadpole_hh_2), precision);
+   BOOST_CHECK_SMALL(m.get_ewsb_eq_vS() - Re(tadpole_hh_3), precision);
+
+   softsusy::numRewsbLoops = 1;
+   s.rewsb(signMu, mt);
+
+   const double mu_ss  = s.displaySusyMu();
+   const double bmu_ss = s.displayM3Squared();
+   const double xis_ss = s.displayXiS();
+
+   const double mu_fs  = m.get_Mu();
+   const double bmu_fs = m.get_BMu();
+   const double xis_fs = m.get_LL1();
+
+   BOOST_CHECK_CLOSE_FRACTION(mu_ss , mu_fs , 5.0e-11);
+   BOOST_CHECK_CLOSE_FRACTION(bmu_ss, bmu_fs, 2.0e-10);
+   BOOST_CHECK_CLOSE_FRACTION(xis_ss, xis_fs, 1.0e-10);
+}
