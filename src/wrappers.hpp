2597dbd517ed2f74880f00aeb2d7ace1ee5e8289
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-11-10 09:11:04 +0100

    generalize the DoubleVector <-> Eigen matrix conversion routines

--- src/wrappers.hpp
+++ src/wrappers.hpp
@@ -225,9 +225,44 @@ Eigen::ArrayXd ToEigenArray(const DoubleVector&);
 Eigen::ArrayXd ToEigenArray(double);
 std::valarray<double> ToValarray(const DoubleVector&);
 std::valarray<double> ToValarray(double);
-DoubleVector ToDoubleVector(const Eigen::ArrayXd&);
 Eigen::MatrixXd ToEigenMatrix(const DoubleMatrix&);
-DoubleMatrix ToDoubleMatrix(const Eigen::MatrixXd&);
+
+template<class Derived>
+DoubleVector ToDoubleVector(const Eigen::ArrayBase<Derived>& a)
+{
+   DoubleVector v(a.rows());
+   for (int i = 0; i < a.rows(); i++)
+      v(i + 1) = a(i);
+   return v;
+}
+
+template<class Derived>
+ComplexMatrix ToComplexMatrix(const Eigen::MatrixBase<Derived>& m)
+{
+   const int r = m.rows();
+   const int c = m.cols();
+   ComplexMatrix result(r,c);
+
+   for (int i = 0; i < r; i++)
+      for (int k = 0; k < c; k++)
+         result(i+1, k+1) = m(i,k);
+
+   return result;
+}
+
+template<class Derived>
+DoubleMatrix ToDoubleMatrix(const Eigen::MatrixBase<Derived>& m)
+{
+   const int r = m.rows();
+   const int c = m.cols();
+   DoubleMatrix result(r,c);
+
+   for (int i = 0; i < r; i++)
+      for (int k = 0; k < c; k++)
+         result(i+1, k+1) = m(i,k);
+
+   return result;
+}
 
 inline DoubleMatrix Transpose(const DoubleMatrix& m)
 {
