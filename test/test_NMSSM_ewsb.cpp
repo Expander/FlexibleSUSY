a61949d435d3e4a141f439ff5a3f81e7fc0ad3ec
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-11-11 10:41:34 +0100

    adapt NMSSM tests to C index convention

--- test/test_NMSSM_ewsb.cpp
+++ test/test_NMSSM_ewsb.cpp
@@ -84,9 +84,9 @@ BOOST_AUTO_TEST_CASE( test_NMSSM_one_loop_tadpoles )
    const double vu = m.get_vu();
    const double vS = m.get_vS();
 
-   const Complex tadpole_hh_1(m.tadpole_hh(1));
-   const Complex tadpole_hh_2(m.tadpole_hh(2));
-   const Complex tadpole_hh_3(m.tadpole_hh(3));
+   const Complex tadpole_hh_1(m.tadpole_hh(0));
+   const Complex tadpole_hh_2(m.tadpole_hh(1));
+   const Complex tadpole_hh_3(m.tadpole_hh(2));
 
    const double tadpole_ss_1 = s.doCalcTadpole1oneLoop(mt, sinthDRbar);
    const double tadpole_ss_2 = s.doCalcTadpole2oneLoop(mt, sinthDRbar);
@@ -132,9 +132,9 @@ BOOST_AUTO_TEST_CASE( test_NMSSM_one_loop_ewsb )
    m.set_ewsb_iteration_precision(precision);
    m.solve_ewsb_one_loop();
 
-   const Complex tadpole_hh_1(m.tadpole_hh(1));
-   const Complex tadpole_hh_2(m.tadpole_hh(2));
-   const Complex tadpole_hh_3(m.tadpole_hh(3));
+   const Complex tadpole_hh_1(m.tadpole_hh(0));
+   const Complex tadpole_hh_2(m.tadpole_hh(1));
+   const Complex tadpole_hh_3(m.tadpole_hh(2));
 
    BOOST_CHECK_SMALL(Im(tadpole_hh_1), 1.0e-12);
    BOOST_CHECK_SMALL(Im(tadpole_hh_2), 1.0e-12);
