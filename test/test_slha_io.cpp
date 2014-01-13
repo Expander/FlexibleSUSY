ec3cc0c3dd700766e6676d35b1dca6142f628162
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2014-01-13 09:26:16 +0100

    adding speed test for the SLHA_io tuple processors

--- test/test_slha_io.cpp
+++ test/test_slha_io.cpp
@@ -5,6 +5,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "slha_io.hpp"
+#include "stopwatch.hpp"
 #include <Eigen/Core>
 
 using namespace flexiblesusy;
@@ -78,4 +79,99 @@ BOOST_AUTO_TEST_CASE( test_read_block_dense )
    BOOST_CHECK_EQUAL(matrix(0,1), 2.0);
    BOOST_CHECK_EQUAL(matrix(1,0), 3.0);
    BOOST_CHECK_EQUAL(matrix(1,1), 4.0);
+}
+
+/**
+ * Creates a SLHA_io object with one block named `TestBlock' with
+ *  `number_of_entries' entries of the form
+ *
+ *  Block TestBlock
+ *     0  0
+ *     1  1
+ *     2  2
+ *     3  3
+ *
+ * @param number_of_entries number of block entries
+ *
+ * @return a SLHA_io object with one block named `TestBlock'
+ */
+SLHA_io create_block(int number_of_entries)
+{
+   SLHAea::Coll coll;
+   SLHAea::Block block;
+   SLHA_io reader;
+   std::string str = "Block TestBlock\n";
+
+   for (int i = 0; i < number_of_entries; i++) {
+      const std::string num(std::to_string(i));
+      str += "   " + num + "  " + num + "\n";
+   }
+
+   block.str(str);
+   coll.push_back(block);
+   reader.set_data(coll);
+
+   return reader;
+}
+
+void process_tuple(double* array, int key, double value)
+{
+   array[key] = value;
+}
+
+void check_array(double* array, int number_of_entries)
+{
+   for (int i = 0; i < number_of_entries; i++)
+      BOOST_CHECK_EQUAL(array[i], i);
+}
+
+void reset_array(double* array, int number_of_entries, double value = 0.)
+{
+   for (int i = 0; i < number_of_entries; i++)
+      array[i] = value;
+}
+
+/**
+ * This test compares the speed of reading an array from a long SLHA
+ * block using a) a tuple processor and b) a for loop.  The tuple
+ * processor is more complicated to use, but is more than 1000 times
+ * faster than the for loop.
+ */
+BOOST_AUTO_TEST_CASE( test_processor_vs_loop )
+{
+   using namespace std::placeholders;
+   Stopwatch timer;
+   double processor_time = 0., loop_time = 0.;
+   const int number_of_entries = 10000;
+   SLHA_io reader = create_block(number_of_entries);
+
+   double array[number_of_entries];
+   reset_array(array, number_of_entries);
+
+   SLHA_io::Tuple_processor processor
+      = std::bind(&process_tuple, array, _1, _2);
+
+   // measure time to read the array using a tuple processor
+   timer.start();
+   reader.read_block("TestBlock", processor);
+   timer.stop();
+   processor_time = timer.get_time_in_seconds();
+
+   check_array(array, number_of_entries);
+   reset_array(array, number_of_entries);
+
+   // measure time to read the array using a for loop
+   timer.start();
+   for (int i = 0; i < number_of_entries; i++) {
+      array[i] = reader.read_entry("TestBlock", i);
+   }
+   timer.stop();
+   loop_time = timer.get_time_in_seconds();
+
+   check_array(array, number_of_entries);
+
+   BOOST_MESSAGE("time using the tuple processor: " << processor_time << " s");
+   BOOST_MESSAGE("time using the for loop: " << loop_time << " s");
+
+   BOOST_CHECK_LT(1000. * processor_time, loop_time);
 }
