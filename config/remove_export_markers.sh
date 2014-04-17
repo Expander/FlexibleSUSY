47159f57e19b14722ee0179484b8453c2300428f
Jae-hyeon Park <jhyeon@gmail.com>
2014-04-17 17:36:34 +0200

    Revert "Simplify config/remove_export_markers.sh"
    This reverts commit 995b884f4f641c9bccafff8749fd058ecbf32e17.
    --
    However, you removed a subtile feature: When calling
    
       ./config/remove_export_markers.sh
    
    (i.e. without any arguments) the script will stuck.  In the previous
    version I catched this this by the following if condition:
    
       if [ ! -t 0 ]; then
          # do not wait for more stdin
       fi
    
    Would you mind adding such a condition again, please?  The reason is
    that this script is called inside make like this
    
      ./config/remove_export_markers.sh $(SOME_VARIABLE)
    
    And in the stupid case where $(SOME_VARIABLE) is empty (for what
    reason what so ever), make will stuck, because the script stucks.
    
    Sorry, if I'm too pedantic here.  :)
    
    Best,
    Alex

--- config/remove_export_markers.sh
+++ config/remove_export_markers.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-# This script removes lines between two markers from files (or stdin).
+# This script removes lines beween two markers from a file (or stdin).
 # The markers are removed as well.
-# All arguments understood by sed are accepted such as `-i'.
 
 begin_marker="# *BEGIN: *NOT EXPORTED *#*"
 end_marker="# *END: *NOT EXPORTED *#*"
 
-exec sed -e "/${begin_marker}/,/${end_marker}/d" "$@"
+if test $# -lt 1; then
+    # read from stdin
+    if [ ! -t 0 ]; then
+        sed -e "/${begin_marker}/,/${end_marker}/d" < /dev/stdin
+    fi
+else
+    # read from file
+    input_file="$1"
+    sed -e "/${begin_marker}/,/${end_marker}/d" ${input_file}
+fi
