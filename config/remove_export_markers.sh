995b884f4f641c9bccafff8749fd058ecbf32e17
Jae-hyeon Park <jhyeon@gmail.com>
2014-04-17 16:36:23 +0200

    Simplify config/remove_export_markers.sh

--- config/remove_export_markers.sh
+++ config/remove_export_markers.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-# This script removes lines beween two markers from a file (or stdin).
+# This script removes lines between two markers from files (or stdin).
 # The markers are removed as well.
+# All arguments understood by sed are accepted such as `-i'.
 
 begin_marker="# *BEGIN: *NOT EXPORTED *#*"
 end_marker="# *END: *NOT EXPORTED *#*"
 
-if test $# -lt 1; then
-    # read from stdin
-    if [ ! -t 0 ]; then
-        sed -e "/${begin_marker}/,/${end_marker}/d" < /dev/stdin
-    fi
-else
-    # read from file
-    input_file="$1"
-    sed -e "/${begin_marker}/,/${end_marker}/d" ${input_file}
-fi
+exec sed -e "/${begin_marker}/,/${end_marker}/d" "$@"
