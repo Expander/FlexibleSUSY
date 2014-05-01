731a40be55c0acc57edb639b2ab45eb083f02cb2
Expander <Hole.destructor@gmx.de>
2014-05-01 15:57:46 +0200

    adding plot all script which executes the root script

---
+++ plot_all.sh
@@ -0,0 +1,10 @@
+
+# MSSM
+
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scanMSSM.dat\",\"CMSSM \$m_{h}$ / GeV\")
+
+# NMSSM
+
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scanNMSSM_lamda-0.1.dat\",\"CNMSSM \$m_{h}$ / GeV\")
+
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scanNMSSM_lamda-0.2.dat\",\"CNMSSM \$m_{h}$ / GeV\")
