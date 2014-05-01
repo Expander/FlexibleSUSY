b98442e37f131ce402296cd1e23fa83648c7192f
Expander <Hole.destructor@gmx.de>
2014-05-01 15:33:31 +0200

    factor out helper function

--- higgs-study/plotMSSM.C
+++ higgs-study/plotMSSM.C
@@ -13,30 +13,7 @@
 #include "TString.h"
 #include "TStyle.h"
 
-void SetZminZmax(TProfile2D* hist)
-{
-   Double_t min = DBL_MAX;
-   Double_t max = DBL_MIN;
-
-   for (Int_t x = 1; x <= hist->GetNbinsX(); ++x) {
-      for (Int_t y = 1; y <= hist->GetNbinsY(); ++y) {
-         const Int_t bin = hist->GetBin(x, y);
-         const Double_t entries = hist->GetBinEntries(bin);
-         if (entries > 0) {
-            const Double_t content = hist->GetBinContent(bin);
-            if (content < min)
-               min = content;
-            if (content > max)
-               max = content;
-         }
-      }
-   }
-   min -= 0.01 * TMath::Abs(min);
-   max += 0.01 * TMath::Abs(max);
-
-   hist->SetMinimum(static_cast<Double_t>(min));
-   hist->SetMaximum(static_cast<Double_t>(max));
-}
+#include "plot.h"
 
 void plotMSSM(const TString& file_name = "higgs-study/data/scanMSSM.dat")
 {
