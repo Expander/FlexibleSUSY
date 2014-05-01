4331767015fc1ebbce19f81568ad3acb7788f042
Expander <Hole.destructor@gmx.de>
2014-05-01 17:31:34 +0200

    plot all files by default

--- plot_all.sh
+++ plot_all.sh
@@ -1,10 +1,17 @@
 
 # MSSM
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_MSSM.dat\",\"CMSSM \$m_{h}$ / GeV\")
+# root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_MSSM.dat\",\"CMSSM \$m_{h}$ / GeV\")
 
 # NMSSM
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_NMSSM_lamda-0.1.dat\",\"CNMSSM \$m_{h}$ / GeV\")
+# root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_NMSSM_lambda-0.1.dat\",\"CNMSSM \$m_{h}$ / GeV\")
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_NMSSM_lamda-0.2.dat\",\"CNMSSM \$m_{h}$ / GeV\")
+# root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_NMSSM_lambda-0.2.dat\",\"CNMSSM \$m_{h}$ / GeV\")
+
+files="`ls higgs-study/data/scan_*.dat`"
+
+for f in ${files}
+do
+    root -l -b -q higgs-study/plot2d.C+(\"${f}\",\"\$m_{h}$ / GeV\")
+done
