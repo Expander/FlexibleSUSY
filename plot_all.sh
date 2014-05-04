12c9e0525125215ef129baa3cfafc1c939e88327
Expander <Hole.destructor@gmx.de>
2014-05-04 13:05:41 +0200

    remove -x

--- plot_all.sh
+++ plot_all.sh
@@ -1,14 +1,4 @@
 
-# MSSM
-
-# root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_MSSM.dat\",\"CMSSM \$m_{h}$ / GeV\")
-
-# NMSSM
-
-# root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_NMSSM_lambda-0.1.dat\",\"CNMSSM \$m_{h}$ / GeV\")
-
-# root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_NMSSM_lambda-0.2.dat\",\"CNMSSM \$m_{h}$ / GeV\")
-
 files="`ls higgs-study/data/scan_*.dat`"
 
 for f in ${files}
