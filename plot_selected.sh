1dc74ea9f14b22c80aed25174693815d20d0c164
Expander <Hole.destructor@gmx.de>
2014-05-16 15:02:03 +0200

    update scan data after gluino phase bugfix

--- plot_selected.sh
+++ plot_selected.sh
@@ -1,27 +1,27 @@
 
 # MSSM
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_MSSM.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_MSSM.dat\")
 
 # NMSSM
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_NMSSM_lambda-0.1.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_NMSSM_lambda-0.2.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_NMSSM_lambda-0.1-a0-5000.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_NMSSM_lambda-0.2-a0-5000.dat\")
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_2_negative_A/scan_NMSSM_lambda-0.1_negative_A.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_2_negative_A/scan_NMSSM_lambda-0.2_negative_A.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_NMSSM_lambda-0.1-a0--5000.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_NMSSM_lambda-0.2-a0--5000.dat\")
 
 # phdUMSSM
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdUMSSM_lambda-0.1_vs-10000.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdUMSSM_lambda-0.2_vs-10000.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdUMSSM_lambda-0.4_vs-10000.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdUMSSM_lambda-0.1_vs-10000.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdUMSSM_lambda-0.2_vs-10000.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdUMSSM_lambda-0.4_vs-10000.dat\")
 
 # phdE6SSM
 
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdE6SSM_lambda-0.1_vs-10000_kappa-0.1.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdE6SSM_lambda-0.1_vs-10000_kappa-0.2.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdE6SSM_lambda-0.2_vs-10000_kappa-0.1.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdE6SSM_lambda-0.2_vs-10000_kappa-0.2.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdE6SSM_lambda-0.4_vs-10000_kappa-0.1.dat\")
-root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_1/scan_phdE6SSM_lambda-0.4_vs-10000_kappa-0.2.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdE6SSM_lambda-0.1_vs-10000_kappa-0.1.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdE6SSM_lambda-0.1_vs-10000_kappa-0.2.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdE6SSM_lambda-0.2_vs-10000_kappa-0.1.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdE6SSM_lambda-0.2_vs-10000_kappa-0.2.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdE6SSM_lambda-0.4_vs-10000_kappa-0.1.dat\")
+root -l -b -q higgs-study/plot2d.C+(\"higgs-study/data/scan_3/scan_phdE6SSM_lambda-0.4_vs-10000_kappa-0.2.dat\")
