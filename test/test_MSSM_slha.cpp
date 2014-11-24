09beabbd0804b00d4116493de66af699df4b3b4b
Alexander Voigt <alexander.voigt@desy.de>
2014-11-24 14:41:18 +0100

    adding tests for SLHA wrapper class

---
+++ test/test_MSSM_slha.cpp
@@ -0,0 +1,192 @@
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_MSSM_slha
+
+#include <boost/test/unit_test.hpp>
+
+#include "MSSM_two_scale_model.hpp"
+#include "MSSM_two_scale_model_slha.hpp"
+
+using namespace flexiblesusy;
+
+BOOST_AUTO_TEST_CASE( test_MSSM_two_scale_slha_cctor )
+{
+   MSSM_input_parameters input;
+   input.m0 = 125.;
+   input.m12 = 500.;
+   input.TanBeta = 10.;
+   input.SignMu = 1;
+   input.Azero = 0.;
+
+   const double ALPHASMZ = 0.1176;
+   const double ALPHAMZ = 1.0 / 127.918;
+   const double sinthWsq = 0.23122;
+   const double alpha1 = 5 * ALPHAMZ / (3 * (1 - sinthWsq));
+   const double alpha2 = ALPHAMZ / sinthWsq;
+   const double g1 = sqrt(4 * PI * alpha1);
+   const double g2 = sqrt(4 * PI * alpha2);
+   const double g3 = sqrt(4 * PI * ALPHASMZ);
+   const double tanBeta = 10;
+   const double sinBeta = sin(atan(tanBeta));
+   const double cosBeta = cos(atan(tanBeta));
+   const double M12 = 100.0;
+   const double m0 = 250.0;
+   const double a0 = 50.0;
+   const double root2 = sqrt(2.0);
+   const double vev = 246.0;
+   const double vu = vev * sinBeta;
+   const double vd = vev * cosBeta;
+   const double susyMu = 120.0;
+   const double BMu = sqr(2.0 * susyMu);
+   DoubleMatrix Yu_SS(3,3), Yd_SS(3,3), Ye_SS(3,3);
+   Yu_SS(3,3) = 165.0   * root2 / (vev * sinBeta);
+   Yd_SS(3,3) = 2.9     * root2 / (vev * cosBeta);
+   Ye_SS(3,3) = 1.77699 * root2 / (vev * cosBeta);
+   DoubleMatrix ID(3, 3), mm0_SS(3, 3);
+   for (int i=1; i<=3; i++) ID(i, i) = 1.0;
+   mm0_SS = ID * sqr(m0);
+
+   Eigen::Matrix<double,3,3> Yu(Eigen::Matrix<double,3,3>::Zero()),
+      Yd(Eigen::Matrix<double,3,3>::Zero()),
+      Ye(Eigen::Matrix<double,3,3>::Zero()),
+      mm0(Eigen::Matrix<double,3,3>::Zero());
+   Yu(2,2) = 165.0   * root2 / (vev * sinBeta);
+   Yd(2,2) = 2.9     * root2 / (vev * cosBeta);
+   Ye(2,2) = 1.77699 * root2 / (vev * cosBeta);
+   mm0 = sqr(m0) * Eigen::Matrix<double,3,3>::Identity();
+
+   MSSM<Two_scale> model(input);
+   model.set_scale(91);
+   model.set_loops(1);
+   model.set_g1(g1);
+   model.set_g2(g2);
+   model.set_g3(g3);
+   model.set_Yu(Yu);
+   model.set_Yd(Yd);
+   model.set_Ye(Ye);
+   model.set_MassB(M12);
+   model.set_MassG(M12);
+   model.set_MassWB(M12);
+   model.set_mq2(mm0);
+   model.set_ml2(mm0);
+   model.set_md2(mm0);
+   model.set_mu2(mm0);
+   model.set_me2(mm0);
+   model.set_mHd2(sqr(m0));
+   model.set_mHu2(sqr(m0));
+   model.set_TYu(a0 * Yu);
+   model.set_TYd(a0 * Yd);
+   model.set_TYe(a0 * Ye);
+   model.set_Mu(susyMu);
+   model.set_BMu(BMu);
+   model.set_vu(vu);
+   model.set_vd(vd);
+
+   model.solve_ewsb_tree_level();
+   model.calculate_spectrum();
+
+   BOOST_REQUIRE(!model.get_problems().have_problem());
+
+   // check that model is in non-SLHA convention
+   BOOST_CHECK_GT(model.get_physical().MChi.maxCoeff(), 0.);
+   BOOST_CHECK_GT(model.get_physical().MChi.minCoeff(), 0.);
+   BOOST_CHECK_GT(model.get_physical().ZN.imag().maxCoeff(), 0.);
+
+   // fill SLHA wrapper class
+   // automatic conversion to SLHA happens here
+   MSSM_slha<Two_scale> slha_model(model);
+
+   // check that model wrapper is in SLHA convention
+   BOOST_CHECK_GT(slha_model.get_physical_slha().MChi.maxCoeff(), 0.);
+   BOOST_CHECK_LT(slha_model.get_physical_slha().MChi.minCoeff(), 0.);
+   BOOST_CHECK_EQUAL(slha_model.get_physical_slha().ZN.imag().maxCoeff(), 0.);
+}
+
+BOOST_AUTO_TEST_CASE( test_MSSM_two_scale_slha_calculate_spectrum )
+{
+   MSSM_input_parameters input;
+   input.m0 = 125.;
+   input.m12 = 500.;
+   input.TanBeta = 10.;
+   input.SignMu = 1;
+   input.Azero = 0.;
+
+   const double ALPHASMZ = 0.1176;
+   const double ALPHAMZ = 1.0 / 127.918;
+   const double sinthWsq = 0.23122;
+   const double alpha1 = 5 * ALPHAMZ / (3 * (1 - sinthWsq));
+   const double alpha2 = ALPHAMZ / sinthWsq;
+   const double g1 = sqrt(4 * PI * alpha1);
+   const double g2 = sqrt(4 * PI * alpha2);
+   const double g3 = sqrt(4 * PI * ALPHASMZ);
+   const double tanBeta = 10;
+   const double sinBeta = sin(atan(tanBeta));
+   const double cosBeta = cos(atan(tanBeta));
+   const double M12 = 100.0;
+   const double m0 = 250.0;
+   const double a0 = 50.0;
+   const double root2 = sqrt(2.0);
+   const double vev = 246.0;
+   const double vu = vev * sinBeta;
+   const double vd = vev * cosBeta;
+   const double susyMu = 120.0;
+   const double BMu = sqr(2.0 * susyMu);
+   DoubleMatrix Yu_SS(3,3), Yd_SS(3,3), Ye_SS(3,3);
+   Yu_SS(3,3) = 165.0   * root2 / (vev * sinBeta);
+   Yd_SS(3,3) = 2.9     * root2 / (vev * cosBeta);
+   Ye_SS(3,3) = 1.77699 * root2 / (vev * cosBeta);
+   DoubleMatrix ID(3, 3), mm0_SS(3, 3);
+   for (int i=1; i<=3; i++) ID(i, i) = 1.0;
+   mm0_SS = ID * sqr(m0);
+
+   Eigen::Matrix<double,3,3> Yu(Eigen::Matrix<double,3,3>::Zero()),
+      Yd(Eigen::Matrix<double,3,3>::Zero()),
+      Ye(Eigen::Matrix<double,3,3>::Zero()),
+      mm0(Eigen::Matrix<double,3,3>::Zero());
+   Yu(2,2) = 165.0   * root2 / (vev * sinBeta);
+   Yd(2,2) = 2.9     * root2 / (vev * cosBeta);
+   Ye(2,2) = 1.77699 * root2 / (vev * cosBeta);
+   mm0 = sqr(m0) * Eigen::Matrix<double,3,3>::Identity();
+
+   MSSM_slha<Two_scale> model(input);
+   model.set_scale(91);
+   model.set_loops(1);
+   model.set_g1(g1);
+   model.set_g2(g2);
+   model.set_g3(g3);
+   model.set_Yu(Yu);
+   model.set_Yd(Yd);
+   model.set_Ye(Ye);
+   model.set_MassB(M12);
+   model.set_MassG(M12);
+   model.set_MassWB(M12);
+   model.set_mq2(mm0);
+   model.set_ml2(mm0);
+   model.set_md2(mm0);
+   model.set_mu2(mm0);
+   model.set_me2(mm0);
+   model.set_mHd2(sqr(m0));
+   model.set_mHu2(sqr(m0));
+   model.set_TYu(a0 * Yu);
+   model.set_TYd(a0 * Yd);
+   model.set_TYe(a0 * Ye);
+   model.set_Mu(susyMu);
+   model.set_BMu(BMu);
+   model.set_vu(vu);
+   model.set_vd(vd);
+
+   model.solve_ewsb_tree_level();
+   model.calculate_spectrum(); // conversion happens here
+
+   BOOST_REQUIRE(!model.get_problems().have_problem());
+
+   // check that model class is in non-SLHA convention
+   BOOST_CHECK_GT(model.get_physical().MChi.maxCoeff(), 0.);
+   BOOST_CHECK_GT(model.get_physical().MChi.minCoeff(), 0.);
+   BOOST_CHECK_GT(model.get_physical().ZN.imag().maxCoeff(), 0.);
+
+   // check that model wrapper class is in SLHA convention
+   BOOST_CHECK_GT(model.get_physical_slha().MChi.maxCoeff(), 0.);
+   BOOST_CHECK_LT(model.get_physical_slha().MChi.minCoeff(), 0.);
+   BOOST_CHECK_EQUAL(model.get_physical_slha().ZN.imag().maxCoeff(), 0.);
+}
