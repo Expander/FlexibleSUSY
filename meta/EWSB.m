4ce618bf55ccda8117bdffa9fd6cb52b404f31cb
Alexander Voigt <Alexander.Voigt@desy.de>
2014-12-25 10:37:40 +0100

    convert parameter indices only if not of the form L[_], B[_], Q[_], ...

--- meta/EWSB.m
+++ meta/EWSB.m
@@ -487,7 +487,7 @@ CreateEWSBRootFinders[rootFinders_List] :=
 CreateIndices[indices_List] :=
     "(" <> WriteOut`StringJoinWithSeparator[ToString /@ indices,","] <> ")";
 
-SetEWSBSolution[par_[indices__], idx_, func_String] :=
+SetEWSBSolution[par_[indices__] /; MemberQ[Join[Parameters`GetModelParameters[],Parameters`GetOutputParameters[]],par], idx_, func_String] :=
     CConversion`ToValidCSymbolString[par] <> CreateIndices[{indices}] <> " = " <> func <> "(" <> ToString[idx-1] <> ");\n";
 
 SetEWSBSolution[par_, idx_, func_String] :=
