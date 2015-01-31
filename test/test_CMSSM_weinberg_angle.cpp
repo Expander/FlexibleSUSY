a7267c4055431a6c027fdf42c5ecf12ab6999287
Alexander Voigt <Alexander.Voigt@desy.de>
2015-01-31 19:33:53 +0100

    setup weinberg angle class

--- test/test_CMSSM_weinberg_angle.cpp
+++ test/test_CMSSM_weinberg_angle.cpp
@@ -35,16 +35,27 @@ BOOST_AUTO_TEST_CASE( test_rho_sinTheta )
    double outrho = 1.0, outsin = 0.48;
    const double tol = 1.0e-8;
    const int maxTries = 20;
+   const double gfermi = Electroweak_constants::gfermi;
    const double pizztMZ = ss.piZZT(mz_pole, scale, true);
    const double piwwt0  = ss.piWWT(0., scale, true);
    const double piwwtMW = ss.piWWT(mw_pole, scale, true);
    const double alphaMsbar = ss.displayDataSet().displayAlpha(ALPHA);
    const double alphaDrbar = ss.qedSusythresh(alphaMsbar, scale);
 
+   softsusy::GMU = gfermi;
+
    ss.rhohat(outrho, outsin, alphaDrbar, pizztMZ, piwwt0, piwwtMW,
              tol, maxTries);
 
    Weinberg_angle weinberg;
+
+   weinberg.set_number_of_iterations(maxTries);
+   weinberg.set_precision_goal(tol);
+   weinberg.set_alpha_em_drbar(alphaDrbar);
+   weinberg.set_fermi_contant(gfermi);
+   weinberg.set_self_energy_z_at_mz(pizztMZ);
+   weinberg.set_self_energy_z_at_0(piwwt0);
+   weinberg.set_self_energy_w_at_mw(piwwtMW);
 
    const double fs_sintheta = weinberg.calculate();
    const double fs_rhohat   = weinberg.get_rho_hat();
