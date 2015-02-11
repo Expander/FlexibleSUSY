2549eb809c074b0af1631c6d781b32be01c7f468
Alexander Voigt <Alexander.Voigt@desy.de>
2015-02-11 19:13:12 +0100

    adding convergence tester expressions for model parameters

--- meta/ConvergenceTester.m
+++ meta/ConvergenceTester.m
@@ -17,6 +17,14 @@ CountNumberOfParameters[parameter_[__]] :=
 CountNumberOfParameters[parameter_] :=
     If[Parameters`IsRealParameter[parameter], 1, 2] * (Times @@ Parameters`GetParameterDimensions[parameter]);
 
+(* Maps tensor indices to linear space *)
+IndexMapping[{_}, {i1_String}] :=
+    i1;
+
+IndexMapping[d_List, i_List] :=
+    IndexMapping[Take[d,Length[d]-1], Take[i,Length[i]-1]] <>
+    " + " <> ToString[Times @@ Take[d,Length[d]-1]] <> "*" <> Last[i];
+
 CalcDifference[FlexibleSUSY`M[particle_], offset_Integer, diff_String] :=
     Module[{result, body, dim, dimStart, esStr},
            dim = TreeMasses`GetDimension[particle];
@@ -32,6 +40,61 @@ CalcDifference[FlexibleSUSY`M[particle_], offset_Integer, diff_String] :=
                      "MaxRelDiff(OLD1(" <> esStr <> ",i),NEW1(" <> esStr <> ",i));";
               result = result <> IndentText[body] <> "\n}\n";
              ];
+           Return[result];
+          ];
+
+CalcDifference[parameter_[], offset_Integer, diff_String] :=
+    Module[{result, parStr},
+           parStr = ToValidCSymbolString[parameter];
+           result = diff <> "[" <> ToString[offset] <> "] = " <>
+                    "MaxRelDiff(OLD(" <> parStr <> "),NEW(" <> parStr <> "));\n";
+           Return[result];
+          ];
+
+CalcDifference[parameter_[idx___], offset_Integer, diff_String] :=
+    Module[{result, parStr, dim, dimStr},
+           dim = Length[{idx}];
+           dimStr = ToString[dim];
+           parStr = ToValidCSymbolString[parameter] <> "," <>
+                    Utils`StringJoinWithSeparator[ToString[#-1]& /@ {idx},","];
+           result = diff <> "[" <> ToString[offset] <> "] = " <>
+                    "MaxRelDiff(OLD" <> dimStr <> "(" <> parStr <> "),NEW" <> dimStr <> "(" <> parStr <> "));\n";
+           Return[result];
+          ];
+
+CalcDifference[parameter_ /; Parameters`GetParameterDimensions[parameter] == {1}, offset_Integer, diff_String] :=
+    CalcDifference[parameter[], offset, diff];
+
+CalcDifference[parameter_, offset_Integer, diff_String, idx_List, pos_Integer /; pos > (Length[idx]+1), idxPool_List] :=
+    Module[{body, dim, dimStr, parStr},
+           Print["pos = ", pos];
+           dim = Length[idx];
+           dimStr = ToString[dim];
+           parStr = ToValidCSymbolString[parameter] <> "," <>
+                    Utils`StringJoinWithSeparator[ToString /@ idxPool,","];
+           body = diff <> "[" <> IndexMapping[idx,idxPool] <> " + " <> ToString[offset] <> "] = " <>
+                  "MaxRelDiff(OLD" <> dimStr <> "(" <> parStr <> "),NEW" <> dimStr <> "(" <> parStr <> "));";
+           Return[body];
+          ];
+
+CalcDifference[parameter_, offset_Integer, diff_String, idx_List, pos_Integer, idxPool_List] :=
+    Module[{result, dim, dimStr, i},
+           dim = Length[idx];
+           dimStr = ToString[dim];
+           i = idxPool[[pos]];
+           result = "for (unsigned " <> i <> " = 0; " <> i <> " < " <> ToString[idx[[pos]]] <> "; " <> i <> "++) {\n" <>
+                    IndentText[
+                        CalcDifference[parameter, offset, diff, idx, pos+1, idxPool]
+                    ] <>
+                    "\n}";
+           Return[result];
+          ];
+
+CalcDifference[parameter_, offset_Integer, diff_String] :=
+    Module[{result, body, dim, parStr, idxPool},
+           dim = Parameters`GetParameterDimensions[parameter];
+           idxPool = Take[{"i", "j", "k", "l","m","n"}, Length[dim]];
+           result = CalcDifference[parameter, offset, diff, dim, 1, idxPool] <> "\n";
            Return[result];
           ];
 
