3fc182ed638c5a5b8fd57c1f7654108379622f5d
Alexander Voigt <Alexander.Voigt@desy.de>
2015-04-14 17:39:10 +0200

    adding tests for one-loop EWSB

--- test/test_CMSSMCPV_ewsb.cpp
+++ test/test_CMSSMCPV_ewsb.cpp
@@ -42,10 +42,7 @@ BOOST_AUTO_TEST_CASE( test_CMSSMCPV_ewsb_tree_level )
    BOOST_CHECK(Abs(m.get_ewsb_eq_hh_1()) > precision);
    BOOST_CHECK(Abs(m.get_ewsb_eq_hh_2()) > precision);
    BOOST_CHECK(Abs(m.get_ewsb_eq_hh_3()) > precision);
-
-   // check relation between tree-level tadpoles 3 and 4
-   const double vu = m.get_vu(), vd = m.get_vd();
-   BOOST_CHECK_CLOSE_FRACTION(m.get_ewsb_eq_hh_3(), (vu/vd) * m.get_ewsb_eq_hh_4(), 1e-10);
+   BOOST_CHECK(Abs(m.get_ewsb_eq_hh_4()) > precision);
 
    m.set_ewsb_iteration_precision(precision);
    const int error = m.solve_ewsb_tree_level();
@@ -55,4 +52,114 @@ BOOST_AUTO_TEST_CASE( test_CMSSMCPV_ewsb_tree_level )
    BOOST_CHECK_SMALL(m.get_ewsb_eq_hh_2(), precision);
    BOOST_CHECK_SMALL(m.get_ewsb_eq_hh_3(), precision);
    BOOST_CHECK_SMALL(m.get_ewsb_eq_hh_4(), precision);
+}
+
+BOOST_AUTO_TEST_CASE( test_CMSSMCPV_ewsb_one_loop )
+{
+   CMSSMCPV_input_parameters input;
+   input.m0 = 125;
+   input.m12 = 200;
+   input.TanBeta = 10;
+   input.Azero = 0;
+   input.etaInput = 0.1;
+   input.PhaseMu = std::complex<double>(1,0);
+
+   CMSSMCPV<Two_scale> m;
+   const double precision = 1.0e-5;
+   setup_CMSSMCPV(m, input);
+
+   // initial guess
+   m.set_mHu2(-Sqr(input.m0));
+   m.set_mHd2(Sqr(input.m0));
+   m.set_Mu(input.m0);
+   m.set_BMu(input.m0);
+
+   m.set_ewsb_iteration_precision(precision);
+   const int error = m.solve_ewsb_one_loop();
+
+   BOOST_REQUIRE(error == 0);
+
+   const int neq = CMSSMCPV<Two_scale>::number_of_ewsb_equations;
+   double tadpole[neq] = { 0. };
+   m.tadpole_equations(tadpole);
+
+   BOOST_CHECK_SMALL(Abs(tadpole[0]), precision);
+   BOOST_CHECK_SMALL(Abs(tadpole[1]), precision);
+   BOOST_CHECK_SMALL(Abs(tadpole[2]), precision);
+   BOOST_CHECK_SMALL(Abs(tadpole[3]), precision);
+}
+
+BOOST_AUTO_TEST_CASE( test_CMSSMCPV_tree_level_tadpoles )
+{
+   CMSSMCPV_input_parameters input;
+   input.m0 = 125;
+   input.m12 = 200;
+   input.TanBeta = 10;
+   input.Azero = 0;
+   input.etaInput = 0.1;
+   input.PhaseMu = std::complex<double>(1,0);
+
+   CMSSMCPV<Two_scale> m;
+   const double precision = 1.0e-5;
+   setup_CMSSMCPV(m, input);
+
+   // initial guess
+   m.set_mHu2(-Sqr(input.m0));
+   m.set_mHd2(Sqr(input.m0));
+   m.set_Mu(input.m0);
+   m.set_BMu(input.m0);
+
+   BOOST_CHECK(Abs(m.get_ewsb_eq_hh_1()) > precision);
+   BOOST_CHECK(Abs(m.get_ewsb_eq_hh_2()) > precision);
+   BOOST_CHECK(Abs(m.get_ewsb_eq_hh_3()) > precision);
+   BOOST_CHECK(Abs(m.get_ewsb_eq_hh_4()) > precision);
+
+   // check relation between tree-level tadpoles 3 and 4
+   const double vu = m.get_vu(), vd = m.get_vd();
+   BOOST_CHECK_CLOSE_FRACTION(m.get_ewsb_eq_hh_3(), (vu/vd) * m.get_ewsb_eq_hh_4(), 1e-10);
+}
+
+BOOST_AUTO_TEST_CASE( test_CMSSMCPV_one_loop_tadpoles )
+{
+   CMSSMCPV_input_parameters input;
+   input.m0 = 125;
+   input.m12 = 200;
+   input.TanBeta = 10;
+   input.Azero = 0;
+   input.etaInput = 0.1;
+   input.PhaseMu = std::complex<double>(1,0);
+
+   CMSSMCPV<Two_scale> m;
+   const double precision = 1.0e-5;
+   int error = 0;
+   setup_CMSSMCPV(m, input);
+
+   // initial guess
+   m.set_mHu2(-Sqr(input.m0));
+   m.set_mHd2(Sqr(input.m0));
+   m.set_Mu(input.m0);
+   m.set_BMu(input.m0);
+
+   m.set_ewsb_iteration_precision(precision);
+   error = m.solve_ewsb_tree_level();
+
+   BOOST_REQUIRE(error == 0);
+
+   m.calculate_DRbar_masses();
+
+   BOOST_CHECK(Abs(m.tadpole_hh(0)) > precision);
+   BOOST_CHECK(Abs(m.tadpole_hh(1)) > precision);
+   BOOST_CHECK(Abs(m.tadpole_hh(2)) > precision);
+   BOOST_CHECK(Abs(m.tadpole_hh(3)) > precision);
+
+   // check relation between one-loop tadpoles 3 and 4
+   const double vu = m.get_vu(), vd = m.get_vd();
+   BOOST_CHECK_CLOSE_FRACTION(Re(m.tadpole_hh(2)), (vu/vd) * Re(m.tadpole_hh(3)), 1e-10);
+
+   const int neq = CMSSMCPV<Two_scale>::number_of_ewsb_equations;
+   double tadpole[neq] = { 0. };
+   m.tadpole_equations(tadpole);
+
+   // check relation between sum of tree-level and one-loop tadpoles 3 and 4
+   BOOST_CHECK_CLOSE_FRACTION(tadpole[2], (vu/vd) * tadpole[3], 1e-10);
 }
