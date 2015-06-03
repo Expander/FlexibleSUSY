4795dea6443a4077018b1e83ebb92e3efc9e6d1a
Markus Bach <Markus.Bach@web.de>
2015-06-03 12:02:51 +0200

    rename test/test_CMSSM_weinberg_angle_new.cpp

--- test/module.mk
+++ test/module.mk
@@ -178,7 +178,7 @@ TEST_SRC += \
 		$(DIR)/test_CMSSM_slha_input.cpp \
 		$(DIR)/test_CMSSM_two_loop_spectrum.cpp \
 		$(DIR)/test_CMSSM_info.cpp \
-		$(DIR)/test_CMSSM_weinberg_angle_new.cpp
+		$(DIR)/test_CMSSM_weinberg_angle_pointer.cpp
 endif
 
 ifeq ($(shell $(FSCONFIG) --with-NMSSM),yes)
@@ -483,7 +483,7 @@ $(DIR)/test_CMSSMCKM_low_scale_constraint.x: $(LIBSoftsusyFlavourMSSM) $(LIBSoft
 
 $(DIR)/test_CMSSM_weinberg_angle.x: $(LIBSoftsusyMSSM) $(LIBCMSSM) $(LIBFLEXI) $(LIBLEGACY) $(LIBTEST) $(filter-out -%,$(LOOPFUNCLIBS))
 
-$(DIR)/test_CMSSM_weinberg_angle_new.x: $(LIBCMSSM) $(LIBFLEXI) $(LIBLEGACY) $(LIBTEST) $(filter-out -%,$(LOOPFUNCLIBS))
+$(DIR)/test_CMSSM_weinberg_angle_pointer.x: $(LIBCMSSM) $(LIBFLEXI) $(LIBLEGACY) $(LIBTEST) $(filter-out -%,$(LOOPFUNCLIBS))
 
 $(DIR)/test_CMSSMMassWInput_spectrum.x: $(LIBSoftsusyMSSM) $(LIBCMSSMMassWInput) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))
 
