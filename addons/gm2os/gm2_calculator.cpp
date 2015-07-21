0e4cc24b3e8e2a070945be223b9d5b4fe9e3c10b
Alexander Voigt <alexander.voigt@desy.de>
2015-07-21 18:29:31 +0200

    remove redundant parameters to avoid any ambiguities

--- addons/gm2os/gm2_calculator.cpp
+++ addons/gm2os/gm2_calculator.cpp
@@ -19,64 +19,96 @@
 #include "gm2_calculator.hpp"
 #include "wrappers.hpp"
 #include "logger.hpp"
+#include "numerics2.hpp"
 
 namespace flexiblesusy {
 namespace gm2 {
 
 Gm2_calculator::Gm2_calculator(const MSSMNoFV_mass_eigenstates& model_)
-   : MSSMNoFV_mass_eigenstates(model_),
-   MW(80.385), MZ(91.1876), TB(10.), EL(0.303), ME(0.00051),
-   MM(0.105), ML(1.777), MU(0.04), MC(1.5), MT(173.5), MD(0.04), MS(0.15),
-   MB(3.), gY(0.35), EL0(0.30282212), MA0(500.)
+   : MSSMNoFV_mass_eigenstates(model_)
+   , EL0(0.30282212)
 {}
 
-void Gm2_calculator::convert_parameters_reverse() {
-   set_TB(get_vu() / get_vd());
-   double gY = get_gY();
-   double g2 = get_g2();
-   set_EL(gY * g2 / sqrt(sqr(gY) + sqr(g2)));
-   double v = sqrt(sqr(get_vu()) + sqr(get_vd()));
-   set_MZ(0.5 * sqrt(sqr(gY) + sqr(g2)) * v);
-   set_MW(0.5 * g2 * v);
+Eigen::Matrix<double,3,3> Gm2_calculator::get_Ae() const {
+   Eigen::Matrix<double,3,3> Ae(get_TYe());
+   Eigen::Matrix<double,3,3> Ye(get_Ye());
+
+   for (int i = 0; i < 3; i++)
+      if (!is_zero(Ye(i,i)))
+         Ae(i,i) /= Ye(i,i);
+
+   return Ae;
 }
 
+Eigen::Matrix<double,3,3> Gm2_calculator::get_Au() const {
+   Eigen::Matrix<double,3,3> Au(get_TYu());
+   Eigen::Matrix<double,3,3> Yu(get_Yu());
 
-void Gm2_calculator::convert_parameters() {
+   for (int i = 0; i < 3; i++)
+      if (!is_zero(Yu(i,i)))
+         Au(i,i) /= Yu(i,i);
+
+   return Au;
+}
+
+Eigen::Matrix<double,3,3> Gm2_calculator::get_Ad() const {
+   Eigen::Matrix<double,3,3> Ad(get_TYd());
+   Eigen::Matrix<double,3,3> Yd(get_Yd());
+
+   for (int i = 0; i < 3; i++)
+      if (!is_zero(Yd(i,i)))
+         Ad(i,i) /= Yd(i,i);
+
+   return Ad;
+}
+
+double Gm2_calculator::get_EL() const {
+   const double gY = get_gY();
+   const double g2 = get_g2();
+   return gY * g2 / sqrt(sqr(gY) + sqr(g2));
+}
+
+void Gm2_calculator::convert_to_onshell() {
+
+   // get pole masses
+   const double MW = get_MW();
+   const double MZ = get_MZ();
+   const double MA = get_MA0();
+
+   const double TB = get_TB(); // DR-bar
+   const double EL = get_EL(); // DR-bar
+
    set_g1(sqrt(5. / 3.) * EL * MZ / MW);
    set_g2(EL / sqrt(1. - sqr(MW / MZ)));
-   double v = 2. * MW / get_g2();
+
+   const double v = 2. * MW / get_g2();
    set_vu(v / sqrt(1. + 1. / sqr(TB)));
    set_vd(get_vu() / TB);
+
    Eigen::Matrix<double,3,3> Ye_neu(get_Ye());
-   Ye_neu(0, 0) = sqrt(2.) * ME / get_vd();
-   Ye_neu(1, 1) = sqrt(2.) * MM / get_vd();
-   Ye_neu(2, 2) = sqrt(2.) * ML / get_vd();
+   Ye_neu(0, 0) = sqrt(2.) * get_ME() / get_vd();
+   Ye_neu(1, 1) = sqrt(2.) * get_MM() / get_vd();
+   Ye_neu(2, 2) = sqrt(2.) * get_ML() / get_vd();
    set_Ye(Ye_neu);
+
    Eigen::Matrix<double,3,3> Yu_neu(get_Yu());
-   Yu_neu(0, 0) = sqrt(2.) * MU / get_vu();
-   Yu_neu(1, 1) = sqrt(2.) * MC / get_vu();
-   Yu_neu(2, 2) = sqrt(2.) * MT / get_vu();
+   Yu_neu(0, 0) = sqrt(2.) * get_MU() / get_vu();
+   Yu_neu(1, 1) = sqrt(2.) * get_MC() / get_vu();
+   Yu_neu(2, 2) = sqrt(2.) * get_MT() / get_vu();
    set_Yu(Yu_neu);
+
    Eigen::Matrix<double,3,3> Yd_neu(get_Yd());
-   Yd_neu(0, 0) = sqrt(2.) * MD / get_vd();
-   Yd_neu(1, 1) = sqrt(2.) * MS / get_vd();
-   Yd_neu(2, 2) = sqrt(2.) * MB / get_vd();
+   Yd_neu(0, 0) = sqrt(2.) * get_MD() / get_vd();
+   Yd_neu(1, 1) = sqrt(2.) * get_MS() / get_vd();
+   Yd_neu(2, 2) = sqrt(2.) * get_MB() / get_vd();
    set_Yd(Yd_neu);
-   set_TYe(Ye_neu * Ae);
-   set_TYu(Yu_neu * Au);
-   set_TYd(Yd_neu * Ad);
-   double tan2b = 2. * TB / (1. - sqr(TB));
-   set_BMu(0.5 * sqr(MA0) * (tan2b / sqrt(1. + sqr(tan2b))));
-}
 
-void Gm2_calculator::calculate_DRbar_parameters() {
-   convert_parameters();
-   MSSMNoFV_mass_eigenstates::calculate_DRbar_parameters();
+   set_TYe(Ye_neu * get_Ae());
+   set_TYu(Yu_neu * get_Au());
+   set_TYd(Yd_neu * get_Ad());
 
-   MSmu = get_MSm();
-   MStau = get_MStau();
-   MSbot = get_MSb();
-   MStop = get_MSt();
+   const double tan2b = 2. * TB / (1. - sqr(TB));
+   set_BMu(0.5 * sqr(MA) * (tan2b / sqrt(1. + sqr(tan2b))));
 }
 
 } // gm2
--- addons/gm2os/gm2_calculator.hpp
+++ addons/gm2os/gm2_calculator.hpp
@@ -30,119 +30,74 @@ public:
    Gm2_calculator(const MSSMNoFV_mass_eigenstates&);
    virtual ~Gm2_calculator() {}
 
-   Eigen::Array<double,2,1> get_MSmu() const {return MSmu;}
-   Eigen::Matrix<double,2,2> get_USmu() const {return USmu;}
-   Eigen::Array<double,2,1> get_MStau() const {return MStau;}
-   Eigen::Matrix<double,2,2> get_UStau() const {return UStau;}
-   Eigen::Array<double,2,1> get_MSbot() const {return MSbot;}
-   Eigen::Matrix<double,2,2> get_USbot() const {return USbot;}
-   Eigen::Array<double,2,1> get_MStop() const {return MStop;}
-   Eigen::Matrix<double,2,2> get_UStop() const {return UStop;}
-   double get_MA0() const {return MA0;}
+   Eigen::Array<double,2,1> get_MSmu() const {return get_MSm();}
+   Eigen::Matrix<double,2,2> get_USmu() const {return get_ZM();}
+   Eigen::Array<double,2,1> get_MStau() const {return get_MStau();}
+   Eigen::Matrix<double,2,2> get_UStau() const {return get_ZTau();}
+   Eigen::Array<double,2,1> get_MSbot() const {return get_MSb();}
+   Eigen::Matrix<double,2,2> get_USbot() const {return get_ZB();}
+   Eigen::Array<double,2,1> get_MStop() const {return get_MSt();}
+   Eigen::Matrix<double,2,2> get_UStop() const {return get_ZT();}
+   double get_MA0() const {return get_MAh(1);}
    double get_MUDIM() const {return get_scale();}
    double get_EL0() const {return EL0;}
-   double get_gY() const {return sqrt(3. / 5.) * get_g1();}
-   double get_EL() const {return EL;}
-   double get_TB() const {return TB;}
-   double get_MW() const {return MW;}
-   double get_MZ() const {return MZ;}
-   double get_ME() const {return ME;}
-   double get_MM() const {return MM;}
-   double get_ML() const {return ML;}
-   double get_MU() const {return MU;}
-   double get_MC() const {return MC;}
-   double get_MT() const {return MT;}
-   double get_MD() const {return MD;}
-   double get_MS() const {return MS;}
-   double get_MB() const {return MB;}
-   Eigen::Matrix<double,3,3> get_Ae() const {return Ae;}
-   Eigen::Matrix<double,3,3> get_Au() const {return Au;}
-   Eigen::Matrix<double,3,3> get_Ad() const {return Ad;}
+   double get_gY() const {return sqrt(0.6) * get_g1();}
+   double get_EL() const;
+   double get_TB() const {return get_vu() / get_vd();}
+   double get_MW() const {return get_physical().MVWm;}
+   double get_MZ() const {return get_physical().MVZ;}
+   double get_ME() const {return get_physical().MFe;}
+   double get_MM() const {return get_physical().MFm;}
+   double get_ML() const {return get_physical().MFtau;}
+   double get_MU() const {return get_physical().MFu;}
+   double get_MC() const {return get_physical().MFc;}
+   double get_MT() const {return get_physical().MFt;}
+   double get_MD() const {return get_physical().MFd;}
+   double get_MS() const {return get_physical().MFs;}
+   double get_MB() const {return get_physical().MFb;}
+   Eigen::Matrix<double,3,3> get_Ae() const;
+   Eigen::Matrix<double,3,3> get_Au() const;
+   Eigen::Matrix<double,3,3> get_Ad() const;
 
    void set_MSmu(const Eigen::Array<double,2,1>& MSmu_new)
-   {MSmu = MSmu_new;}
-   void set_USmu(const Eigen::Matrix<double,2,2>& USmu_new)
-   {USmu = USmu_new;}
+   {get_physical().MSm = MSmu_new;}
    void set_MStau(const Eigen::Array<double,2,1>& MStau_new)
-   {MStau = MStau_new;}
-   void set_UStau(const Eigen::Matrix<double,2,2>& UStau_new)
-   {UStau = UStau_new;}
+   {get_physical().MStau = MStau_new;}
    void set_MSbot(const Eigen::Array<double,2,1>& MSbot_new)
-   {MSbot = MSbot_new;}
-   void set_USbot(const Eigen::Matrix<double,2,2>& USbot_new)
-   {USbot = USbot_new;}
+   {get_physical().MSb = MSbot_new;}
    void set_MStop(const Eigen::Array<double,2,1>& MStop_new)
-   {MStop = MStop_new;}
-   void set_UStop(const Eigen::Matrix<double,2,2>& UStop_new)
-   {UStop = UStop_new;}
-   void set_TB(double TB_new)
-   {TB = TB_new;}
+   {get_physical().MSt = MStop_new;}
    void set_MW(double MW_new)
-   {MW = MW_new;}
+   {get_physical().MVWm = MW_new;}
    void set_MZ(double MZ_new)
-   {MZ = MZ_new;}
-   void set_EL(double EL_new)
-   {EL = EL_new;}
+   {get_physical().MVZ = MZ_new;}
    void set_ME(double ME_new)
-   {ME = ME_new;}
+   {get_physical().MFe = ME_new;}
    void set_MM(double MM_new)
-   {MM = MM_new;}
+   {get_physical().MFm = MM_new;}
    void set_ML(double ML_new)
-   {ML = ML_new;}
+   {get_physical().MFtau = ML_new;}
    void set_MU(double MU_new)
-   {MU = MU_new;}
+   {get_physical().MFu = MU_new;}
    void set_MC(double MC_new)
-   {MC = MC_new;}
+   {get_physical().MFc = MC_new;}
    void set_MT(double MT_new)
-   {MT = MT_new;}
+   {get_physical().MFt = MT_new;}
    void set_MD(double MD_new)
-   {MD = MD_new;}
+   {get_physical().MFu = MD_new;}
    void set_MS(double MS_new)
-   {MS = MS_new;}
+   {get_physical().MFs = MS_new;}
    void set_MB(double MB_new)
-   {MB = MB_new;}
+   {get_physical().MFb = MB_new;}
    void set_MUDIM(double MUDIM_new)
    {set_scale(MUDIM_new);}
    void set_MA0(double MA0_new)
-   {MA0 = MA0_new;}
-   void set_Ae(const Eigen::Matrix<double,3,3>& Ae_new)
-   {Ae = Ae_new;}
-   void set_Au(const Eigen::Matrix<double,3,3>& Au_new)
-   {Au = Au_new;}
-   void set_Ad(const Eigen::Matrix<double,3,3>& Ad_new)
-   {Ad = Ad_new;}
-   void convert_parameters();
-   void calculate_DRbar_parameters();
-   void convert_parameters_reverse();
+   {get_physical().MAh(1) = MA0_new;}
+
+   void convert_to_onshell();
 
 private:
-   Eigen::Matrix<double,3,3> Ae;
-   Eigen::Matrix<double,3,3> Au;
-   Eigen::Matrix<double,3,3> Ad;
-   Eigen::Array<double,2,1> MSmu;
-   Eigen::Matrix<double,2,2> USmu;
-   Eigen::Array<double,2,1> MStau;
-   Eigen::Matrix<double,2,2> UStau;
-   Eigen::Array<double,2,1> MSbot;
-   Eigen::Matrix<double,2,2> USbot;
-   Eigen::Array<double,2,1> MStop;
-   Eigen::Matrix<double,2,2> UStop;
-   double MW;
-   double MZ;
-   double TB;
-   double EL;
-   double ME;
-   double MM;
-   double ML;
-   double MU;
-   double MC;
-   double MT;
-   double MD;
-   double MS;
-   double MB;
-   double gY;
-   double EL0;
-   double MA0;
+   double EL0; ///< electromagnetic gauge coupling in the Thompson limit
 };
 
 } // namespace gm2
