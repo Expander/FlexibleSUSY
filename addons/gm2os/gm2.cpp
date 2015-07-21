acaf0653702b26331bbe44d920dbd49bd90cb2a4
Alexander Voigt <alexander.voigt@desy.de>
2015-07-21 19:34:47 +0200

    read model parameters from SLHA input source

--- addons/gm2os/gm2.cpp
+++ addons/gm2os/gm2.cpp
@@ -16,18 +16,78 @@
 // <http://www.gnu.org/licenses/>.
 // ====================================================================
 
+#include "command_line_options.hpp"
 #include "logger.hpp"
 #include "gm2_1loop.hpp"
 #include "gm2_2loop.hpp"
-#include "MSSMNoFV_onshell.hpp"
 #include "wrappers.hpp"
+
+#include "MSSMNoFV_slha_io.hpp"
+#include "MSSMNoFV_onshell.hpp"
 
 using namespace flexiblesusy;
 
-int main()
+void print_usage(const char* program_name)
 {
+   std::cout <<
+      "Usage: " << program_name << " [options]\n"
+      "Options:\n"
+      "  --slha-input-file=<source>\n      SLHA input source (file name or - for stdin)"
+      "  --help,-h                         print this help message"
+             << std::endl;
+}
+
+struct Gm2_cmd_line_options {
+   std::string slha_input_source;
+};
+
+Gm2_cmd_line_options get_cmd_line_options(int argc, const char* argv[])
+{
+   Gm2_cmd_line_options options;
+
+   for (int i = 1; i < argc; ++i) {
+      const std::string option(argv[i]);
+
+      if (Command_line_options::starts_with(option, "--slha-input-file=")) {
+         options.slha_input_source = option.substr(18);
+         continue;
+      }
+
+      if (option == "--help" || option == "-h") {
+         print_usage(argv[0]);
+         exit(EXIT_SUCCESS);
+      }
+
+      ERROR("Unrecognized command line option: " << option);
+      exit(EXIT_FAILURE);
+   }
+
+   return options;
+}
+
+int main(int argc, const char* argv[])
+{
+   Gm2_cmd_line_options options(get_cmd_line_options(argc, argv));
+
+   if (options.slha_input_source.empty()) {
+      ERROR("No SLHA input source given!\n"
+            "   Please provide one via the option --slha-input-file=");
+      return EXIT_FAILURE;
+   }
+
    MSSMNoFV_mass_eigenstates model;
+   MSSMNoFV_slha_io slha_io;
+
+   try {
+      slha_io.read_from_source(options.slha_input_source);
+      slha_io.fill(model);
+   } catch (const Error& error) {
+      ERROR(error.what());
+      return EXIT_FAILURE;
+   }
+
    gm2os::MSSMNoFV_onshell osmodel(model);
+   osmodel.convert_to_onshell();
 
    const double gm2_1l = gm2os::calculate_gm2_1loop(osmodel);
    const double gm2_2l = amu2LFSfapprox(osmodel)
