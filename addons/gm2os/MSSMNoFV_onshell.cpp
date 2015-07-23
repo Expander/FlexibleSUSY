84fdfc5f807891c471fa575d29b451593c54e6a1
Alexander Voigt <alexander.voigt@desy.de>
2015-07-23 16:36:27 +0200

    initialize MW, MZ, MM and ren. scale to proper value

--- addons/gm2os/MSSMNoFV_onshell.cpp
+++ addons/gm2os/MSSMNoFV_onshell.cpp
@@ -47,7 +47,12 @@ MSSMNoFV_onshell::MSSMNoFV_onshell()
    : MSSMNoFVSLHA2_mass_eigenstates()
    , EL(calculate_e(ALPHA_EM_MZ))
    , EL0(calculate_e(ALPHA_EM_THOMPSON))
-{}
+{
+   get_physical().MFm  = 0.1056583715;
+   get_physical().MVWm = 80.385;
+   get_physical().MVZ  = 91.1876;
+   set_scale(get_physical().MVZ);
+}
 
 MSSMNoFV_onshell::MSSMNoFV_onshell(const MSSMNoFVSLHA2_mass_eigenstates& model_)
    : MSSMNoFVSLHA2_mass_eigenstates(model_)
