1b55cd4dd401dd5668ed39f9859f62cbce81b1f6
Alexander Voigt <alexander.voigt@desy.de>
2015-07-27 14:53:36 +0200

    export dilog and numerics2 module

--- addons/gm2os/module.mk
+++ addons/gm2os/module.mk
@@ -74,13 +74,21 @@ clean::         clean-$(MODNAME)
 
 distclean::     distclean-$(MODNAME)
 
+# dependent files in src/
+LIBgm2os_DEP_SRC := \
+		src/dilog.h \
+		src/dilog.f \
+		src/numerics2.hpp
+
 export-gm2os:
 		install -d $(LIBgm2os_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(LIBgm2os_MK) $(LIBgm2os_INSTALL_DIR)/Makefile
 		install -d $(LIBgm2os_INSTALL_DIR)/gm2
 		install -m u=rw,g=r,o=r $(LIBgm2os_SRC) $(LIBgm2os_INSTALL_DIR)/gm2
 		install -m u=rw,g=r,o=r $(LIBgm2os_HDR) $(LIBgm2os_INSTALL_DIR)/gm2
-		install -m u=rw,g=r,o=r $(LIBgm2os_MK) $(LIBgm2os_INSTALL_DIR)/Makefile
 		install -m u=rw,g=r,o=r $(LIBgm2os_gm2_MK) $(LIBgm2os_INSTALL_DIR)/gm2/module.mk
+		install -d $(LIBgm2os_INSTALL_DIR)/src
+		install -m u=rw,g=r,o=r $(LIBgm2os_DEP_SRC) $(LIBgm2os_INSTALL_DIR)/src
 
 $(LIBgm2os_DEP) $(EXEgm2os_DEP) $(LIBgm2os_OBJ) $(EXEgm2os_OBJ): CPPFLAGS += $(GSLFLAGS) $(EIGENFLAGS) $(BOOSTFLAGS)
 
