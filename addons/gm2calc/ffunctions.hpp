33e5e7fb96b902909a12b6a1d8c13e7c6e3a1bac
Alexander Voigt <Alexander.Voigt@desy.de>
2015-07-30 20:06:02 +0200

    adding documentation to F-functions

--- addons/gm2calc/ffunctions.hpp
+++ addons/gm2calc/ffunctions.hpp
@@ -22,18 +22,27 @@
 namespace flexiblesusy {
 namespace gm2calc {
 
+/// \f$F_1^C(x)\f$, Eq. (54) arXiv:hep-ph/0609168
 double F1C(double);
+/// \f$F_2^C(x)\f$, Eq. (55) arXiv:hep-ph/0609168
 double F2C(double);
 double F3C(double);
 double F4C(double);
+/// \f$F_1^N(x)\f$, Eq. (52) arXiv:hep-ph/0609168
 double F1N(double);
+/// \f$F_2^N(x)\f$, Eq. (53) arXiv:hep-ph/0609168
 double F2N(double);
 double F3N(double);
 double F4N(double);
+/// \f$F_a(x)\f$, Eq. (6.3a) arXiv:1311.1775
 double Fa(double, double);
+/// \f$F_b(x)\f$, Eq. (6.3b) arXiv:1311.1775
 double Fb(double, double);
+/// \f$G_3(x)\f$, Eq. (6.4a) arXiv:1311.1775
 double G3(double);
+/// \f$G_4(x)\f$, Eq. (6.4b) arXiv:1311.1775
 double G4(double);
+/// \f$H_2(x,y)\f$, Eq. (34) of arxiv:0901.2065
 double H2(double, double);
 double Iabc(double, double, double);
 /// \f$f_{PS}(z)\f$, Eq. (70) arXiv:hep-ph/0609168
