74a1355c886a708c57c8c2a04ad04e8adf7954e4
Alexander Voigt <Alexander.Voigt@desy.de>
2015-09-05 10:46:18 +0200

    fix compilation of sm and smcw after commit f4eed5d

--- models/sm/sm_two_scale.cpp
+++ models/sm/sm_two_scale.cpp
@@ -1,8 +1,27 @@
+// ====================================================================
+// This file is part of FlexibleSUSY.
+//
+// FlexibleSUSY is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// FlexibleSUSY is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FlexibleSUSY.  If not, see
+// <http://www.gnu.org/licenses/>.
+// ====================================================================
 
 #include "sm_two_scale.hpp"
 #include "error.hpp"
 
 #include <cassert>
+
+using namespace softsusy;
 
 namespace flexiblesusy {
 
