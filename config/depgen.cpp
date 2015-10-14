e5db9a6d84b450c5fe2d60356311d17c04aba1f6
Alexander Voigt <alexander.voigt@desy.de>
2015-10-14 13:30:31 +0200

    adding support for -MG

--- config/depgen.cpp
+++ config/depgen.cpp
@@ -80,6 +80,7 @@ void print_usage(const std::string& program_name)
       "Options:\n"
       "  -I<path>      Search for header files in <path>\n"
       "  -MF <file>    Write dependencies to <file>\n"
+      "  -MG           Add missing headers to dependency list\n"
       "  -MM           Ignore system headers enclosed by < and >\n"
       "  -MMD <file>   Equivalent to -MM -MF <file>\n"
       "  -MT <target>  Set name of the target\n"
@@ -89,7 +90,6 @@ void print_usage(const std::string& program_name)
       "Unsupported options:\n"
       "  -M            Add system headers to dependency list\n"
       "  -MD <file>    Equivalent to -M -MF <file>\n"
-      "  -MG           Add missing headers to dependency list\n"
       "  -MP           Add phony target for each dependency other than main file\n"
       "  -MQ <target>  Same as -MT <traget> but quote characters special to make\n";
 }
@@ -132,25 +132,34 @@ std::vector<std::string> get_includes(const std::string& file_name)
 }
 
 /// returns files existing in `dir'
-std::vector<std::string> get_existing(const std::string& dir,
-                                      const std::vector<std::string>& files)
+template <class Predicate>
+std::vector<std::string> filter(const std::string& dir,
+                                const std::vector<std::string>& files,
+                                const Predicate& pred)
 {
    std::vector<std::string> existing_files;
 
    for (std::vector<std::string>::const_iterator it = files.begin(),
            end = files.end(); it != end; ++it) {
       const std::string file_with_path(dir.empty() || dir == "." ? *it : dir + '/' + *it);
-      if (file_exists(file_with_path))
+      if (pred(file_with_path))
          existing_files.push_back(file_with_path);
    }
 
    return existing_files;
 }
 
+struct File_exists : std::unary_function<std::string, bool> {
+   bool operator()(const std::string& file_name) const {
+      return file_exists(file_name);
+   }
+};
+
 /// search recursively for include statments in `file_name'
 /// taking into account only directories given in `paths'
 std::vector<std::string> search_includes(const std::string& file_name,
                                          const std::vector<std::string>& paths,
+                                         bool ignore_non_existing = true,
                                          unsigned max_depth = 10)
 {
    if (max_depth == 0)
@@ -163,7 +172,7 @@ std::vector<std::string> search_includes(const std::string& file_name,
    std::vector<std::string> existing;
    for (std::vector<std::string>::const_iterator it = paths.begin(),
            end = paths.end(); it != end; ++it) {
-      const std::vector<std::string> existing_in_path(get_existing(*it, includes));
+      const std::vector<std::string> existing_in_path(filter(*it, includes, File_exists()));
       existing.insert(existing.end(), existing_in_path.begin(), existing_in_path.end());
    }
 
@@ -173,6 +182,18 @@ std::vector<std::string> search_includes(const std::string& file_name,
            end = tmp_existing.end(); it != end; ++it) {
       const std::vector<std::string> sub_existing(search_includes(*it, paths, max_depth - 1));
       existing.insert(existing.end(), sub_existing.begin(), sub_existing.end());
+   }
+
+   // search for non-existing headers
+   if (!ignore_non_existing) {
+      std::vector<std::string> non_existing;
+      for (std::vector<std::string>::const_iterator it = paths.begin(),
+              end = paths.end(); it != end; ++it) {
+         const std::vector<std::string> non_existing_in_path(filter(*it, includes, std::not1(File_exists())));
+         non_existing.insert(non_existing.end(), non_existing_in_path.begin(), non_existing_in_path.end());
+      }
+
+      existing.insert(existing.end(), non_existing.begin(), non_existing.end());
    }
 
    return existing;
@@ -189,6 +210,7 @@ int main(int argc, const char* argv[])
    // include paths
    std::vector<std::string> paths(1, ".");
    std::string file_name, target_name, output_file;
+   bool ignore_non_existing = true;
 
    for (int i = 1; i < argc; i++) {
       const std::string arg(argv[i]);
@@ -196,11 +218,15 @@ int main(int argc, const char* argv[])
          paths.push_back(arg.substr(2));
          continue;
       }
+      if (arg == "-MG") {
+         ignore_non_existing = false;
+         continue;
+      }
       if (arg == "-MM") {
          // ignore headers from system directories (default)
          continue;
       }
-      if (arg == "-M" || arg == "-MG" || arg == "-MP") {
+      if (arg == "-M" || arg == "-MP") {
          std::cerr << "Warning: ignoring unsupported option " << arg << '\n';
          continue;
       }
@@ -240,7 +266,7 @@ int main(int argc, const char* argv[])
 
    // search for header inclusions
    std::vector<std::string> dependencies
-      = delete_duplicates(search_includes(file_name, paths));
+      = delete_duplicates(search_includes(file_name, paths, ignore_non_existing));
 
    // add file name to dependency list
    dependencies.insert(dependencies.begin(), file_name);
