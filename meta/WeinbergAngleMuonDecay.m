dffea0dc2b71fd39a809fa62181407732f102af1
Markus Bach <Markus.Bach@web.de>
2015-10-30 17:24:35 +0100

    modify deltaRhoHat2LoopSM
    ensures that previous results of calculate_delta_rho_hat are reproduced
    effect should be of higher order and necessity of this change is questionable

--- meta/WeinbergAngleMuonDecay.m
+++ meta/WeinbergAngleMuonDecay.m
@@ -7,7 +7,7 @@ deltaRHat2LoopSM::usage="";
 
 Begin["`Private`"];
 
-extPars={sinThetaW, rhohat, gfermi, mw, mz, mt, RHO2, deltaRHat1Loop};
+extPars={sinThetaW, rhohat, gfermi, mw, mz, mt, RHO2, deltaRHat1Loop, pizztMZcorrected};
 Do[Format[extPars[[i]],CForm]=Format[ToString[extPars[[i]]],OutputForm],{i,Length[extPars]}];
 
 (*formula according to (C.6) from hep-ph/9606211*)
@@ -16,7 +16,7 @@ deltaRhoHat2LoopSM[]:=Module[{gY, alphaDRbar, xt, hmix, expr, result},
                              alphaDRbar = gY^2 SARAH`leftCoupling^2 / (4 Pi (gY^2 + SARAH`leftCoupling^2));
                              xt = 3 / (8 Pi^2 Sqrt[2]) gfermi mt^2;
                              hmix = (SARAH`HiggsMixingMatrix[0,1] / Sin[ArcTan[FlexibleSUSY`vu/FlexibleSUSY`vd]])^2;
-                             expr = alphaDRbar SARAH`strongCoupling^2/(16 Pi^3 sinThetaW^2)(-2.145 mt^2/mw^2 + 1.262 Log[mt/mz] - 2.24 - 0.85 mz^2/mt^2) + xt^2 hmix RHO2[FlexibleSUSY`M[SARAH`HiggsBoson[0]]/mt] / 3;
+                             expr = (alphaDRbar SARAH`strongCoupling^2/(16 Pi^3 sinThetaW^2)(-2.145 mt^2/mw^2 + 1.262 Log[mt/mz] - 2.24 - 0.85 mz^2/mt^2) + xt^2 hmix RHO2[FlexibleSUSY`M[SARAH`HiggsBoson[0]]/mt] / 3) / (1 + pizztMZcorrected / mz^2);
                              result = Parameters`CreateLocalConstRefs[expr] <> "\n";
                              result = result <> "deltaRhoHat2LoopSM = ";
                              result = result <> CConversion`RValueToCFormString[expr] <> ";";
