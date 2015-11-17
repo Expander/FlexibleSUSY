93491da80cbd724c9a330ca734d85547085ce3ce
Markus Bach <Markus.Bach@web.de>
2015-11-17 17:50:31 +0100

    change rhohat -> rhohat_ratio = rhohat / rhohat_tree
    to use correct formulas; no difference in MSSM because of rhohat_tree = 1

--- meta/WeinbergAngleMuonDecay.m
+++ meta/WeinbergAngleMuonDecay.m
@@ -20,7 +20,7 @@ FindMass[massMatrices_List, particle_] :=
            Return[massExpr[[1]] /. SARAH`Weinberg[] -> SARAH`Weinberg];
           ];
 
-extPars={SINTHETAW, RHOHAT, GFERMI, MW, MZ, MT, RHO2, DELTARHAT1LOOP, PIZZTMZ};
+extPars={SINTHETAW, RHOHATRATIO, GFERMI, MW, MZ, MT, RHO2, DELTARHAT1LOOP, PIZZTMZ};
 Do[Format[extPars[[i]],CForm]=Format[ToString[extPars[[i]]],OutputForm],{i,Length[extPars]}];
 
 (*formula according to (C.6) from hep-ph/9606211*)
@@ -42,7 +42,7 @@ deltaRHat2LoopSM[]:=Module[{gY, alphaDRbar, xt, hmix, expr, result},
                            alphaDRbar = gY^2 SARAH`leftCoupling^2 / (4 Pi (gY^2 + SARAH`leftCoupling^2));
                            xt = 3 / (8 Pi^2 Sqrt[2]) GFERMI MT^2;
                            hmix = (SARAH`HiggsMixingMatrix[0,1] / Sin[ArcTan[FlexibleSUSY`vu/FlexibleSUSY`vd]])^2;
-                           expr = alphaDRbar SARAH`strongCoupling^2/(16 Pi^3 SINTHETAW^2 (1 - SINTHETAW^2))(2.145 MT^2/MZ^2 + 0.575 Log[MT/MZ] - 0.224 - 0.144 MZ^2/MT^2) - xt^2 hmix RHO2[FlexibleSUSY`M[SARAH`HiggsBoson[0]]/MT] (1 - DELTARHAT1LOOP) RHOHAT / 3;
+                           expr = alphaDRbar SARAH`strongCoupling^2/(16 Pi^3 SINTHETAW^2 (1 - SINTHETAW^2))(2.145 MT^2/MZ^2 + 0.575 Log[MT/MZ] - 0.224 - 0.144 MZ^2/MT^2) - xt^2 hmix RHO2[FlexibleSUSY`M[SARAH`HiggsBoson[0]]/MT] (1 - DELTARHAT1LOOP) RHOHATRATIO / 3;
                            result = Parameters`CreateLocalConstRefs[expr] <> "\n";
                            result = result <> "deltaRHat2LoopSM = ";
                            result = result <> CConversion`RValueToCFormString[expr] <> ";";
