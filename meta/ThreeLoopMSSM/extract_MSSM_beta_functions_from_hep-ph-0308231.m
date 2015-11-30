a14451c63ea43962194deec05e95fc187d2f15b6
Alexander Voigt <Alexander.Voigt@desy.de>
2015-11-30 16:02:53 +0100

    adding script to extract 3L MSSM beta function
    from hep-ph/0308231 and http://www.liv.ac.uk/~dij/betas/

---
+++ meta/ThreeLoopMSSM/extract_MSSM_beta_functions_from_hep-ph-0308231.m
@@ -0,0 +1,74 @@
+(*
+   This script extracts the 3-loop MSSM beta-functions from
+   http://www.liv.ac.uk/~dij/betas/allgennb.log .
+
+   Author: Alexander Voigt
+
+   Run it like this:
+
+   math -run "<< extract_MSSM_beta_functions_from_hep-ph-0308231.m; Quit[]"
+*)
+
+betaStr = Import["allgennb.log"];
+
+(* fix syntax error *)
+betaStr = StringReplace[betaStr, "beta_htau^(3)) =" -> "beta_htau^(3) ="];
+    
+(* wrap expressions around brackets *)
+betaStr = StringReplace[betaStr, {"=" -> "= (", ";" -> ");"}];
+
+(* convert symbols into valid Wolfram symbols *)
+betaStr = StringReplace[betaStr,
+   {"_" -> "", "^(1)" -> "1", "^(2)" -> "2", "^(3)" -> "3"}];
+
+matrixPattern = (
+    (WordBoundary ~~ "Ye" ~~ WordBoundary) | "Yec" |
+    (WordBoundary ~~ "Yt" ~~ WordBoundary) | "Ytc" |
+    (WordBoundary ~~ "Yb" ~~ WordBoundary) | "Ybc" |
+    (WordBoundary ~~ "he" ~~ WordBoundary) | "hec" |
+    (WordBoundary ~~ "ht" ~~ WordBoundary) | "htc" |
+    (WordBoundary ~~ "hb" ~~ WordBoundary) | "hbc");
+
+(* convert matrix products to non-commutative products *)
+betaStr = StringReplace[betaStr,
+   {"tr(" ~~ p:(Except[")"]..) ~~ ")" :>
+    "trace[" <> StringReplace[p, "*" -> ","] <> "]",
+    p:(matrixPattern ~~ ("*" ~~ matrixPattern)..) :>
+    "MatMul[" <> StringReplace[p, "*" -> ","] <> "]"}];
+
+(* convert adjoint symbols to SARAH convention *)
+betaStr = StringReplace[betaStr,
+   {"Yec" -> "Adj[Ye]",
+    "Yuc" -> "Adj[Yu]",
+    "Ydc" -> "Adj[Yd]"}];
+
+ToExpression[betaStr];
+
+repl = {
+    a1 -> g1^2,
+    a2 -> g2^2,
+    a3 -> g3^2,
+    k -> 6 Zeta[3],
+    n5 -> 0,
+    n10 -> 0
+};
+
+{bg11     , bg12     , bg13     } /. repl >> "beta_g1.m";
+{bg21     , bg22     , bg23     } /. repl >> "beta_g2.m";
+{bg31     , bg32     , bg33     } /. repl >> "beta_g3.m";
+{betat1   , betat2   , betat3   } /. repl >> "beta_Yu.m";
+{betab1   , betab2   , betab3   } /. repl >> "beta_Yd.m";
+{betae1   , betae2   , betae3   } /. repl >> "beta_Ye.m";
+{betamq1  , betamq2  , betamq3  } /. repl >> "beta_mq2.m";
+{betamt1  , betamt2  , betamt3  } /. repl >> "beta_mu2.m";
+{betamb1  , betamb2  , betamb3  } /. repl >> "beta_md2.m";
+{betamL1  , betamL2  , betamL3  } /. repl >> "beta_ml2.m";
+{betamtau1, betamtau2, betamtau3} /. repl >> "beta_me2.m";
+{betamH21 , betamH22 , betamH23 } /. repl >> "beta_mHu2.m";
+{betamH11 , betamH12 , betamH13 } /. repl >> "beta_mHd2.m";
+{betaht1  , betaht2  , betaht3  } /. repl >> "beta_TYu.m";
+{betahb1  , betahb2  , betahb3  } /. repl >> "beta_TYd.m";
+{betahtau1, betahtau2, betahtau3} /. repl >> "beta_TYe.m";
+{betaM11  , betaM12  , betaM13  } /. repl >> "beta_M1.m";
+{betaM21  , betaM22  , betaM23  } /. repl >> "beta_M2.m";
+{betaM31  , betaM32  , betaM33  } /. repl >> "beta_M3.m";
