da2d4b270c14145ffd399b85fc2105b4f6b949e4
Alexander Voigt <Alexander.Voigt@desy.de>
2015-12-01 21:54:37 +0100

    adding susy traces to make more tests pass

--- test/test_CMSSM_3loop_beta.m
+++ test/test_CMSSM_3loop_beta.m
@@ -24,36 +24,35 @@ FindBetaFunction[lst_List, c_] :=
 
 UniformTraces[] := {
     trace[Adj[x_], y_] :> trace[y, Adj[x]],
-    trace[a_, Adj[b_], x_, Adj[d_]] :> trace[Adj[b], x, Adj[d], a]
+    trace[a_, Adj[b_], x_, Adj[d_]] :> trace[Adj[b], x, Adj[d], a],
+    trace[args__ /; !FreeQ[{args}, SARAH`Tp[_]]] :> trace[Sequence @@ (Reverse[SARAH`Tp /@ {args}])],
+    trace[Adj[Yd], T[Yd], Adj[Yu], Yu] -> trace[Adj[Yu], Yu, Adj[Yd], T[Yd]],
+    trace[md2, Yd, Adj[Yd]] -> trace[Yd, Adj[Yd], md2],
+    trace[me2, Ye, Adj[Ye]] -> trace[Ye, Adj[Ye], me2],
+    trace[mq2, Adj[Yd], Yd] -> trace[Adj[Yd], Yd, mq2],
+    trace[mq2, Adj[Yu], Yu] -> trace[Adj[Yu], Yu, mq2],
+    trace[mu2, Yu, Adj[Yu]] -> trace[Yu, Adj[Yu], mu2],
+    trace[ml2, Adj[Ye], Ye] -> trace[Adj[Ye], Ye, ml2]
 };
 
-Roatations[trace[lst__]] :=
-    Table[trace @@ RotateLeft[{lst},i], {i,1,Length[{lst}]}];
-
-(* calc difference, taking equivalent trace expressions into account *)
-CalcDifference[a_, b_] :=
-    Module[{diff, traces, differences = {}, repls = {}, i},
-           diff = Simplify[Expand[a] - Expand[b]];
-           traces = Cases[diff, trace[__], {0, Infinity}];
-           For[i = 1, i <= Length[traces], i++,
-               AppendTo[repls, (traces[[i]] -> #)& /@ Roatations[traces[[i]]]];
-              ];
-           repls = Outer[List, repls];
-           differences = (Sequence @@ #)& /@ ((Simplify[Expand[a /. #] - Expand[b]])& /@ repls);
-           If[MemberQ[differences, 0], 0, diff]
-          ];
+CalcDifference[a_, b_] := Simplify[Expand[a] - Expand[b]];
 
 TestBetaEquality[lst_, c_, loop_] :=
-    Module[{sa, sm, difference},
+    Module[{sa, sm},
            sm = ThreeLoopMSSM`BetaMSSM[c][[loop]] /. UniformTraces[];
            sa = FindBetaFunction[lst, c][[loop]] /. {
                Kronecker[_,_] :> 1,
                a_[i1,i2] :> a,
                conj[a_] :> a,
-               Tr1[1] -> 0 (* why is this term missing in the JJ result? *)
-           } /. UniformTraces[];
-           difference = CalcDifference[sm, sa];
-           TestEquality[difference, 0];
+               Conj[a_] :> a,
+               Tr1[1] -> 0, (* why is this term missing in the JJ result? *)
+               Tr2[2] -> (mHd2 + mHu2 + trace[ml2] + 3*trace[mq2])/2,
+               Tr2[3] -> (trace[md2] + 2*trace[mq2] + trace[mu2])/2,
+               Tr3[1] -> 0, (* why is this term missing in the JJ result? *)
+               Tr2U1[1,1] -> (g1^2*(3*mHd2 + 3*mHu2 + 2*trace[md2] + 6*trace[me2] +
+                                    3*trace[ml2] + trace[mq2] + 8*trace[mu2]))/10
+           } /. UniformTraces[] /. SARAH`Tp -> SARAH`Adj /. UniformTraces[];
+           TestEquality[CalcDifference[sm, sa], 0];
           ];
 
 For[l = 1, l <= 2, l++,
