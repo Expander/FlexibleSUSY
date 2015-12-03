141588225c9741c2c2cc7329137719aef3576222
Alexander Voigt <Alexander.Voigt@desy.de>
2015-12-03 15:13:02 +0100

    support inclusion of 3L beta function for BMu at the user side

--- meta/FlexibleSUSY.m
+++ meta/FlexibleSUSY.m
@@ -1522,12 +1522,14 @@ AddMSSM3LoopRGEs[] := Module[{
               SARAH`SoftLeftLepton, SARAH`SoftRightLepton,
               Parameters`GetParameterFromDescription["Softbreaking Down-Higgs Mass"],
               Parameters`GetParameterFromDescription["Softbreaking Up-Higgs Mass"] },
-    mu    = { Parameters`GetParameterFromDescription["Mu-parameter"] }
+    mu    = { Parameters`GetParameterFromDescription["Mu-parameter"] },
+    bmu   = { Parameters`GetParameterFromDescription["Bmu-parameter"] }
     },
     SARAH`BetaGauge = AddMSSM3LoopRGE[SARAH`BetaGauge, gauge];
     SARAH`BetaYijk  = AddMSSM3LoopRGE[SARAH`BetaYijk , yuks];
     SARAH`BetaMi    = AddMSSM3LoopRGE[SARAH`BetaMi   , gaugi];
     SARAH`BetaMuij  = AddMSSM3LoopRGE[SARAH`BetaMuij , mu   ];
+    SARAH`BetaBij   = AddMSSM3LoopRGE[SARAH`BetaBij  , bmu  ];
     SARAH`BetaTijk  = AddMSSM3LoopRGE[SARAH`BetaTijk , trili];
     SARAH`Betam2ij  = AddMSSM3LoopRGE[SARAH`Betam2ij , mass2];
     ];
