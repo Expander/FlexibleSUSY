cd6a17d58da670f2b2541cbeec4c112c86d7aad8
Alexander Voigt <Alexander.Voigt@desy.de>
2015-12-20 14:29:24 +0100

    test database entry for amu uncertainty

--- test/test_CMSSM_database.cpp
+++ test/test_CMSSM_database.cpp
@@ -179,6 +179,7 @@ BOOST_AUTO_TEST_CASE( test_CMSSM_read_write )
 {
    Observables obs1, obs2;
    obs1.a_muon_gm2calc = 1e-9;
+   obs1.a_muon_gm2calc_uncertainty = 1e-10;
    softsusy::QedQcd qedqcd1, qedqcd2;
    qedqcd1.setAlpha(softsusy::ALPHA, 1./127.);
    qedqcd1.toMz();
