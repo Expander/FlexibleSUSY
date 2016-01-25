79a16906e810c68030fb3aa5c3c73d4ec484a38e
Markus Bach <Markus.Bach@web.de>
2016-01-25 18:46:02 +0100

    introduce new ExpressWeinbergAngleInTermsOfGaugeCouplings

--- meta/WeinbergAngle.m
+++ meta/WeinbergAngle.m
@@ -76,64 +76,10 @@ couplings.  The equation which could not be solved is: `2`.  I'll use \
 the Standard Model definition `3` instead.";
 
 ExpressWeinbergAngleInTermsOfGaugeCouplings[masses_List] :=
-    Module[{weinbergDef, eqs, reducedEq, solution, smValue},
+    Module[{solution},
            Print["Expressing Weinberg angle in terms of model parameters ..."];
-           (* SM value of the Weinberg angle *)
-           smValue = Simplify[
-               ArcTan[SARAH`hyperchargeCoupling / SARAH`leftCoupling] /.
-               Parameters`ApplyGUTNormalization[]];
-           (* search weinberg angle definition *)
-           weinbergDef = FindWeinbergAngleDef[] /. Parameters`ApplyGUTNormalization[];
-           If[weinbergDef === 0,
-              Print["Warning: Weinberg angle is defined to be zero."];
-              Return[weinbergDef];
-             ];
-           (* Assumption: the masses contain GUT normalized gauge
-              couplings, but the Weinberg angle does not (because we
-              take it directly from SARAH`ParameterDefinitions, where
-              no GUT normalization has been applied so far)*)
-           eqs = {SARAH`Weinberg == weinbergDef,
-                  SARAH`Mass[SARAH`VectorW]^2 == FindMassW[masses],
-                  SARAH`Mass[SARAH`VectorZ]^2 == UnmixedZMass[] /. Parameters`ApplyGUTNormalization[]
-                 };
-           DebugPrint["The 3 equations to determine the Weinberg angle are: ",
-                      eqs];
-           reducedEq = Eliminate[eqs, {SARAH`Mass[SARAH`VectorW],
-                                       SARAH`Mass[SARAH`VectorZ]}];
-           DebugPrint["Elimination of ",
-                      {SARAH`Mass[SARAH`VectorW], SARAH`Mass[SARAH`VectorZ]},
-                      " yields: ", reducedEq];
-           (* Try Standard Model definition first *)
-           (* If[SolvesWeinbergEq[reducedEq, smValue],
-              Return[smValue];,
-              Message[ExpressWeinbergAngleInTermsOfGaugeCouplings::nonSMDef,
-                      InputForm[SARAH`Weinberg == smValue],
-                      InputForm[eqs]];
-             ]; *)
-           DebugPrint["Solving equation for ", SARAH`Weinberg,
-                      " using a time constraint of ",
-                      FlexibleSUSY`FSSolveWeinbergAngleTimeConstraint];
-           Off[Solve::ifun];
-           solution = TimeConstrained[Normal[Solve[(reducedEq /. Cos[SARAH`Weinberg] -> Sqrt[1 - Sin[SARAH`Weinberg]^2]) &&
-                                                   Pi/2 > SARAH`Weinberg > 0 && SARAH`hyperchargeCoupling > 0 && SARAH`leftCoupling > 0, SARAH`Weinberg]],
-                                      FlexibleSUSY`FSSolveWeinbergAngleTimeConstraint, {}];
-           On[Solve::ifun];
-           If[Head[solution] =!= List || solution === {},
-              Message[ExpressWeinbergAngleInTermsOfGaugeCouplings::noSolution,
-                      InputForm[SARAH`Weinberg],
-                      InputForm[reducedEq],
-                      InputForm[SARAH`Weinberg == smValue]];
-              Return[smValue];
-             ];
-           solution = Simplify[solution, Pi/2 > SARAH`Weinberg > 0 && SARAH`hyperchargeCoupling > 0 && SARAH`leftCoupling > 0] [[All, 1, 2]];
-           If[solution === {},
-              Message[ExpressWeinbergAngleInTermsOfGaugeCouplings::noSolution,
-                      InputForm[SARAH`Weinberg],
-                      InputForm[reducedEq],
-                      InputForm[SARAH`Weinberg == smValue]];
-              Return[smValue];
-             ];
-           Sort[solution, ByteCount[#1] < ByteCount[#2]&][[1]]
+           solution = ArcCos[Sqrt[FindMassW[masses] / (UnmixedZMass[] /. Parameters`ApplyGUTNormalization[]) / RhoZero[]]];
+           Simplify[solution]
           ];
 
 extPars={SINTHETAW, RHOHATRATIO, GFERMI, MW, MZ, MT, RHO2, DELTARHAT1LOOP, PIZZTMZ};
