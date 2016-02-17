40b39641320258ed8e47a119887bc36c7ebed0f9
Alexander Voigt <Alexander.Voigt@desy.de>
2016-02-17 10:24:24 +0100

    use local Output/ and sarah/ directory by default

--- utils/load-FlexibleSUSY.m
+++ utils/load-FlexibleSUSY.m
@@ -2,10 +2,21 @@
    This script loads SARAH and FlexibleSUSY.
    Usage:
 
-     Get["load-FlexibleSUSY.m"];
+     Get["utils/load-FlexibleSUSY.m"];
 *)
 
 AppendTo[$Path, FileNameJoin[{Directory[], "meta"}]];
 
 Needs["SARAH`"];
 Needs["FlexibleSUSY`", FileNameJoin[{Directory[], "meta", "FlexibleSUSY.m"}]];
+
+SARAH`SARAH[OutputDirectory] = FileNameJoin[{Directory[], "Output"}];
+
+SARAH`SARAH[InputDirectories] = {
+    FileNameJoin[{Directory[], "sarah"}],
+    ToFileName[{$sarahDir, "Models"}]
+};
+
+Print["Current working directory: ", Directory[]];
+Print["SARAH output directory   : ", SARAH`SARAH[OutputDirectory]];
+Print["SARAH models directories : ", SARAH`SARAH[InputDirectories]];
