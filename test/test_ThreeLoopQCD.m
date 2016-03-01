e6134c01662e534d09161d94d8de8d52fac28f43
Alexander Voigt <Alexander.Voigt@desy.de>
2016-03-01 11:17:48 +0100

    test 2-loop expressions the other way round

--- test/test_ThreeLoopQCD.m
+++ test/test_ThreeLoopQCD.m
@@ -5,7 +5,7 @@ Needs["ThreeLoopQCD`", "ThreeLoopQCD.m"];
 
 Start["SM"];
 
-Print["Testing 1L arxiv:hep-ph/9912391 vs. hep-ph/9803493 ..."];
+Print["Testing 1L arxiv:hep-ph/9912391 vs. arxiv:hep-ph/9803493 ..."];
 
 m1 = M/(1 + 
     h GetDeltaMOverMQCDOneLoopMSbar[TopQuark, FlexibleSUSY`M[TopQuark]]);
@@ -17,7 +17,7 @@ m2 = M (GetMTopMSbarOverMTopPole[{1, 0, 0, 0}] +
 
 TestEquality[Simplify[m1 - m2], 0];
 
-Print["Testing 2L arxiv:hep-ph/9912391 vs. hep-ph/9803493 ..."];
+Print["Testing 2L arxiv:hep-ph/9912391 vs. arxiv:hep-ph/9803493 ..."];
 
 m1 = M/(1 + 
     h GetDeltaMOverMQCDOneLoopMSbar[TopQuark, FlexibleSUSY`M[TopQuark]] + 
@@ -30,5 +30,19 @@ m2 = M (GetMTopMSbarOverMTopPole[{1, 0, 0, 0}] +
         GetMTopMSbarOverMTopPole[{0, 0, 1, 0}])
 
 TestEquality[Simplify[m1 - m2], 0];
+
+Print["Testing 2L arxiv:hep-ph/9803493 vs. arxiv:hep-ph/9912391 ..."];
+
+M1 = m (1 +
+        h   GetDeltaMOverMQCDOneLoopMSbar[TopQuark, FlexibleSUSY`M[TopQuark]] +
+        h^2 GetDeltaMOverMQCDTwoLoopMSbar[TopQuark, FlexibleSUSY`M[TopQuark]]);
+
+M2 = m / (GetMTopMSbarOverMTopPole[{1, 0, 0  , 0}] +
+          GetMTopMSbarOverMTopPole[{0, h, 0  , 0}] +
+          GetMTopMSbarOverMTopPole[{0, 0, h^2, 0}])
+
+M2 = Simplify[Normal[Series[M2, {h, 0, 2}]]];
+
+TestEquality[Simplify[(M1 - M2) /. h -> 1], 0];
 
 PrintTestSummary[];
