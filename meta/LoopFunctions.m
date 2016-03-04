14ce3eb8c53bffd19950bf99e0233e3e9dce77f6
Alexander Voigt <Alexander.Voigt@desy.de>
2016-03-04 14:08:25 +0100

    re-design LoopFunctions` package

--- meta/LoopFunctions.m
+++ meta/LoopFunctions.m
@@ -1,7 +1,8 @@
-(* Global symbols *)
-{A0, B0, B1, B22, B22tilde, C0, D0, D27, F, G, H};
-
 BeginPackage["LoopFunctions`"];
+EndPackage[];
+
+(* loop functions *)
+{A0, B0, B1, B22, B22tilde, C0, D0, D27, F, G, H};
 
 FullMomentum::usage = "Returns loop functions with full momentum
 dependence in BPMZ convention (arxiv:hep-ph/9606211)";
@@ -11,38 +12,38 @@ ZeroMomentum::usage = "Returns loop functions at zero momentum in BPMZ
 
 Delta::usage = "1/eps - \[Gamma]_E + Log[4 Pi]";
 
-Begin["`Private`"];
+Begin["LoopFunctions`Private`"];
 
 ZeroMomentum[] := {
-                                        Global`A0[m_,mu_]               :> A0impl[m,mu],
-    Global`B0[0,m1_,m2_,mu_]          | Global`B0[m1_,m2_,mu_]          :> B0zero[m1,m2,mu],
-    Global`B1[0,m1_,m2_,mu_]          | Global`B1[m1_,m2_,mu_]          :> B1zero[m1,m2,mu],
-    Global`B00[0,m1_,m2_,mu_]         | Global`B00[m1_,m2_,mu_]         :> B22zero[m1,m2,mu],
-    Global`B11[0,m1_,m2_,mu_]         | Global`B11[m1_,m2_,mu_]         :> B11zero[m1,m2,mu],
-    Global`B22[0,m1_,m2_,mu_]         | Global`B22[m1_,m2_,mu_]         :> B22zero[m1,m2,mu],
-    Global`B22tilde[0,m1_,m2_,mu_]    | Global`B22tilde[m1_,m2_,mu_]    :> B22tildezero[m1,m2,mu],
-    Global`C0[0,m1_,m2_,m2_,mu_]      | Global`C0[m1_,m2_,m2_,mu_]      :> C0zero[m1,m2,m3,mu],
-    Global`D0[0,m1_,m2_,m3_,m4_,mu_]  | Global`D0[m1_,m2_,m3_,m4_,mu_]  :> D0zero[m1,m2,m3,m4,mu],
-    Global`D27[0,m1_,m2_,m3_,m4_,mu_] | Global`D27[m1_,m2_,m3_,m4_,mu_] :> D27zero[m1,m2,m3,m4,mu],
-    Global`F[0,m1_,m2_,mu_]           | Global`F[m1_,m2_,mu_]           :> Fzero[m1,m2,mu],
-    Global`G[0,m1_,m2_,mu_]           | Global`G[m1_,m2_,mu_]           :> Gzero[m1,m2,mu],
-    Global`H[0,m1_,m2_,mu_]           | Global`H[m1_,m2_,mu_]           :> Hzero[m1,m2,mu]
+    A0[m_,mu_]                                               :> A0impl[m,mu],
+    B0[0,m1_,m2_,mu_]             | B0[m1_,m2_,mu_]          :> B0zero[m1,m2,mu],
+    B1[0,m1_,m2_,mu_]             | B1[m1_,m2_,mu_]          :> B1zero[m1,m2,mu],
+    B00[0,m1_,m2_,mu_]            | B00[m1_,m2_,mu_]         :> B22zero[m1,m2,mu],
+    B11[0,m1_,m2_,mu_]            | B11[m1_,m2_,mu_]         :> B11zero[m1,m2,mu],
+    B22[0,m1_,m2_,mu_]            | B22[m1_,m2_,mu_]         :> B22zero[m1,m2,mu],
+    B22tilde[0,m1_,m2_,mu_]       | B22tilde[m1_,m2_,mu_]    :> B22tildezero[m1,m2,mu],
+    C0[0,0,m1_,m2_,m2_,mu_]       | C0[m1_,m2_,m2_,mu_]      :> C0zero[m1,m2,m3,mu],
+    D0[0,0,0,m1_,m2_,m3_,m4_,mu_] | D0[m1_,m2_,m3_,m4_,mu_]  :> D0zero[m1,m2,m3,m4,mu],
+    D27[0,m1_,m2_,m3_,m4_,mu_]    | D27[m1_,m2_,m3_,m4_,mu_] :> D27zero[m1,m2,m3,m4,mu],
+    F[0,m1_,m2_,mu_]              | F[m1_,m2_,mu_]           :> Fzero[m1,m2,mu],
+    G[0,m1_,m2_,mu_]              | G[m1_,m2_,mu_]           :> Gzero[m1,m2,mu],
+    H[0,m1_,m2_,mu_]              | H[m1_,m2_,mu_]           :> Hzero[m1,m2,mu]
 };
 
 FullMomentum[] := {
-    Global`A0[m_,mu_]                  :> A0impl[m,mu],
-    Global`B0[p_,m1_,m2_,mu_]          :> B0[p,m1,m2,mu],
-    Global`B1[p_,m1_,m2_,mu_]          :> B1[p,m1,m2,mu],
-    Global`B00[p_,m1_,m2_,mu_]         :> B22[p,m1,m2,mu],
-    Global`B11[p_,m1_,m2_,mu_]         :> B11[p,m1,m2,mu],
-    Global`B22[p_,m1_,m2_,mu_]         :> B22[p,m1,m2,mu],
-    Global`B22tilde[p_,m1_,m2_,mu_]    :> B22tilde[p,m1,m2,mu],
-    Global`C0[p_,m1_,m2_,m2_,mu_]      :> C0[p,m1,m2,m3,mu],
-    Global`D0[p_,m1_,m2_,m3_,m4_,mu_]  :> D0[p,m1,m2,m3,m4,mu],
-    Global`D27[p_,m1_,m2_,m3_,m4_,mu_] :> D27[p,m1,m2,m3,m4,mu],
-    Global`F[p_,m1_,m2_,mu_]           :> F[p,m1,m2,mu],
-    Global`G[p_,m1_,m2_,mu_]           :> G[p,m1,m2,mu],
-    Global`H[p_,m1_,m2_,mu_]           :> H[p,m1,m2,mu]
+    A0[m_,mu_]                          :> A0impl[m,mu],
+    B0[p_,m1_,m2_,mu_]                  :> B0impl[p,m1,m2,mu],
+    B1[p_,m1_,m2_,mu_]                  :> B1impl[p,m1,m2,mu],
+    B00[p_,m1_,m2_,mu_]                 :> B22impl[p,m1,m2,mu],
+    B11[p_,m1_,m2_,mu_]                 :> B11impl[p,m1,m2,mu],
+    B22[p_,m1_,m2_,mu_]                 :> B22impl[p,m1,m2,mu],
+    B22tilde[p_,m1_,m2_,mu_]            :> B22tildeimpl[p,m1,m2,mu],
+    C0[p1_,p2_,m1_,m2_,m2_,mu_]         :> C0impl[p1,p2,m1,m2,m3,mu],
+    D0[p1_,p2_,p3_,m1_,m2_,m3_,m4_,mu_] :> D0impl[p1,p2,p3,m1,m2,m3,m4,mu],
+    D27[p1_,p2_,p3_,m1_,m2_,m3_,m4_,mu_]:> D27impl[p1,p2,p3,m1,m2,m3,m4,mu],
+    F[p_,m1_,m2_,mu_]                   :> Fimpl[p,m1,m2,mu],
+    G[p_,m1_,m2_,mu_]                   :> Gimpl[p,m1,m2,mu],
+    H[p_,m1_,m2_,mu_]                   :> Himpl[p,m1,m2,mu]
 };
 
 (* A0, Eq. (B.5) *)
@@ -55,7 +56,7 @@ B0zero[m1_, m2_, mu_] := If[PossibleZeroQ[m1 - m2],
    ];
 
 (* Eq. (B.6) *)
-B0[p_, m1_, m2_, mu_] :=
+B0impl[p_, m1_, m2_, mu_] :=
     If[PossibleZeroQ[p],
        B0zero[m1,m2,mu],
        B0analytic[p,m1,m2,mu]
@@ -78,10 +79,10 @@ B0integral[p_, m1_, m2_, mu_] :=
           ];
 
 (* Eq. (B.9) *)
-B1[p_, m1_, m2_, mu_] :=
+B1impl[p_, m1_, m2_, mu_] :=
     If[PossibleZeroQ[p],
        B1zero[m1,m2,mu],
-       1/(2 p^2) (A0impl[m2,mu] - A0impl[m1,mu] + (p^2 + m1^2 - m2^2) B0[p,m1,m2])
+       1/(2 p^2) (A0impl[m2,mu] - A0impl[m1,mu] + (p^2 + m1^2 - m2^2) B0impl[p,m1,m2,mu])
       ];
 
 B1zero[m1_, m2_, mu_] :=
@@ -91,26 +92,26 @@ B1zero[m1_, m2_, mu_] :=
             + 1/2 (m1^2 + m2^2)/(m1^2 - m2^2))
       ];
 
-B11[p_, m1_, m2_, mu_] :=
+B11impl[p_, m1_, m2_, mu_] :=
     If[PossibleZeroQ[p],
        B11zero[m1,m2,mu],
        1/(6 p^2) (
            2 A0impl[m2,mu]
-           - 2 m1^2 B0[p,m1,m2,mu]
-           - 4 (p^2 - m2^2 + m1^2) B1[p,m1,m2,mu]
+           - 2 m1^2 B0impl[p,m1,m2,mu]
+           - 4 (p^2 - m2^2 + m1^2) B1impl[p,m1,m2,mu]
            - m1^2 - m2^2 - p^2/3)
       ];
 
 B11zero[m1_, m2_, mu_] := 0;
 
 (* Eq. (B.10), identical to B00[p,m1,m2,mu] *)
-B22[p_, m1_, m2_, mu_] :=
+B22impl[p_, m1_, m2_, mu_] :=
     If[PossibleZeroQ[p],
        B22zero[m1,m2,mu],
        1/6 (1/2 (A0impl[m1,mu] + A0impl[m2,mu])
-            + (m1^2 + m2^2 - p^2/2) B0[p,m1,m2,mu]
+            + (m1^2 + m2^2 - p^2/2) B0impl[p,m1,m2,mu]
             + (m2^2 - m1^2)/(2 p^2) (A0impl[m2,mu] - A0impl[m1,mu]
-                                     - (m2^2 - m1^2) B0[p,m1,m2,mu])
+                                     - (m2^2 - m1^2) B0impl[p,m1,m2,mu])
             + m1^2 + m2^2 - p^2/3)
       ];
 
@@ -161,34 +162,32 @@ D0zero[m1_, m2_, m3_, m4_] := Which[
       m3^2*m4^2*(-2*m2^2 + m3^2 + m4^2)*
        Log[m4^2/m3^2])/((m2^2 - m3^2)^2*(m2^2 - m4^2)^2*(m3^2 - m4^2)),
    True,
-   (C0[m1, m3, m4] - C0[m2, m3, m4])/(m1^2 - m2^2)];
+   (C0impl[m1, m3, m4] - C0impl[m2, m3, m4])/(m1^2 - m2^2)];
 
 (* Eq. (C.22) *)
 D27zero[m1_, m2_, m3_, m4_] :=
-   (m1^2 C0[m1, m3, m4] - m2^2 C0[m2, m3, m4])/(4 (m1^2 - m2^2));
+   (m1^2 C0impl[m1, m3, m4] - m2^2 C0impl[m2, m3, m4])/(4 (m1^2 - m2^2));
 
 (* Eq. (B.11) *)
-F[p_, m1_, m2_, mu_] :=
-    A0impl[m1,mu] - 2 A0impl[m2,mu] - (2 p^2 + 2 m1^2 - m2^2) B0[p,m1,m2,mu];
+Fimpl[p_, m1_, m2_, mu_] :=
+    A0impl[m1,mu] - 2 A0impl[m2,mu] - (2 p^2 + 2 m1^2 - m2^2) B0impl[p,m1,m2,mu];
 
-Fzero[m1_, m2_, mu_] := F[0,m1,m2,mu];
+Fzero[m1_, m2_, mu_] := Fimpl[0,m1,m2,mu];
 
 (* Eq. (B.12) *)
-G[p_, m1_, m2_, mu_] :=
-    (p^2 - m1^2 - m2^2) B0[p,m1,m2,mu] - A0impl[m1,mu] - A0impl[m2,mu];
+Gimpl[p_, m1_, m2_, mu_] :=
+    (p^2 - m1^2 - m2^2) B0impl[p,m1,m2,mu] - A0impl[m1,mu] - A0impl[m2,mu];
 
-Gzero[m1_, m2_, mu_] := G[0,m1,m2,mu];
+Gzero[m1_, m2_, mu_] := Gimpl[0,m1,m2,mu];
 
 (* Eq. (B.13) *)
-H[p_, m1_, m2_, mu_] := 4 B22[p,m1,m2,mu] + G[p,m1,m2,mu];
+Himpl[p_, m1_, m2_, mu_] := 4 B22impl[p,m1,m2,mu] + Gimpl[p,m1,m2,mu];
 
-Hzero[m1_, m2_, mu_] := H[0,m1,m2,mu];
+Hzero[m1_, m2_, mu_] := Himpl[0,m1,m2,mu];
 
 (* Eq. (B.14) *)
-B22tilde[p_, m1_, m2_, mu_] := B22[p,m1,m2,mu] - A0impl[m1,mu]/4 - A0impl[m2,mu]/4;
+B22tildeimpl[p_, m1_, m2_, mu_] := B22impl[p,m1,m2,mu] - A0impl[m1,mu]/4 - A0impl[m2,mu]/4;
 
-B22tildezero[m1_, m2_, mu_] := B22tilde[0,m1,m2,mu];
+B22tildezero[m1_, m2_, mu_] := B22tildeimpl[0,m1,m2,mu];
 
 End[];
-
-EndPackage[];
--- test/test_LoopFunctions.m
+++ test/test_LoopFunctions.m
@@ -3,19 +3,26 @@ Needs["LoopFunctions`", "LoopFunctions.m"];
 
 Print["testing divergences ..."];
 
-loopFunctions = {A0, B0, B1, B22, B22tilde, C0, D0, D27, F, G, H};
+TestEquality[
+    Print["   testing A0 ..."];
+    Coefficient[A0[m,Q] /. FullMomentum[], Delta],
+    Coefficient[A0[m,Q] /. ZeroMomentum[], Delta]
+];
+
+loopFunctions = {B0, B1, B22, B22tilde, F, G, H};
 
 For[i = 1, i <= Length[loopFunctions], i++,
+    Print["   testing ", loopFunctions[[i]], "[p,m,m,Q] ..."];
     TestEquality[
-        Coefficient[loopFunctions[[i]][m,Q] /. LoopFunctions`FullMomentum[], LoopFunctions`Delta],
-        Coefficient[loopFunctions[[i]][m,Q] /. LoopFunctions`ZeroMomentum[], LoopFunctions`Delta]
+        Limit[loopFunctions[[i]][p,m,m,Q] /. FullMomentum[], p -> 0]
+        loopFunctions[[i]][0,m,m,Q] /. ZeroMomentum[]
     ];
    ];
 
 Print["testing B0[] function ..."];
 
 TestEquality[Simplify[
-    (B0[1, 2, 3, 4] /. LoopFunctions`FullMomentum[]) -
+    (B0[1, 2, 3, 4] /. FullMomentum[]) -
     LoopFunctions`Private`B0integral[1, 2, 3, 4]],
     0
 ];
