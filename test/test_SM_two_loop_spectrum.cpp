a186b38d6bc266a31ad50944d0ef368423a65b88
Alexander Voigt <Alexander.Voigt@desy.de>
2016-03-05 12:23:41 +0100

    need to enable 2L threshold corrections in SM spectrum test

--- test/test_SM_two_loop_spectrum.cpp
+++ test/test_SM_two_loop_spectrum.cpp
@@ -22,6 +22,7 @@ BOOST_AUTO_TEST_CASE( test_SM_two_loop_top_pole_mass )
    input.LambdaIN = 0.25;
    SM<Two_scale> m;
    m.do_calculate_sm_pole_masses(true);
+   m.set_thresholds(2);
    setup_SM_const(m, input);
 
    const double mt_pole_input = qedqcd.displayPoleMt();
