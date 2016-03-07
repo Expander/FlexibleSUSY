3d4e8deb19ad321aa119d67bb1af45800fd82366
Alexander Voigt <Alexander.Voigt@desy.de>
2016-03-07 16:04:11 +0100

    running all tests with $BPMZSign = +1/-1

--- test/test_LoopFunctions.m
+++ test/test_LoopFunctions.m
@@ -1,66 +1,75 @@
 Needs["TestSuite`", "TestSuite.m"];
 Needs["LoopFunctions`", "LoopFunctions.m"];
 
-Print["testing p -> 0 limit ..."];
-
 assumptions = m > 0 && m1 > 0 && m2 > 0 && p > 0 && Q > 0;
 
-TestEquality[
-    Print["   testing A0 ..."];
-    Coefficient[A0[m,Q] /. FullMomentum[], Delta],
-    Coefficient[A0[m,Q] /. ZeroMomentum[], Delta]
-];
+RunTests[] := (
+    Print["running all tests with $BPMZSign = ", $BPMZSign];
+    Print["testing p -> 0 limit ..."];
 
-loopFunctions = {B0, B1, B00, B22, B22tilde, F, G, H};
+    TestEquality[
+        Print["   testing A0 ..."];
+        Coefficient[A0[m,Q] /. FullMomentum[], Delta],
+        Coefficient[A0[m,Q] /. ZeroMomentum[], Delta]
+    ];
 
-For[i = 1, i <= Length[loopFunctions], i++,
-    Print["   testing ", loopFunctions[[i]], "[p,m,m,Q] ..."];
-    expr1 = Limit[loopFunctions[[i]][p,m,m,Q] /. FullMomentum[], p -> 0,
-                  Assumptions :> assumptions];
-    expr2 = loopFunctions[[i]][0,m,m,Q] /. ZeroMomentum[];
-    TestEquality[FullSimplify[expr1 - expr2, assumptions], 0];
-   ];
+    loopFunctions = {B0, B1, B00, B22, B22tilde, F, G, H};
 
-Print["testing B0[] function ..."];
+    For[i = 1, i <= Length[loopFunctions], i++,
+        Print["   testing ", loopFunctions[[i]], "[p,m,m,Q] ..."];
+        expr1 = Limit[loopFunctions[[i]][p,m,m,Q] /. FullMomentum[], p -> 0,
+                      Assumptions :> assumptions];
+        expr2 = loopFunctions[[i]][0,m,m,Q] /. ZeroMomentum[];
+        TestEquality[FullSimplify[expr1 - expr2, assumptions], 0];
+       ];
 
-TestEquality[Simplify[
-    (B0[1, 2, 3, 4] /. FullMomentum[]) -
-    LoopFunctions`Private`B0integral[1, 2, 3, 4]],
-    0
-];
+    Print["testing B0[] function ..."];
 
-Print["testing divergences ..."];
+    TestEquality[Simplify[
+        (B0[1, 2, 3, 4] /. FullMomentum[]) -
+        LoopFunctions`Private`B0integral[1, 2, 3, 4]],
+                 0
+    ];
 
-loopFunctions = {
-    A0[m,Q],
-    B0[p,m1,m2,Q],
-    B1[p,m1,m2,Q],
-    B00[p,m1,m2,Q],
-    B11[p,m1,m2,Q],
-    B22[p,m1,m2,Q],
-    B22tilde[p,m1,m2,Q],
-    F[p,m1,m2,Q],
-    G[p,m1,m2,Q],
-    H[p,m1,m2,Q]
-};
+    Print["testing divergences ..."];
 
-For[i = 1, i <= Length[loopFunctions], i++,
-    Print["   testing ", loopFunctions[[i]], " ..."];
-    expr1 = 1/2 Q D[loopFunctions[[i]] /. FullMomentum[], Q];
-    expr2 = loopFunctions[[i]] /. Divergence[] /. Delta -> 1;
-    TestEquality[FullSimplify[expr1 - expr2, assumptions], 0];
-   ];
+    loopFunctions = {
+        A0[m,Q],
+        B0[p,m1,m2,Q],
+        B1[p,m1,m2,Q],
+        B00[p,m1,m2,Q],
+        B11[p,m1,m2,Q],
+        B22[p,m1,m2,Q],
+        B22tilde[p,m1,m2,Q],
+        F[p,m1,m2,Q],
+        G[p,m1,m2,Q],
+        H[p,m1,m2,Q]
+    };
 
-loopFunctions = {
-    C0[p1,p2,m1,m2,m3,Q],
-    D0[p1,p2,p3,m1,m2,m3,m4,Q],
-    D27[p1,p2,p3,m1,m2,m3,m4,Q]
-};
+    For[i = 1, i <= Length[loopFunctions], i++,
+        Print["   testing ", loopFunctions[[i]], " ..."];
+        expr1 = 1/2 Q D[loopFunctions[[i]] /. FullMomentum[], Q];
+        expr2 = loopFunctions[[i]] /. Divergence[] /. Delta -> 1;
+        TestEquality[FullSimplify[expr1 - expr2, assumptions], 0];
+       ];
 
-For[i = 1, i <= Length[loopFunctions], i++,
-    Print["   testing ", loopFunctions[[i]], " ..."];
-    expr2 = loopFunctions[[i]] /. Divergence[];
-    TestEquality[expr2, 0];
-   ];
+    loopFunctions = {
+        C0[p1,p2,m1,m2,m3,Q],
+        D0[p1,p2,p3,m1,m2,m3,m4,Q],
+        D27[p1,p2,p3,m1,m2,m3,m4,Q]
+    };
+
+    For[i = 1, i <= Length[loopFunctions], i++,
+        Print["   testing ", loopFunctions[[i]], " ..."];
+        expr2 = loopFunctions[[i]] /. Divergence[];
+        TestEquality[expr2, 0];
+       ];
+);
+
+$BPMZSign = 1;
+RunTests[];
+
+$BPMZSign = -1;
+RunTests[];
 
 PrintTestSummary[];
