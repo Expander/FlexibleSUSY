1d381b5894a6b8eaf45312d8ca6661fae65c8cfa
Alexander Voigt <Alexander.Voigt@desy.de>
2016-04-07 11:41:03 +0200

    adding test addon which calls TSIL routines

---
+++ addons/test_call_tsil/module.mk
@@ -0,0 +1,106 @@
+DIR          := addons/test_call_tsil
+MODNAME      := test_call_tsil
+
+LIBtest_call_tsil_MK  := $(DIR)/module.mk
+
+# source files
+LIBtest_call_tsil_SRC := \
+		$(DIR)/call_tsil.c
+
+# main()
+EXEtest_call_tsil_SRC := \
+		$(DIR)/run.cpp
+
+# header files
+LIBtest_call_tsil_HDR := \
+		$(DIR)/call_tsil.h
+
+# header files
+LIBtest_call_tsil_HDR :=
+
+LIBtest_call_tsil_OBJ := \
+		$(patsubst %.cpp, %.o, $(filter %.cpp, $(LIBtest_call_tsil_SRC))) \
+		$(patsubst %.c, %.o, $(filter %.c, $(LIBtest_call_tsil_SRC))) \
+		$(patsubst %.f, %.o, $(filter %.f, $(LIBtest_call_tsil_SRC)))
+
+EXEtest_call_tsil_OBJ := \
+		$(patsubst %.cpp, %.o, $(filter %.cpp, $(EXEtest_call_tsil_SRC))) \
+		$(patsubst %.c, %.o, $(filter %.c, $(EXEtest_call_tsil_SRC))) \
+		$(patsubst %.f, %.o, $(filter %.f, $(EXEtest_call_tsil_SRC)))
+
+LIBtest_call_tsil_DEP := \
+		$(LIBtest_call_tsil_OBJ:.o=.d)
+
+EXEtest_call_tsil_DEP := \
+		$(EXEtest_call_tsil_OBJ:.o=.d)
+
+EXEtest_call_tsil_EXE := \
+		$(patsubst %.cpp, %.x, $(filter %.cpp, $(EXEtest_call_tsil_SRC))) \
+		$(patsubst %.f, %.x, $(filter %.f, $(EXEtest_call_tsil_SRC)))
+
+LIBtest_call_tsil     := \
+		$(DIR)/lib$(MODNAME)$(LIBEXT)
+
+LIBtest_call_tsil_INSTALL_DIR := \
+		$(INSTALL_DIR)/$(DIR)
+
+.PHONY:         clean-$(MODNAME) clean-$(MODNAME)-dep clean-$(MODNAME)-obj \
+		 clean-$(MODNAME)-src distclean-$(MODNAME)
+
+clean-$(MODNAME)-dep:
+		-rm -f $(LIBtest_call_tsil_DEP)
+		-rm -f $(EXEtest_call_tsil_DEP)
+
+clean-$(MODNAME)-obj:
+		-rm -f $(LIBtest_call_tsil_OBJ)
+		-rm -f $(EXEtest_call_tsil_OBJ)
+
+# BEGIN: NOT EXPORTED ##########################################
+# remove generated files
+clean-$(MODNAME)-src:
+		@true
+
+clean-$(MODNAME): clean-$(MODNAME)-src
+# END:   NOT EXPORTED ##########################################
+
+clean-$(MODNAME): clean-$(MODNAME)-dep clean-$(MODNAME)-obj
+		-rm -f $(LIBtest_call_tsil)
+		-rm -f $(EXEtest_call_tsil_EXE)
+
+distclean-$(MODNAME): clean-$(MODNAME)
+
+clean-obj::     clean-$(MODNAME)-obj
+
+clean::         clean-$(MODNAME)
+
+distclean::     distclean-$(MODNAME)
+
+ifneq ($(INSTALL_DIR),)
+install-src::
+		install -d $(LIBtest_call_tsil_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(LIBtest_call_tsil_SRC) $(LIBtest_call_tsil_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(LIBtest_call_tsil_HDR) $(LIBtest_call_tsil_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(EXEtest_call_tsil_SRC) $(LIBtest_call_tsil_INSTALL_DIR)
+		$(INSTALL_STRIPPED) $(LIBtest_call_tsil_MK) $(LIBtest_call_tsil_INSTALL_DIR) -m u=rw,g=r,o=r
+endif
+
+$(LIBtest_call_tsil_DEP) $(EXEtest_call_tsil_DEP) $(LIBtest_call_tsil_OBJ) $(EXEtest_call_tsil_OBJ): CPPFLAGS += $(GSLFLAGS) $(EIGENFLAGS) $(BOOSTFLAGS)
+
+ifneq (,$(findstring yes,$(ENABLE_LOOPTOOLS)$(ENABLE_FFLITE)))
+$(LIBtest_call_tsil_DEP) $(EXEtest_call_tsil_DEP) $(LIBtest_call_tsil_OBJ) $(EXEtest_call_tsil_OBJ): CPPFLAGS += $(LOOPFUNCFLAGS)
+endif
+
+ifeq ($(ENABLE_TSIL),yes)
+$(LIBtest_call_tsil_DEP) $(EXEtest_call_tsil_DEP) $(LIBtest_call_tsil_OBJ) $(EXEtest_call_tsil_OBJ): CPPFLAGS += $(TSILFLAGS)
+endif
+
+$(LIBtest_call_tsil): $(LIBtest_call_tsil_OBJ)
+		$(MAKELIB) $@ $^
+
+$(EXEtest_call_tsil_EXE): $(EXEtest_call_tsil_OBJ) $(LIBtest_call_tsil) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))
+		$(CXX) -o $@ $(call abspathx,$^) $(filter -%,$(LOOPFUNCLIBS)) $(GSLLIBS) $(BOOSTTHREADLIBS) $(THREADLIBS) $(LAPACKLIBS) $(BLASLIBS) $(TSILLIBS) $(FLIBS)
+
+ALLDEP += $(LIBtest_call_tsil_DEP) $(EXEtest_call_tsil_DEP)
+ALLSRC += $(LIBtest_call_tsil_SRC) $(EXEtest_call_tsil_SRC)
+ALLLIB += $(LIBtest_call_tsil)
+ALLEXE += $(EXEtest_call_tsil_EXE)
---
+++ addons/test_call_tsil/run.cpp
@@ -0,0 +1,9 @@
+#include "call_tsil.h"
+#include <iostream>
+
+int main()
+{
+   std::cout << "A0(100,100) = " << call_A(100., 100.) << '\n';
+
+   return 0;
+}
