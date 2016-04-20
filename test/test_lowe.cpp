a066fc29377db132243063032b41118233a82f4f
Alexander Voigt <Alexander.Voigt@desy.de>
2016-04-20 16:09:22 +0200

    adding test for new to2() function

--- test/test_lowe.cpp
+++ test/test_lowe.cpp
@@ -2,6 +2,7 @@
 #define BOOST_TEST_MODULE test_lowe
 
 #include <boost/test/unit_test.hpp>
+#include "wrappers.hpp"
 #include "lowe.h"
 
 using namespace softsusy;
@@ -32,16 +33,26 @@ BOOST_AUTO_TEST_CASE( test_toMz_recall )
    QedQcd lowe_Mz;
    lowe_Mz.setPoleMt(173.5);
    lowe_Mz.setAlpha(ALPHAS, 0.118);
-   lowe_Mz.toMz();
-   QedQcd lowe_Mz_save(lowe_Mz);
-
-   BOOST_CHECK(lowe_Mz == lowe_Mz_save);
-
-   BOOST_MESSAGE(lowe_Mz);
+   QedQcd lowe_Mz_new(lowe_Mz);
 
    lowe_Mz.toMz();
+   lowe_Mz_new.to2(lowe_Mz.displayPoleMZ());
 
-   // BOOST_CHECK(lowe_Mz == lowe_Mz_save);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMbMb(), lowe_Mz_new.displayMbMb(), 1e-10);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayAlpha(ALPHA) , lowe_Mz_new.displayAlpha(ALPHA) , 1e-10);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayAlpha(ALPHAS), lowe_Mz_new.displayAlpha(ALPHAS), 1e-10);
+
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mUp)      , lowe_Mz_new.displayMass(mUp)      , 5e-3);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mDown)    , lowe_Mz_new.displayMass(mDown)    , 5e-3);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mStrange) , lowe_Mz_new.displayMass(mStrange) , 5e-3);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mCharm)   , lowe_Mz_new.displayMass(mCharm)   , 5e-2);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mBottom)  , lowe_Mz_new.displayMass(mBottom)  , 3e-2);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mElectron), lowe_Mz_new.displayMass(mElectron), 2.);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mMuon)    , lowe_Mz_new.displayMass(mMuon)    , 2.);
+   BOOST_CHECK_CLOSE(lowe_Mz.displayMass(mTau)     , lowe_Mz_new.displayMass(mTau)     , 2.);
+
+   BOOST_CHECK_CLOSE(lowe_Mz.displayPoleMb(), lowe_Mz_new.displayPoleMb(), 5e-2);
 
    BOOST_MESSAGE(lowe_Mz);
+   BOOST_MESSAGE(lowe_Mz_new);
 }
