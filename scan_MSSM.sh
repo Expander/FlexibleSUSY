9c98436e1a8475bdadf40f03f9a6777e0d52a8ee
Alexander Voigt <Alexander.Voigt@desy.de>
2016-04-28 13:23:46 +0200

    plot SUSYHD w/ error bands

--- scan_MSSM.sh
+++ scan_MSSM.sh
@@ -120,15 +120,17 @@ set key box bottom right width -4
 set logscale x
 set grid
 
-set style line 1 lt 1 dt 1 lw 2 lc rgb '#FF0000'
-set style line 2 lt 1 dt 2 lw 2 lc rgb '#0000FF'
-set style line 3 lt 1 dt 4 lw 2 lc rgb '#45AD53'
-set style line 4 lt 1 dt 3 lw 2 lc rgb '#FFBF00'
-set style line 5 lt 1 dt 5 lw 2 lc rgb '#FF00FF'
-set style line 6 lt 1 dt 6 lw 2 lc rgb '#00FFFF'
-set style line 7 lt 1 dt 7 lw 2 lc rgb '#000000'
-set style line 8 lt 1 dt 4 lw 2 lc rgb '#00FF00'
-set style line 9 lt 1 dt 1 lw 0 lc rgb '#00FF00'
+set style line  1 lt 1 dt 1 lw 2 lc rgb '#FF0000'
+set style line  2 lt 1 dt 2 lw 2 lc rgb '#0000FF'
+set style line  3 lt 1 dt 4 lw 2 lc rgb '#45AD53'
+set style line  4 lt 1 dt 3 lw 2 lc rgb '#FFBF00'
+set style line  5 lt 1 dt 5 lw 2 lc rgb '#FF00FF'
+set style line  6 lt 1 dt 6 lw 2 lc rgb '#00FFFF'
+set style line  7 lt 1 dt 7 lw 2 lc rgb '#000000'
+set style line  8 lt 1 dt 4 lw 2 lc rgb '#00FF00'
+set style line  9 lt 1 dt 1 lw 0 lc rgb '#00FF00'
+set style line 10 lt 1 dt 2 lw 2 lc rgb '#9C4C17'
+set style line 11 lt 1 dt 1 lw 0 lc rgb '#9C4C17'
 
 set xlabel 'M_S / TeV'
 set ylabel 'M_h / GeV'
@@ -142,7 +144,9 @@ plot [:] [60:140] \
      'scale_SPhenoMSSM_TB-5_2L.dat'        u (\$1/1000):2 t 'SPheno/MSSM 2L' w lines ls 6, \
      'scale_SPhenoMSSM_TB-5_2L_FSlike.dat' u (\$1/1000):2 t 'SPheno/MSSM 2L FS-like' w lines ls 7, \
      'scale_MSSM.dat'                      u (\$1/1000):8 t 'FeynHiggs 2.11.3' w lines ls 8, \
-     'scale_MSSM.dat'                      u (\$1/1000):(\$8-\$9):(\$8+\$9) t 'FeynHiggs uncertainty' w filledcurves ls 9 fs transparent solid 0.3
+     'scale_MSSM.dat'                      u (\$1/1000):(\$8-\$9):(\$8+\$9) t 'FeynHiggs uncertainty' w filledcurves ls 9 fs transparent solid 0.3, \
+     'scale_MSSM.dat'                      u (\$1/1000):10 t 'SUSYHD 1.0.2' w lines ls 10, \
+     'scale_MSSM.dat'                      u (\$1/1000):(\$10-\$11):(\$10+\$11) t 'SUSYHD uncertainty' w filledcurves ls 11 fs transparent solid 0.3
 "
 
 echo "$plot_scale" | gnuplot
