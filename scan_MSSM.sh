1fe655a8be424f6f507f5d1bee7804d4a69f5b6e
Alexander Voigt <Alexander.Voigt@desy.de>
2016-05-20 12:23:14 +0200

    plot MSSM SG with yt(MS)

--- scan_MSSM.sh
+++ scan_MSSM.sh
@@ -22,18 +22,22 @@ set style line  8 lt 1 dt 4 lw 2 lc rgb '#00FF00'
 set style line  9 lt 1 dt 1 lw 0 lc rgb '#00FF00'
 set style line 10 lt 1 dt 2 lw 2 lc rgb '#9C4C17'
 set style line 11 lt 1 dt 1 lw 0 lc rgb '#9C4C17'
+set style line 12 lt 1 dt 8 lw 1 lc rgb '#000000' pt 1
+set style line 13 lt 1 dt 9 lw 1 lc rgb '#000000' pt 2
 
 set xlabel 'M_S / TeV'
 set ylabel 'M_h / GeV'
 
-plot [:] [60:140] \
+plot [0.091:] [60:140] \
      'scale_MSSM.dat' u (\$1/1000):2 t 'FlexibleSUSY/MSSM-tower' w lines ls 1, \
-     'scale_MSSM.dat' u (\$1/1000):4 t 'FlexibleSUSY/MSSM 2L' w lines ls 3, \
-     'scale_MSSM.dat' u (\$1/1000):5 t 'HSSUSY 2L' w lines ls 2, \
-     'scale_MSSM.dat' u (\$1/1000):6 t 'SOFTSUSY 2L' w lines ls 4, \
-     'scale_MSSM.dat' u (\$1/1000):7 t 'FlexibleSUSY/MSSM 2L SPheno-like' w lines ls 5, \
-     'scale_MSSM.dat' u (\$1/1000):12 t 'SPheno/MSSM 2L' w lines ls 6, \
-     'scale_MSSM.dat' u (\$1/1000):13 t 'SPheno/MSSM 2L FS-like' w lines ls 7, \
+     'scale_MSSM.dat' u (\$1/1000):4 t 'FlexibleSUSY/MSSM' w lines ls 3, \
+     'scale_MSSM.dat' u (\$1/1000):5 t 'FlexibleSUSY/HSSUSY' w lines ls 2, \
+     'scale_MSSM.dat' u (\$1/1000):6 t 'SOFTSUSY 3.6.2' w lines ls 4, \
+     'scale_MSSM.dat' u (\$1/1000):7 t 'FlexibleSUSY/MSSM SPheno-like' w lines ls 5, \
+     'scale_MSSM.dat' u (\$1/1000):12 t 'SPheno/MSSM' w lines ls 6, \
+     'scale_MSSM.dat' u (\$1/1000):13 t 'SPheno/MSSM FS-like' w lines ls 7, \
+     'scale_MSSM.dat' u (\$1/1000):14 t 'FlexibleSUSY/MSSM yt(MS)' w points ls 12, \
+     'scale_MSSM.dat' u (\$1/1000):15 t 'FlexibleSUSY/MSSM yt(MS) SPheno-like' w points ls 13, \
      'scale_MSSM.dat' u (\$1/1000):8 t 'FeynHiggs 2.11.3' w lines ls 8, \
      'scale_MSSM.dat' u (\$1/1000):(\$8-\$9):(\$8+\$9) t 'FeynHiggs uncertainty' w filledcurves ls 9 fs transparent solid 0.3, \
      'scale_MSSM.dat' u (\$1/1000):10 t 'SUSYHD 1.0.2' w lines ls 10, \
