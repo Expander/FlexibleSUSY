554c4f9e946688e08180846671aa65b6a1ba59da
Alexander Voigt <Alexander.Voigt@desy.de>
2016-05-24 12:31:52 +0200

    plot uncertainty bands

--- scan_MSSM_xt.sh
+++ scan_MSSM_xt.sh
@@ -116,49 +116,78 @@ echo "$slha_templ_delta_high" | \
         --output=Xtt[],MASS[25] \
         | tee xt_MSSMtower_TB-${TB}_delta_high.dat
 
-paste xt_MSSMtower_TB-${TB}_scale_uncertainty.dat \
+printf "# %14s %16s\n" \
+       "MS" "Mh" \
+       > xt_MSSMtower_TB-${TB}_Mh.dat
+
+awk '{ if ($1 !~ "#") { printf "%16s %16s\n", $1, $2 } }' xt_TB-${TB}_MS-${MS}.dat \
+    >> xt_MSSMtower_TB-${TB}_Mh.dat
+
+paste xt_MSSMtower_TB-${TB}_Mh.dat \
+      xt_MSSMtower_TB-${TB}_scale_uncertainty.dat \
       xt_MSSMtower_TB-${TB}_Qmatch_uncertainty.dat \
       xt_MSSMtower_TB-${TB}_delta_low.dat \
       xt_MSSMtower_TB-${TB}_delta_high.dat \
       > xt_MSSMtower_TB-${TB}.dat.$$
 
-printf "# %14s %16s %16s %16s %16s %16s %16s %16s\n" \
-       "MS" "Q uncert." "MS" "Qmatch uncert." "MS" "lambda(2L) low" "MS" "lambda(2L) high" \
+printf "# %14s %16s %16s %16s %16s %16s %16s %16s %16s %16s\n" \
+       "MS" "Mh" \
+       "MS" "Q uncert." \
+       "MS" "Qmatch uncert." \
+       "MS" "lambda(2L) low" \
+       "MS" "lambda(2L) high" \
        > xt_MSSMtower_TB-${TB}_uncertainties.dat
 
 cat xt_MSSMtower_TB-${TB}.dat.$$ >> xt_MSSMtower_TB-${TB}_uncertainties.dat
 
     plot_scale="
-set terminal pdfcairo
+set terminal pdfcairo enhanced size 5in,4in
 set output 'xt_TB-${TB}_MS-${MS}.pdf'
-set key box top center width -2
+set key box top center width -2 at graph 0.5, graph 1.4 opaque
+set border back
 set grid
+set tmargin 8
 
 set style line 1 lt 1 dt 1 lw 2 lc rgb '#FF0000'
 set style line 2 lt 1 dt 2 lw 2 lc rgb '#0000FF'
 set style line 3 lt 1 dt 4 lw 2 lc rgb '#45AD53'
 set style line 4 lt 1 dt 3 lw 2 lc rgb '#FFBF00'
 set style line 5 lt 1 dt 5 lw 2 lc rgb '#FF00FF'
-set style line 6 lt 1 dt 6 lw 1 lc rgb '#00FFFF'
+set style line 6 lt 1 dt 6 lw 2 lc rgb '#00FFFF'
 set style line 7 lt 1 dt 1 lw 1 lc rgb '#000000'
 set style line 8 lt 1 dt 4 lw 2 lc rgb '#00FF00'
 set style line 9 lt 1 dt 1 lw 0 lc rgb '#00FF00'
 set style line 10 lt 1 dt 2 lw 2 lc rgb '#9C4C17'
 set style line 11 lt 1 dt 1 lw 0 lc rgb '#9C4C17'
+set style line 12 lt 1 dt 8 lw 1 lc rgb '#000000' pt 1
+set style line 13 lt 1 dt 9 lw 1 lc rgb '#000000' pt 2
+set style line 14 lt 1 dt 10 lw 2 lc rgb '#FF00FF' pt 3
 
 set xlabel 'X_t / M_S'
 set ylabel 'M_h / GeV'
 
+min(x,y) = x < y ? x : y
+max(x,y) = x < y ? y : x
+
 filename = 'xt_TB-${TB}_MS-${MS}.dat'
+filename2 = 'xt_MSSMtower_TB-${TB}_uncertainties.dat'
 
 plot [:] [:] \
      filename u 1:2 t 'FS/MSSM-tower' w lines ls 1, \
      filename u 1:4 t 'FS/MSSM' w lines ls 3, \
      filename u 1:5 t 'FS/HSSUSY' w lines ls 2, \
-     filename u 1:6 t 'SOFTSUSY 3.6.2' w lines ls 4, \
-     filename u 1:7 t 'FS/MSSM SPheno-like' w lines ls 5, \
-     filename u 1:8 t 'FeynHiggs 2.11.3' w lines ls 8, \
-     filename u 1:10 t 'SUSYHD 1.0.2' w lines ls 10
+     filename u 1:12 t 'SPheno 3.3.8' w lines ls 6, \
+     filename u 1:14 t 'FS/MSSM m_t(M_S)' w points ls 12, \
+     filename u 1:15 t 'FS/MSSM m_t(M_S) SPheno-like' w points ls 13, \
+     filename u 1:16 t 'FS/MSSM-tower (0L y_t(M_S))' w lines ls 14, \
+     filename2 u 1:(\$2-\$4/2):(\$2+\$4/2) t '{/Symbol D} Q' w filledcurves ls 7 fs transparent solid 0.3, \
+     filename2 u 1:(\$2-\$6/2):(\$2+\$6/2) t '{/Symbol D} Q_{match}' w filledcurves ls 5 fs transparent solid 0.3, \
+     filename2 u 1:(min(\$8,\$10)):(max(\$8,\$10)) t '{/Symbol D} {/Symbol l}^{(2)}' w filledcurves ls 10 fs transparent solid 0.3
+
+#      filename u 1:6 t 'SOFTSUSY 3.6.2' w lines ls 4, \
+#      filename u 1:7 t 'FS/MSSM SPheno-like' w lines ls 5, \
+#      filename u 1:8 t 'FeynHiggs 2.11.3' w lines ls 8, \
+#      filename u 1:10 t 'SUSYHD 1.0.2' w lines ls 10
 "
 
 echo "$plot_scale" | gnuplot
