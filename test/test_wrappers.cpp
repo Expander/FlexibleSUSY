e45f8d89c19c758740190018892cac3b8981f1ae
Dylan Harries <dylan.harries@adelaide.edu.au>
2016-05-25 11:40:12 +0930

    Fix whitespace

--- test/test_wrappers.cpp
+++ test/test_wrappers.cpp
@@ -80,60 +80,60 @@ DoubleMatrix random_real_matrix(int n, int m)
 
     DoubleMatrix r(n, m);
     for (int i = 1; i <= n; i++)
-	for (int j = 1; j <= n; j++)
-	    r(i, j) = o1(generator);
+        for (int j = 1; j <= n; j++)
+            r(i, j) = o1(generator);
     return r;
 }
 
 BOOST_AUTO_TEST_CASE(test_svd)
 {
     for (int n = 2; n <= 6; n++) {
-	DoubleMatrix  m(n,n);
-	ComplexMatrix u(n,n);
-	ComplexMatrix v(n,n);
-	DoubleVector  s(n);
-	ComplexMatrix diag(n,n);
+        DoubleMatrix  m(n,n);
+        ComplexMatrix u(n,n);
+        ComplexMatrix v(n,n);
+        DoubleVector  s(n);
+        ComplexMatrix diag(n,n);
 
-	for (int count = 100; count; count--) {
-	    m = random_real_matrix(n,n);
-	    if (n == 2)
-		Diagonalize2by2(m, u, v, s);
-	    else
-		Diagonalize(m, u, v, s);
-	    diag = u.complexConjugate() * m * v.hermitianConjugate();
+        for (int count = 100; count; count--) {
+            m = random_real_matrix(n,n);
+            if (n == 2)
+                Diagonalize2by2(m, u, v, s);
+            else
+                Diagonalize(m, u, v, s);
+            diag = u.complexConjugate() * m * v.hermitianConjugate();
 
-	    for (int i = 1; i <= s.displayEnd(); i++)
-		BOOST_CHECK(s(i) >= 0);
-	    for (int i = 1; i <= diag.displayCols(); i++)
-		for (int j = 1; j <= diag.displayRows(); j++)
-		    BOOST_CHECK_SMALL(abs(diag(i,j) - (i==j?s(i):0)), 1e-13);
-	}
+            for (int i = 1; i <= s.displayEnd(); i++)
+                BOOST_CHECK(s(i) >= 0);
+            for (int i = 1; i <= diag.displayCols(); i++)
+                for (int j = 1; j <= diag.displayRows(); j++)
+                    BOOST_CHECK_SMALL(abs(diag(i,j) - (i==j?s(i):0)), 1e-13);
+        }
     }
 }
 
 BOOST_AUTO_TEST_CASE(test_symmetric)
 {
     for (int n = 2; n <= 6; n++) {
-	DoubleMatrix  m(n,n);
-	ComplexMatrix u(n,n);
-	DoubleVector  s(n);
-	ComplexMatrix diag(n,n);
+        DoubleMatrix  m(n,n);
+        ComplexMatrix u(n,n);
+        DoubleVector  s(n);
+        ComplexMatrix diag(n,n);
 
-	for (int count = 100; count; count--) {
-	    m = random_real_matrix(n,n);
-	    m.symmetrise();
-	    if (n == 2)
-		Diagonalize2by2(m, u, s);
-	    else
-		Diagonalize(m, u, s);
-	    diag = u.complexConjugate() * m * u.hermitianConjugate();
+        for (int count = 100; count; count--) {
+            m = random_real_matrix(n,n);
+            m.symmetrise();
+            if (n == 2)
+                Diagonalize2by2(m, u, s);
+            else
+                Diagonalize(m, u, s);
+            diag = u.complexConjugate() * m * u.hermitianConjugate();
 
-	    for (int i = 1; i <= s.displayEnd(); i++)
-		BOOST_CHECK(s(i) >= 0);
-	    for (int i = 1; i <= diag.displayCols(); i++)
-		for (int j = 1; j <= diag.displayRows(); j++)
-		    BOOST_CHECK_SMALL(abs(diag(i,j) - (i==j?s(i):0)), 1e-13);
-	}
+            for (int i = 1; i <= s.displayEnd(); i++)
+                BOOST_CHECK(s(i) >= 0);
+            for (int i = 1; i <= diag.displayCols(); i++)
+                for (int j = 1; j <= diag.displayRows(); j++)
+                    BOOST_CHECK_SMALL(abs(diag(i,j) - (i==j?s(i):0)), 1e-13);
+        }
     }
 }
 
