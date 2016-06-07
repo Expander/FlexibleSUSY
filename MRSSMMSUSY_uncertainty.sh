3da27b490c6053de6c34be70999252cd58fe8569
Alexander Voigt <Alexander.Voigt@desy.de>
2016-06-07 15:19:59 +0200

    adding scale scan script for the MRSSMSUSY

---
+++ MRSSMMSUSY_uncertainty.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+[ $# -lt 1 ] && {
+    echo "Usage: $0 --slha-input-file=<input-file>"
+    exit 1
+}
+
+input=-
+output_block=MASS
+output_entry=25
+scale_block=SMINPUTS
+scale_entry=0
+
+if test $# -gt 0 ; then
+    while test ! "x$1" = "x" ; do
+        case "$1" in
+            -*=*) optarg=`echo "$1" | sed 's/[-_a-zA-Z0-9]*=//'` ;;
+            *) optarg= ;;
+        esac
+        case $1 in
+            --slha-input-file=*)     input=$optarg ;;
+            *)  echo "Invalid option '$1'. Try $0 --help" ; exit 1 ;;
+        esac
+        shift
+    done
+fi
+
+slha_input=$(cat ${input})
+
+mean_scale=$(echo "$slha_input" | \
+                    awk -f utils/print_slha_block.awk -v block="EXTPAR" | \
+                    awk -f utils/print_slha_block_entry.awk -v entries="0" | tail -n 1)
+
+delta=$(echo "$slha_input" | ./scan_scale_FlexibleSUSY.sh models/MRSSMMSUSY/run_MRSSMMSUSY.x - "${scale_block}[${scale_entry}]" "${output_block}[${output_entry}]" "$mean_scale")
+
+echo "$slha_input"
+cat <<EOF
+Block ${output_block}
+   ${output_entry}  ${delta}
+EOF
