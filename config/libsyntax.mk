355a6f5dcdaf5480f8ca668ccff62bbe9a4035c9
Alexander Voigt <Alexander.Voigt@desy.de>
2016-06-23 14:50:03 +0200

    avoid explicit occurrence of library extension

--- config/libsyntax.mk
+++ config/libsyntax.mk
@@ -1,1 +1,1 @@
-libsyntax = $(foreach name,$(filter %$(2),$(1)),-L$(dir $(name)) -l$(patsubst lib%.a,%,$(notdir $(name)))) $(filter-out %$(2),$(1))
+libsyntax = $(foreach name,$(filter %$(2),$(1)),-L$(dir $(name)) -l$(patsubst lib%$(2),%,$(notdir $(name)))) $(filter-out %$(2),$(1))
