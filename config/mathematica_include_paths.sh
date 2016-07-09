5d521050cc32d57bea7cbcb95d78cd903b25c8f3
Alexander Voigt <Alexander.Voigt@desy.de>
2016-07-09 20:50:31 +0200

    make math kernel command configurable

--- config/mathematica_include_paths.sh
+++ config/mathematica_include_paths.sh
@@ -4,21 +4,25 @@
 # LibraryLink .
 # Author: Alexander Voigt
 
-MATH_CMD=${MATH:-math}
+math_cmd=math
 
-eval `"${MATH_CMD}" -run '
-    Print["sysid=\"", $SystemID, "\""];
-    Print["topdir=\"", $TopDirectory, "\""];
-    Exit[]' < /dev/null | tr '\r' ' ' | tail -2`
+find_math_dirs() {
+    eval `"${math_cmd}" -run '
+       Print["sysid=\"", $SystemID, "\""];
+       Print["topdir=\"", $TopDirectory, "\""];
+       Exit[]' < /dev/null | tr '\r' ' ' | tail -2`
 
-# check whether Cygwin's dlltool can handle 64-bit DLLs
-test "$sysid" = Windows-x86-64 && {
-    ${DLLTOOL:-dlltool} --help | grep x86-64 > /dev/null || sysid=Windows
+    # check whether Cygwin's dlltool can handle 64-bit DLLs
+    test "$sysid" = Windows-x86-64 && {
+        ${DLLTOOL:-dlltool} --help | grep x86-64 > /dev/null || sysid=Windows
+    }
+
+    topdir=`cd "$topdir" ; echo $PWD`
 }
 
-topdir=`cd "$topdir" ; echo $PWD`
-
 get_librarylink_incpath() {
+    find_math_dirs
+
     for p in \
         "$topdir/SystemFiles/IncludeFiles/C" ; do
         test -d "$p" && break
@@ -28,6 +32,8 @@ get_librarylink_incpath() {
 }
 
 get_mathlink_incpath() {
+    find_math_dirs
+
     for p in \
         "$topdir/SystemFiles/Links/MathLink/DeveloperKit/$sysid/CompilerAdditions" \
         "$topdir/SystemFiles/Links/MathLink/DeveloperKit/CompilerAdditions" \
@@ -40,9 +46,15 @@ get_mathlink_incpath() {
 
 if test $# -gt 0 ; then
     while test ! "x$1" = "x" ; do
+        case "$1" in
+            -*=*) optarg=$(echo "$1" | sed 's/[-_a-zA-Z0-9]*=//') ;;
+            *) optarg= ;;
+        esac
+
         case $1 in
             -I)               prepend="-I" ;;
             --librarylink|-l) path="${path} '${prepend}$(get_librarylink_incpath)'" ;;
+            --math-cmd=*)     math_cmd=$optarg ;;
             --mathlink|-m)    path="${path} '${prepend}$(get_mathlink_incpath)'" ;;
             --help|-h)        usage; exit 0 ;;
             *)  echo "Invalid option '$1'. Try $0 --help" ; exit 1 ;;
