6d3b90460dd2a7dcb93db0d05741054793e54b83
Alexander Voigt <Alexander.Voigt@desy.de>
2016-10-03 10:42:29 +0200

    test GM2Calc vs FS 1L

--- test/test_CMSSMNoFV_GM2Calc.sh
+++ test/test_CMSSMNoFV_GM2Calc.sh
@@ -9,7 +9,6 @@ CMSSMNoFV_EXE=$(readlink -f "${BASEDIR}/../models/CMSSMNoFV/run_CMSSMNoFV.x")
 SLHA_IN=$(readlink -f "${BASEDIR}/../models/CMSSMNoFV/LesHouches.in.CMSSMNoFV")
 SLHA_OUT=$(readlink -f "${BASEDIR}/test_CMSSMNoFV_GM2Calc.out.spc")
 print_block="$BASEDIR/../utils/print_slha_block.awk"
-rel_error=0.000000001
 
 [ $("$FSCONFIG" --with-CMSSMNoFV) = yes -a -x ${CMSSMNoFV_EXE} ] || {
     echo "Error: CMSSMNoFV needs to be build!"
@@ -33,6 +32,8 @@ EOF
     exit 1
 }
 
+amu_fs=$(cat "${SLHA_OUT}" | awk -f "$print_block" -v block=FlexibleSUSYLowEnergy | awk '{ if ($1 == 0) print $2 }')
+
 amu_gm2calc_fs=$(cat "${SLHA_OUT}" | awk -f "$print_block" -v block=FlexibleSUSYLowEnergy | awk '{ if ($1 == 1) print $2 }')
 
 amu_gm2calc=$({ cat <<EOF
@@ -49,22 +50,49 @@ EOF
 }
 
 # convert scientific notation to bc friendly notation
+amu_fs=$(echo "${amu_fs}" | sed -e 's/[eE]/\*10\^/')
 amu_gm2calc_fs=$(echo "${amu_gm2calc_fs}" | sed -e 's/[eE]/\*10\^/')
 amu_gm2calc=$(echo "${amu_gm2calc}" | sed -e 's/[eE]/\*10\^/')
 
-diff=$(cat <<EOF | bc $BASEDIR/abs.bc
+### test GM2Calc vs. FlexibleSUSY's embedded GM2Calc
+
+rel_error=0.000000001
+
+diff_gm2calc=$(cat <<EOF | bc $BASEDIR/abs.bc
 scale=10
 abs((abs($amu_gm2calc_fs) - abs($amu_gm2calc)) / ($amu_gm2calc_fs)) < $rel_error
 EOF
     )
 
-if test $diff -ne 1 ; then
+errors=0
+
+if test $diff_gm2calc -ne 1 ; then
     echo "Error: relative difference between"
     echo " $amu_gm2calc_fs and $amu_gm2calc is larger than $rel_error"
     echo "Test status: FAIL"
-    exit 1
-else
-    echo "FlexibleSUSY: amu = $amu_gm2calc_fs"
-    echo "GM2Calc     : amu = $amu_gm2calc"
-    echo "Test status : OK"
+    errors=1
 fi
+
+### test FlexibleSUSY 1L vs. FlexibleSUSY's embedded GM2Calc
+
+rel_error=0.12
+
+diff_gm2calc_fs=$(cat <<EOF | bc $BASEDIR/abs.bc
+scale=10
+abs((abs($amu_gm2calc_fs) - abs($amu_fs)) / ($amu_fs)) < $rel_error
+EOF
+    )
+
+if test $diff_gm2calc_fs -ne 1 ; then
+    echo "Error: relative difference between"
+    echo " $amu_gm2calc_fs and $amu_fs is larger than $rel_error"
+    echo "Test status: FAIL"
+    errors=1
+fi
+
+echo "FlexibleSUSY 1L : amu = $amu_fs"
+echo "embedded GM2Calc: amu = $amu_gm2calc_fs"
+echo "original GM2Calc: amu = $amu_gm2calc"
+echo "Test status     : OK"
+
+exit ${errors}
