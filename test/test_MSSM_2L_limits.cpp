63a0b7a9ac400991d5dd261a6a99b04cab19449d
Alexander Voigt <Alexander.Voigt@desy.de>
2016-10-19 15:42:51 +0200

    avoid s2t == 0 and T1 == T2 in O(at*at) MSSM 2L Higgs tadpoles

--- test/test_MSSM_2L_limits.cpp
+++ test/test_MSSM_2L_limits.cpp
@@ -24,6 +24,13 @@ struct Point {
    double tb = 5;
    double v2 = sqr(245);
    double g3 = sqrt(4*M_PI*0.1184);
+   // for O(at*at) corrections
+   double mb2 = sqr(4.18);
+   double mA2 = sqr(300);
+   double msb12 = sqr(1500);
+   double msb22 = sqr(2500);
+   double sb = 0.0000001;
+   double cb = sqrt (1. - sqr(sb));
 };
 
 // Pietro Slavich implementation of CP-even Mh correction O(at*as)
@@ -68,11 +75,34 @@ Eigen::Matrix<double, 2, 1> calc_tad_at_as_PS(Point p)
    return -result;
 }
 
+// Pietro Slavich implementation of CP-even tadpoles O(at*at)
+Eigen::Matrix<double, 2, 1> calc_tad_at_at_PS(Point p)
+{
+   double t1, t2;
+
+   ddstad_(&p.mt2, &p.mb2, &p.mA2, &p.mst12, &p.mst22, &p.msb12, &p.msb22,
+           &p.st, &p.ct, &p.sb, &p.cb, &p.q2, &p.mu, &p.tb, &p.v2,
+           &t1, &t2);
+
+   Eigen::Matrix<double, 2, 1> result;
+   result << t1, t2;
+
+   return -result;
+}
+
 // FlexibleSUSY wrapper for CP-even tadpoles O(at*as)
 Eigen::Matrix<double, 2, 1> calc_tad_at_as_FS(Point p)
 {
    return mssm_twoloophiggs::tadpole_higgs_2loop_at_as_mssm(
       p.mt2, p.mg, p.mst12, p.mst22, p.st, p.ct, p.q2, p.mu, p.tb, p.v2, p.g3);
+}
+
+// FlexibleSUSY wrapper for CP-even tadpoles O(at*at)
+Eigen::Matrix<double, 2, 1> calc_tad_at_at_FS(Point p)
+{
+   return mssm_twoloophiggs::tadpole_higgs_2loop_at_at_mssm(
+      p.mt2, p.mb2, p.mA2, p.mst12, p.mst22, p.msb12, p.msb22, p.st, p.ct, p.sb, p.cb,
+      p.q2, p.mu, p.tb, p.v2);
 }
 
 // FlexibleSUSY wrapper for CP-even Mh correction O(at*as)
@@ -111,6 +141,26 @@ BOOST_AUTO_TEST_CASE( MSSM_tadpole_at_as_st_0 )
    BOOST_MESSAGE("Limit st -> 0 : " << tad_fs_exact.transpose());
 }
 
+BOOST_AUTO_TEST_CASE( MSSM_tadpole_at_at_st_0 )
+{
+   Point p_close, p_exact;
+   p_close.st = 0.000000001;
+   p_exact.st = 0;
+
+   const auto tad_ps       = calc_tad_at_at_PS(p_close);
+   const auto tad_fs_close = calc_tad_at_at_FS(p_close);
+   const auto tad_fs_exact = calc_tad_at_at_FS(p_exact);
+
+   BOOST_CHECK_EQUAL(tad_ps(0), tad_fs_close(0));
+   BOOST_CHECK_EQUAL(tad_ps(1), tad_fs_close(1));
+
+   BOOST_CHECK_CLOSE(tad_ps(0), tad_fs_exact(0), 5e-5);
+   BOOST_CHECK_CLOSE(tad_ps(1), tad_fs_exact(1), 1e-5);
+
+   BOOST_MESSAGE("Pietro Slavich: " << tad_ps.transpose());
+   BOOST_MESSAGE("Limit st -> 0 : " << tad_fs_exact.transpose());
+}
+
 BOOST_AUTO_TEST_CASE( MSSM_tadpole_at_as_st_0_mst1_eq_mst2 )
 {
    Point p_close, p_exact;
@@ -130,6 +180,30 @@ BOOST_AUTO_TEST_CASE( MSSM_tadpole_at_as_st_0_mst1_eq_mst2 )
 
    BOOST_CHECK_CLOSE(tad_ps(0), tad_fs_exact(0), 1e-3);
    BOOST_CHECK_CLOSE(tad_ps(1), tad_fs_exact(1), 1e-3);
+
+   BOOST_MESSAGE("Pietro Slavich                : " << tad_ps.transpose());
+   BOOST_MESSAGE("Limit st -> 0 and mst1 -> mst2: " << tad_fs_exact.transpose());
+}
+
+BOOST_AUTO_TEST_CASE( MSSM_tadpole_at_at_st_0_mst1_eq_mst2 )
+{
+   Point p_close, p_exact;
+   p_close.st = 0.00000001;
+   p_close.mst12 = sqr(4000);
+   p_close.mst22 = sqr(4000.00001);
+   p_exact.st = 0;
+   p_exact.mst12 = sqr(4000);
+   p_exact.mst22 = sqr(4000);
+
+   const auto tad_ps       = calc_tad_at_at_PS(p_close);
+   const auto tad_fs_close = calc_tad_at_at_FS(p_close);
+   const auto tad_fs_exact = calc_tad_at_at_FS(p_exact);
+
+   BOOST_CHECK_EQUAL(tad_ps(0), tad_fs_close(0));
+   BOOST_CHECK_EQUAL(tad_ps(1), tad_fs_close(1));
+
+   BOOST_CHECK_CLOSE(tad_ps(0), tad_fs_exact(0), 5e-5);
+   BOOST_CHECK_CLOSE(tad_ps(1), tad_fs_exact(1), 1e-5);
 
    BOOST_MESSAGE("Pietro Slavich                : " << tad_ps.transpose());
    BOOST_MESSAGE("Limit st -> 0 and mst1 -> mst2: " << tad_fs_exact.transpose());
