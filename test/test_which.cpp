e34fe43f09537a5599df52710a81a8f259578857
Jae-hyeon Park <jhyeon@gmail.com>
2016-12-29 17:33:47 +0900

    Test laziness of WHICH()

--- test/test_which.cpp
+++ test/test_which.cpp
@@ -6,6 +6,30 @@
 
 using namespace flexiblesusy;
 
+namespace {
+
+int eval_count = 0;
+
+}
+
+int fail(bool really)
+{
+   eval_count++;
+   if (really)
+      BOOST_ERROR(eval_count << ": eager evaluation occurred!");
+   else
+      BOOST_TEST_MESSAGE(eval_count << ": test evaluation");
+   return 0;
+}
+
+BOOST_AUTO_TEST_CASE(test_calls_to_fail)
+{
+   eval_count = 0;
+   fail(false);
+   fail(false);
+   BOOST_CHECK(eval_count == 2);
+}
+
 BOOST_AUTO_TEST_CASE(test_WHICH_1)
 {
    BOOST_CHECK_EQUAL(WHICH(true, 0), 0);
@@ -16,29 +40,30 @@ BOOST_AUTO_TEST_CASE(test_WHICH_2)
    const int i = 1;
 
    BOOST_CHECK_EQUAL(WHICH(i == 1, 1), 1);
-   BOOST_CHECK_EQUAL(WHICH(i == 0, 1), 0);
+   BOOST_CHECK_EQUAL(WHICH(i == 0, (fail(true), 1)), 0);
 }
 
 BOOST_AUTO_TEST_CASE(test_WHICH_4)
 {
    const int i = 0;
 
-   BOOST_CHECK_EQUAL(WHICH(i > 0, 0, true, 1), 1);
+   BOOST_CHECK_EQUAL(WHICH(i > 0, (fail(true), 0), true, 1), 1);
+   BOOST_CHECK_EQUAL(WHICH(i < 1, 0, true, (fail(true), 1)), 0);
 }
 
 BOOST_AUTO_TEST_CASE(test_WHICH_10)
 {
    const int i = 0;
 
-   BOOST_CHECK_EQUAL(WHICH(i > 0, 0,
-                           i > 1, 1,
-                           i > 2, 2,
-                           i > 3, 3,
-                           i > 4, 4,
-                           i > 5, 5,
-                           i > 6, 6,
-                           i > 7, 7,
-                           i > 8, 8,
-                           i > 9, 9,
+   BOOST_CHECK_EQUAL(WHICH(i > 0, (fail(true), 0),
+                           i > 1, (fail(true), 1),
+                           i > 2, (fail(true), 2),
+                           i > 3, (fail(true), 3),
+                           i > 4, (fail(true), 4),
+                           i > 5, (fail(true), 5),
+                           i > 6, (fail(true), 6),
+                           i > 7, (fail(true), 7),
+                           i > 8, (fail(true), 8),
+                           i > 9, (fail(true), 9),
                            true, -1), -1);
 }
