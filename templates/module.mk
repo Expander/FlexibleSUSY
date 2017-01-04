5692a222fed0edbb893a5787a8496b5dcefe4787
Dylan Harries <dylan.harries@adelaide.edu.au>
2017-01-04 13:09:03 +1030

    Add initial template for outer, semi-analytic constraint
    Still to do: neat way to update input scale consistently

--- templates/module.mk
+++ templates/module.mk
@@ -89,10 +89,13 @@ SEMI_ANALYTIC_TEMPLATES := \
 		$(DIR)/semi_analytic_low_scale_constraint.cpp.in \
 		$(DIR)/semi_analytic_model.hpp.in \
 		$(DIR)/semi_analytic_model.cpp.in \
+		$(DIR)/semi_analytic_soft_parameters_constraint.hpp.in \
+		$(DIR)/semi_analytic_soft_parameters_constraint.cpp.in \
 		$(DIR)/semi_analytic_susy_convergence_tester.hpp.in \
 		$(DIR)/semi_analytic_susy_convergence_tester.cpp.in \
 		$(DIR)/semi_analytic_susy_scale_constraint.hpp.in \
 		$(DIR)/semi_analytic_susy_scale_constraint.cpp.in \
+		$(DIR)/soft_parameters_constraint.hpp.in \
 		$(DIR)/susy_convergence_tester.hpp.in
 
 TEMPLATES    := \
