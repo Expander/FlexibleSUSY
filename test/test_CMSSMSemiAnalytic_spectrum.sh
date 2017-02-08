72562ef0c7a1b5d5229af0c9627e01575c96528e
Dylan Harries <dylan.harries@adelaide.edu.au>
2017-02-09 10:15:16 +1030

    Change order of testing the two-scale and semi-analytic solutions
    since the two-scale solver seems to converge to the semi-analytic
    solution, whereas the semi-analytic solver tends to produce
    solutions that are more different to the two-scale solver.

--- test/test_CMSSMSemiAnalytic_spectrum.sh
+++ test/test_CMSSMSemiAnalytic_spectrum.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# This script generates a CMSSM spectrum and writes it to
-# a SLHA file.  The EWSB output parameters from this point are then
-# used as inputs for the CMSSM solved using the semi-analytic solver.
-# After running the semi-analytic CMSSM we compare that the
+# This script generates a CMSSM spectrum using the semi-analytic solver
+# and writes it to a SLHA file.  The EWSB output parameters from this point
+# are then used as inputs for the CMSSM solved using the two-scale solver.
+# After running the two-scale CMSSM we compare that the
 # semi-analytic and two-scale spectrums are the same.
 
 BASEDIR=$(dirname $0)
@@ -12,11 +12,11 @@ print_block="$UTILSDIR/print_slha_block.awk"
 print_block_entry="$UTILSDIR/print_slha_block_entry.awk"
 remove_block="$UTILSDIR/remove_slha_block.awk"
 
-two_scale_slha="$BASEDIR/../model_files/CMSSM/LesHouches.in.CMSSM"
-two_scale_input="$BASEDIR/test_CMSSMSemiAnalytic_spectrum_CMSSM.in.spc"
-two_scale_output="$BASEDIR/test_CMSSMSemiAnalytic_spectrum_CMSSM.out.spc"
+semi_analytic_slha="$BASEDIR/../model_files/CMSSMSemiAnalytic/LesHouches.in.CMSSMSemiAnalytic"
 semi_analytic_input="$BASEDIR/test_CMSSMSemiAnalytic_spectrum.in.spc"
 semi_analytic_output="$BASEDIR/test_CMSSMSemiAnalytic_spectrum.out.spc"
+two_scale_input="$BASEDIR/test_CMSSMSemiAnalytic_spectrum_CMSSM.in.spc"
+two_scale_output="$BASEDIR/test_CMSSMSemiAnalytic_spectrum_CMSSM.out.spc"
 rel_error="1.7e-2"
 
 sed_cmd=`command -v sed`
@@ -73,6 +73,18 @@ remove_extra_blocks() {
     $awk_cmd -f "$remove_block" -v block=EWSBOutputs
 }
 
+sign() {
+    local a=$(echo "$1" | sed -e 's/[eE]+*/*10^/')
+    cat <<EOF | bc -l
+define sign(i) {
+    if (i >= 0) return(1)
+    return(-1)
+}
+
+sign($a)
+EOF
+}
+
 match_input_blocks() {
     if test $# -lt 3; then
         echo "match_input_blocks: Too few arguments"
@@ -80,60 +92,66 @@ match_input_blocks() {
     fi
     _input_file="$1"
     _output_file="$2"
-    _mu_val="$3"
+    _m0_val="$3"
+
+    _mu_val=$($awk_cmd -f "$print_block" -v block=EXTPAR "$_input_file" \
+                  | $awk_cmd -f "$print_block_entry" -v entries=23)
+    _sign_mu=$(sign $_mu_val)
 
     $awk_cmd -f "$print_block" -v block=FlexibleSUSY "$_input_file" \
         | $awk_cmd -f "$remove_block" -v block=FlexibleSUSY -v entry=2 \
              > "$_output_file"
-    echo "    2   2" >> "$_output_file"
+    echo "    2   0" >> "$_output_file"
 
     $awk_cmd -f "$remove_block" -v block=FlexibleSUSY "$_input_file" \
-        | $awk_cmd -f "$remove_block" -v block=MINPAR -v entry=1 \
-        | $awk_cmd -f "$remove_block" -v block=MINPAR -v entry=4 \
+        | $awk_cmd -f "$remove_block" -v block=EXTPAR \
                    >> "$_output_file"
 
-    echo "Block EXTPAR" >> "$_output_file"
-    echo "   23   $_mu_val" >> "$_output_file"
+    echo "   1   $_m0_val" >> "$_output_file"
+    echo "   4   $_sign_mu" >> "$_output_file"
 }
 
-# generate point using two-scale solver
-cp $two_scale_slha $two_scale_input
+# generate point using semi-analytic solver
+cp $semi_analytic_slha $semi_analytic_input
+echo -n "running semi-analytic solver ..."
+$semi_analytic_exe --slha-input-file=$semi_analytic_input \
+                   --slha-output-file=$semi_analytic_output
+echo "done"
+echo "CMSSMSemiAnalytic SLHA input file:  $semi_analytic_input"
+echo "CMSSMSemiAnalytic SLHA output file: $semi_analytic_output"
+
+if test ! -r "$semi_analytic_output"; then
+    echo "Error: generated CMSSMSemiAnalytic spectrum not found: $semi_analytic_output"
+    exit 1
+fi
+
+# remove comments from the semi-analytic output spectrum
+$sed_cmd -i~ -e '/^ *#/d' $semi_analytic_output
+
+# construct matching input point for the two-scale solver
+m0Sq_sol=$(awk -f "$print_block" -v block=EWSBOutputs "$semi_analytic_output" \
+               | awk -f "$print_block_entry" -v entries=2 \
+               | sed -e 's/[eE]+*/*10^/')
+m0_sol=$(echo "scale=20; sqrt($m0Sq_sol)" | bc)
+match_input_blocks $semi_analytic_input $two_scale_input $m0_sol
+
+# generate point using the two-scale solver
 echo -n "running two-scale solver ..."
 $two_scale_exe --slha-input-file=$two_scale_input --slha-output-file=$two_scale_output
 echo "done"
 echo "CMSSM SLHA input file:  $two_scale_input"
 echo "CMSSM SLHA output file: $two_scale_output"
 
-if test ! -r "$two_scale_output"; then
-    echo "Error: generated CMSSM spectrum not found: $two_scale_output"
-    exit 1
-fi
-
-# remove comments from the two-scale output spectrum
+# remove comments from two-scale output spectrum
 $sed_cmd -i~ -e '/^ *#/d' $two_scale_output
-
-# construct matching input point for the semi-analytic solver
-mu_sol=$(awk -f "$print_block" -v block=HMIX "$two_scale_output" \
-             | awk -f "$print_block_entry" -v entries=1)
-match_input_blocks $two_scale_input $semi_analytic_input $mu_sol
-
-# generate point using semi-analytic solver
-echo -n "running semi-analytic solver ..."
-$semi_analytic_exe --slha-input-file=$semi_analytic_input --slha-output-file=$semi_analytic_output
-echo "done"
-echo "CMSSMSemiAnalytic SLHA input file:  $semi_analytic_input"
-echo "CMSSMSemiAnalytic SLHA output file: $semi_analytic_output"
-
-# remove comments from semi-analytic output spectrum
-$sed_cmd -i~ -e '/^ *#/d' $semi_analytic_output
 
 # remove mixing matrix blocks because we don't want to compare objects
 # with phase ambiguities, and input blocks since these do not match
-cp $two_scale_output $two_scale_output~
 cp $semi_analytic_output $semi_analytic_output~
+cp $two_scale_output $two_scale_output~
 
-remove_mixing_and_input_blocks < $two_scale_output~ > $two_scale_output
 remove_mixing_and_input_blocks < $semi_analytic_output~ > $semi_analytic_output
+remove_mixing_and_input_blocks < $two_scale_output~ > $two_scale_output
 
 # also remove additional output blocks present for the semi-analytic
 # solver but not for the two-scale solver
@@ -142,20 +160,20 @@ cp $semi_analytic_output $semi_analytic_output~
 remove_extra_blocks < $semi_analytic_output~ > $semi_analytic_output
 
 echo ""
-echo "comparing files $two_scale_output and $semi_analytic_output ..."
+echo "comparing files $semi_analytic_output and $two_scale_output ..."
 echo "required relative agreement: $rel_error"
 
 diff=`$numdiff_cmd\
  --absolute-tolerance=1.0e-12\
  --relative-tolerance=$rel_error\
- $two_scale_output $semi_analytic_output`
+ $semi_analytic_output $two_scale_output`
 
 diff_without_comments=`echo $diff | $sed_cmd -e '/^ *#/d' | $sed_cmd -e '/^+++/d'`
 
 exit_code=0
 
 if [ -n "$diff_without_comments" ]; then
-    echo "Error: difference between $two_scale_output and $semi_analytic_output larger than $rel_error"
+    echo "Error: difference between $semi_analytic_output and  $two_scale_output larger than $rel_error"
     echo "$diff"
     echo ""
     echo "Test result: FAIL"
