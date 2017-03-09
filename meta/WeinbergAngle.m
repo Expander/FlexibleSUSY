9ecf6c9a34dcd8202fb3cb4dd9a552ca27fa9f69
Markus Bach <Markus.Bach@web.de>
2017-03-09 19:38:07 +0100

    delete unnecessary Return[] statements

--- meta/WeinbergAngle.m
+++ meta/WeinbergAngle.m
@@ -28,7 +28,7 @@ FindMass2[masses_List, particle_] :=
                     " in masses list."];
               Return[0];
              ];
-           Return[TreeMasses`ReplaceDependenciesReverse[massExpr[[1]]]];
+           TreeMasses`ReplaceDependenciesReverse[massExpr[[1]]]
           ];
 
 (*extracts squared tree-level mass of Z boson before mixing with additional Z`*)
@@ -42,7 +42,7 @@ UnmixedZMass2[] :=
            If[Length[submatrix] != 1, Print["Error: Photon-Z mass matrix could not be identified"]; Return[0];];
            mass2Eigenvalues = Eigenvalues[submatrix];
            If[Length[mass2Eigenvalues] != 2 || !MemberQ[mass2Eigenvalues, 0], Print["Error: Determination of UnmixedZMass2 failed"]; Return[0];];
-           Return[Select[mass2Eigenvalues, # =!= 0 &][[1]] /. Parameters`ApplyGUTNormalization[]];
+           Select[mass2Eigenvalues, # =!= 0 &][[1]] /. Parameters`ApplyGUTNormalization[]
           ];
 
 (*extracts squared tree-level mass of W boson before mixing with additional W`*)
@@ -56,7 +56,7 @@ UnmixedWMass2[] :=
            If[Length[submatrix] != 1, Print["Error: W mass matrix could not be identified"]; Return[0];];
            mass2Eigenvalues = Eigenvalues[submatrix];
            If[Length[DeleteDuplicates[mass2Eigenvalues]] != 1, Print["Error: Determination of UnmixedWMass2 failed"]; Return[0];];
-           Return[mass2Eigenvalues[[1]] /. Parameters`ApplyGUTNormalization[]];
+           mass2Eigenvalues[[1]] /. Parameters`ApplyGUTNormalization[]
           ];
 
 (*checks whether the Gell-Mann-Nishijima relation is valid*)
@@ -73,7 +73,7 @@ GellMannNishijimaRelationHolds[] :=
            photonEigenSystem = Eigensystem[photonMassMatrix];
            photonVector = Extract[photonEigenSystem[[2]], Position[photonEigenSystem[[1]], 0]];
            If[!MemberQ[Total[Abs[Part[#, Complement[Range[Length[photonMassMatrix]], BW3pos]] & /@ photonVector], {2}], 0], Print["Error: SM-like photon could not be identified. Please choose another method for the determination of the Weinberg angle."]; Return[False];];
-           Return[True];
+           True
           ];
 
 (*calculates rho_0 from SU(2)_L representations of the Higgs multipletts as in (16) from 0801.1345 [hep-ph]*)
@@ -86,15 +86,15 @@ RhoZero[] :=
            (* extract isospin from SU(2)_left representation and its third component from Gell-Mann-Nishijima formula with given hypercharge and electric charge = 0 *)
            vevlist = vevlist /. {fieldname_Symbol, vevinfo_List, comp1_List, __} :> Flatten[{vevinfo Boole[ReleaseHold[SARAH`getElectricCharge[comp1[[1]]]] == 0], (SA`DimensionGG[fieldname, leftPos] - 1) / 2, -SA`ChargeGG[fieldname, hyperchargePos]}];
            If[!FreeQ[vevlist, None], Print["Error: determination of electric charge did not work"]; Return[0];];
-           Return[Simplify[Plus @@ ((#[[3]]^2 - #[[4]]^2 + #[[3]]) Abs[#[[1]] #[[2]] Sqrt[2]]^2 & /@ vevlist) / Plus @@ (2 #[[4]]^2 Abs[#[[1]] #[[2]] Sqrt[2]]^2 & /@ vevlist),
-                           Element[Alternatives @@ Cases[SARAH`DEFINITION[SARAH`EWSB][SARAH`VEVs][[All, 2, 1]], x_ /; Parameters`IsRealParameter[x]], Reals]]];
+           Simplify[Plus @@ ((#[[3]]^2 - #[[4]]^2 + #[[3]]) Abs[#[[1]] #[[2]] Sqrt[2]]^2 & /@ vevlist) / Plus @@ (2 #[[4]]^2 Abs[#[[1]] #[[2]] Sqrt[2]]^2 & /@ vevlist),
+                    Element[Alternatives @@ Cases[SARAH`DEFINITION[SARAH`EWSB][SARAH`VEVs][[All, 2, 1]], x_ /; Parameters`IsRealParameter[x]], Reals]]
           ];
 
 ExpressWeinbergAngleInTermsOfGaugeCouplings[] :=
     Module[{solution},
            Print["Expressing Weinberg angle in terms of model parameters ..."];
            solution = ArcCos[Sqrt[UnmixedWMass2[] / UnmixedZMass2[] / RhoZero[]]];
-           Return[Simplify[solution]];
+           Simplify[solution]
           ];
 
 extPars={SINTHETAW, RHOHATRATIO, GFERMI, MW, MZ, MT, RHO2, DELTARHAT1LOOP, PIZZTMZ};
@@ -113,7 +113,7 @@ HiggsTopVertices[higgsName_] :=
            higgsVertices = Cases[higgsVertices, {{__, higgsField_}, {coeffPL_, SARAH`PL}, {coeffPR_, SARAH`PR}}
                                  /; ((coeffPL/I //. rule) * Susyno`LieGroups`conj[coeffPL/I //. rule] === (coeffPR/I //. rule) * Susyno`LieGroups`conj[coeffPR/I //. rule])
                                     && !TreeMasses`IsGoldstone[higgsField] :> {higgsField /. List -> Sequence, coeffPL/I}];
-           Return[higgsVertices];
+           higgsVertices
           ];
 
 (*generalize Higgs dependent part of (C.5) and (C.6) in hep-ph/9606211 analogous to (C.9) and (C.10)*)
@@ -126,7 +126,7 @@ HiggsContributions2LoopSM[massMatrices_List] :=
            higgsVEVlist = Cases[Parameters`GetDependenceSPhenoRules[], RuleDelayed[SARAH`VEVSM, repr_] :> repr];
            If[higgsVEVlist === {}, higgsVEVlist = {SARAH`VEVSM};];
            higgsDep = Abs[#[[2]]]^2 RHO2[FlexibleSUSY`M[#[[1]]]/MT] &;
-           Return[Simplify[3 (GFERMI MT higgsVEVlist[[1]] / (8 Pi^2 Sqrt[2]))^2 (Plus @@ (higgsDep /@ HiggsTopVertices[SARAH`HiggsBoson]) - Plus @@ (higgsDep /@ HiggsTopVertices[SARAH`PseudoScalar]))]];
+           Simplify[3 (GFERMI MT higgsVEVlist[[1]] / (8 Pi^2 Sqrt[2]))^2 (Plus @@ (higgsDep /@ HiggsTopVertices[SARAH`HiggsBoson]) - Plus @@ (higgsDep /@ HiggsTopVertices[SARAH`PseudoScalar]))]
           ];
 
 (*formula according to (C.6) from hep-ph/9606211*)
@@ -137,7 +137,7 @@ DeltaRhoHat2LoopSM[massMatrices_List]:=
            expr = (alphaDRbar SARAH`strongCoupling^2/(16 Pi^3 SINTHETAW^2)(-2.145 MT^2/MW^2 + 1.262 Log[MT/MZ] - 2.24 - 0.85 MZ^2/MT^2) + HiggsContributions2LoopSM[massMatrices]) / (1 + PIZZTMZ / MZ^2);
            result = Parameters`CreateLocalConstRefs[expr] <> "\n";
            result = result <> TreeMasses`ExpressionToString[expr, "deltaRhoHat2LoopSM"];
-           Return[result];
+           result
           ];
 
 (*formula according to (C.5) from hep-ph/9606211*)
@@ -148,7 +148,7 @@ DeltaRHat2LoopSM[massMatrices_List]:=
            expr = alphaDRbar SARAH`strongCoupling^2/(16 Pi^3 SINTHETAW^2 (1 - SINTHETAW^2))(2.145 MT^2/MZ^2 + 0.575 Log[MT/MZ] - 0.224 - 0.144 MZ^2/MT^2) - HiggsContributions2LoopSM[massMatrices] (1 - DELTARHAT1LOOP) RHOHATRATIO;
            result = Parameters`CreateLocalConstRefs[expr] <> "\n";
            result = result <> TreeMasses`ExpressionToString[expr, "deltaRHat2LoopSM"];
-           Return[result];
+           result
           ];
 
 (*calculates tree-level value of rhohat parameter from umixed and mixed Z mass as well as RhoZero*)
@@ -160,7 +160,7 @@ RhoHatTree[]:=
            result = Parameters`CreateLocalConstRefs[expr] <> "\n";
            result = result <> "rhohat_tree = ";
            result = result <> CConversion`RValueToCFormString[expr] <> ";";
-           Return[result];
+           result
           ];
 
 
@@ -184,7 +184,7 @@ GenerateDiagramsWave[particle_] :=
            diagrs = diagrs /. (Rule[SARAH`Internal[i_], x_] /; TreeMasses`GetDimension[x] > 1) :> Rule[SARAH`Internal[i], x[{ToExpression["SARAH`gI" <> ToString[i]]}]];
            diagrs = diagrs /. (Rule[SARAH`External[i_], x_] /; TreeMasses`GetDimension[x] > 1) :> Rule[SARAH`External[i], x[{ToExpression["SARAH`gO" <> ToString[i]]}]];
            diagrs = ({{C[SARAH`External[1], SARAH`Internal[1], SARAH`AntiField[SARAH`Internal[2]]]} /. #[[2]], #[[2]]}) & /@ diagrs;
-           Return[diagrs];
+           diagrs
           ];
 
 WaveResult[diagr_List, includeGoldstones_] :=
@@ -197,7 +197,7 @@ WaveResult[diagr_List, includeGoldstones_] :=
            intpartwithindex = Cases[intparticles, _[{_}]];
            Do[result = SARAH`sum[intpartwithindex[[i, 1, 1]], If[includeGoldstones, 1, TreeMasses`GetDimensionStartSkippingGoldstones[intpartwithindex[[i]]]], TreeMasses`GetDimension[intpartwithindex[[i]]], result],
                  {i, Length[intpartwithindex]}];
-           Return[result];
+           result
           ];
 
 DeltaVBwave[includeGoldstones_:False] :=
@@ -206,7 +206,7 @@ DeltaVBwave[includeGoldstones_:False] :=
            electrondiagrs = ExcludeDiagrams[GenerateDiagramsWave[SARAH`Electron], TreeMasses`IsVector];
            result = {WeinbergAngle`DeltaVB[{WeinbergAngle`fswave, {SARAH`gO1}, SARAH`Neutrino}, Plus @@ (WaveResult[#, includeGoldstones] &) /@ neutrinodiagrs],
                      WeinbergAngle`DeltaVB[{WeinbergAngle`fswave, {SARAH`gO1}, SARAH`Electron}, Plus @@ (WaveResult[#, includeGoldstones] &) /@ electrondiagrs]};
-           Return[result];
+           result
           ];
 
 indextype = CConversion`CreateCType[CConversion`ScalarType[CConversion`integerScalarCType]];
@@ -239,7 +239,7 @@ CreateDeltaVBContribution[deltaVBcontri_WeinbergAngle`DeltaVB, vertexRules_List]
            body = body <> "\nreturn result;";
            body = TextFormatting`IndentText[TextFormatting`WrapLines[body]];
            decl = decl <> body <> "}\n";
-           Return[{prototype, decl}];
+           {prototype, decl}
           ];
 
 PrintDeltaVBContributionName[WeinbergAngle`DeltaVB[{WeinbergAngle`fswave, {idx_}, part_}, _]] :=
@@ -258,7 +258,7 @@ CreateDeltaVBContributions[deltaVBcontris_List, vertexRules_List] :=
                prototypes = prototypes <> p;
                defs = defs <> d;
               ];
-           Return[{prototypes, defs}];
+           {prototypes, defs}
           ];
 
 End[];
