db044c6cc209bab00ec1a546dee4d73dc00604f6
Dylan Harries <dylan.harries@adelaide.edu.au>
2017-03-14 13:37:58 +1030

    Add TO DO note regarding semi-analytic solutions for complex parameters
    (not yet encountered since only models with real parameters have so
    far been used)

--- meta/SemiAnalytic.m
+++ meta/SemiAnalytic.m
@@ -445,6 +445,7 @@ GetSoftLinearsSolutions[softLinears_List, boundaryConditionSubs_List, dimOneSoln
            GetLinearSystemSolutions[softLinears, boundaryConditionSubs, extraTerms]
           ];
 
+(* @todo add missing complex conjugates for complex soft parameters *)
 GetSemiAnalyticSolutions[settings_List] :=
     Module[{boundaryValSubs, dimOnePars, dimOneBCs, dimOneSolns = {},
             diracPars, diracBCs, diracSolns = {}, scalarMasses, scalarMassesBCs,
