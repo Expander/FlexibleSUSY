02f6495e23c3dc1b07c79d74055da390139b5382
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-03-24 17:55:33 +0100

    adding 2-loop SQCD corrections to yt^DRbar in the MSSM
    The GiNaC expression in "dmtas2.expr" has been extracted from SOFTSUSY
    3.7.4 by adding the following C++ code snippet into the file
    src/two_loop_thresholds/two_loop_archives/tquark_2loop_strong_pole.gar
    right after the cache has been filled:
    
    if (cache.size() == 1) {
       ofstream out("dmtas2.expr");
       out << cache[0] << endl;
    }
    
    Note: The expression does not include the 2-loop factor 1/(4 Pi)^4 .

---
+++ meta/MSSM/extract_MSSM_Mt_over_mt_from_softsusy.m
@@ -0,0 +1,21 @@
+(* Converts the 2-loop SUSY-QCD corrections O(alpha_s^2) to the DR-bar
+   top Yukawa coupling in the MSSM from GiNaC form to Mathematica
+   form.
+
+   The GiNaC expression in "dmtas2.expr" has been extracted from
+   SOFTSUSY 3.7.4 by adding the following C++ code snippet into the
+   file
+   src/two_loop_thresholds/two_loop_archives/tquark_2loop_strong_pole.gar
+   right after the cache has been filled:
+
+   if (cache.size() == 1) {
+      ofstream out("dmtas2.expr");
+      out << cache[0] << endl;
+   }
+
+   Note: The expression does not include the 2-loop factor 1/(4 Pi)^4 .
+ *)
+
+str = Import["dmtas2.expr", "String"];
+ex  = ToExpression[str, TraditionalForm];
+ex >> "dmtas2.m"
