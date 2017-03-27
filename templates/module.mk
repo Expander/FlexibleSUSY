fe539edd4c0b7b16c3d0e5a21d63d7caf4e4e840
Dylan Harries <dylan.harries@adelaide.edu.au>
2017-03-27 12:39:08 +1030

    Add low-scale variants of semi-analytic initial guesser and spectrum generator
    to allow compilation with OnlyLowEnergyFlexibleSUSY = True.

--- templates/module.mk
+++ templates/module.mk
@@ -93,6 +93,10 @@ SEMI_ANALYTIC_TEMPLATES := \
 		$(DIR)/semi_analytic_high_scale_spectrum_generator.cpp.in \
 		$(DIR)/semi_analytic_low_scale_constraint.hpp.in \
 		$(DIR)/semi_analytic_low_scale_constraint.cpp.in \
+		$(DIR)/semi_analytic_low_scale_initial_guesser.hpp.in \
+		$(DIR)/semi_analytic_low_scale_initial_guesser.cpp.in \
+		$(DIR)/semi_analytic_low_scale_spectrum_generator.hpp.in \
+		$(DIR)/semi_analytic_low_scale_spectrum_generator.cpp.in \
 		$(DIR)/semi_analytic_model.hpp.in \
 		$(DIR)/semi_analytic_model.cpp.in \
 		$(DIR)/semi_analytic_soft_parameters_constraint.hpp.in \
