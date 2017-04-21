d6e3fddcfdcab1fd3110bae737395c32f966f75f
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-04-21 10:30:10 +0200

    adding operator<< to print H3m parameters

--- H3m/H3m_interface.hpp
+++ H3m/H3m_interface.hpp
@@ -2,6 +2,7 @@
 #define H3m_interface_HPP
 
 #include <complex>
+#include <iostream>
 #include <Eigen/Core>
 
 namespace h3m {
@@ -37,6 +38,34 @@ struct Parameters {
    double s2t{};	   ///< sine of 2 times the stop mixing angle
    double s2b{};	   ///< sine of 2 times the sbot mixing angle
 };
+
+inline std::ostream& operator<<(std::ostream& ostr, const Parameters& pars)
+{
+   ostr <<
+      "H3m parameters:\n"
+      "  Q   = " << pars.scale << '\n' <<
+      "  mu  = " << pars.mu << '\n' <<
+      "  g3  = " << pars.g3 << '\n' <<
+      "  vd  = " << pars.vd << '\n' <<
+      "  vu  = " << pars.vu << '\n' <<
+      "  mq2 = " << pars.mq2 << '\n' <<
+      "  md2 = " << pars.md2 << '\n' <<
+      "  mu2 = " << pars.mu2 << '\n' <<
+      "  At  = " << pars.At << '\n' <<
+      "  Ab  = " << pars.Ab << '\n' <<
+      "  MG  = " << pars.MG << '\n' <<
+      "  MW  = " << pars.MW << '\n' <<
+      "  MZ  = " << pars.MZ << '\n' <<
+      "  Mt  = " << pars.Mt << '\n' <<
+      "  Mb  = " << pars.Mb << '\n' <<
+      "  MA  = " << pars.MA << '\n' <<
+      "  MSt = " << pars.MSt.transpose() << '\n' <<
+      "  MSb = " << pars.MSb.transpose() << '\n' <<
+      "  s2t = " << pars.s2t << '\n' <<
+      "  s2b = " << pars.s2b << '\n';
+
+   return ostr;
+}
 
 }	//	h3m
 
