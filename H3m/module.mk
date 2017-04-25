89c2773a097afe94ebccee5a95fcbd261ff4eed5
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-04-25 17:53:11 +0200

    correcting installation of H3m sub-directories

--- H3m/module.mk
+++ H3m/module.mk
@@ -2,54 +2,55 @@ DIR          := H3m
 MODNAME      := H3m
 WITH_$(MODNAME) := yes
 
+LIBH3M_HIERARCHIES_DIR := $(DIR)/hierarchies
+
 LIBH3M_INC := \
-		$(DIR)/hierarchies/h32q2g/sigS12Full.inc \
-		$(DIR)/hierarchies/h32q2g/sigS1Full.inc \
-		$(DIR)/hierarchies/h32q2g/sigS2Full.inc \
-		$(DIR)/hierarchies/h3q22g/sigS12Full.inc \
-		$(DIR)/hierarchies/h3q22g/sigS1Full.inc \
-		$(DIR)/hierarchies/h3q22g/sigS2Full.inc \
-		$(DIR)/hierarchies/h3/sigS12Full.inc \
-		$(DIR)/hierarchies/h3/sigS1Full.inc \
-		$(DIR)/hierarchies/h3/sigS2Full.inc \
-		$(DIR)/hierarchies/h4/sigS12Full.inc \
-		$(DIR)/hierarchies/h4/sigS1Full.inc \
-		$(DIR)/hierarchies/h4/sigS2Full.inc \
-		$(DIR)/hierarchies/h5g1/sigS12Full.inc \
-		$(DIR)/hierarchies/h5g1/sigS1Full.inc \
-		$(DIR)/hierarchies/h5g1/sigS2Full.inc \
-		$(DIR)/hierarchies/h5/sigS12Full.inc \
-		$(DIR)/hierarchies/h5/sigS1Full.inc \
-		$(DIR)/hierarchies/h5/sigS2Full.inc \
-		$(DIR)/hierarchies/h6b2qg2/sigS12Full.inc \
-		$(DIR)/hierarchies/h6b2qg2/sigS1Full.inc \
-		$(DIR)/hierarchies/h6b2qg2/sigS2Full.inc \
-		$(DIR)/hierarchies/h6bq22g/sigS12Full.inc \
-		$(DIR)/hierarchies/h6bq22g/sigS1Full.inc \
-		$(DIR)/hierarchies/h6bq22g/sigS2Full.inc \
-		$(DIR)/hierarchies/h6bq2g2/sigS12Full.inc \
-		$(DIR)/hierarchies/h6bq2g2/sigS1Full.inc \
-		$(DIR)/hierarchies/h6bq2g2/sigS2Full.inc \
-		$(DIR)/hierarchies/h6b/sigS12Full.inc \
-		$(DIR)/hierarchies/h6b/sigS1Full.inc \
-		$(DIR)/hierarchies/h6b/sigS2Full.inc \
-		$(DIR)/hierarchies/h6g2/sigS12Full.inc \
-		$(DIR)/hierarchies/h6g2/sigS1Full.inc \
-		$(DIR)/hierarchies/h6g2/sigS2Full.inc \
-		$(DIR)/hierarchies/h6/sigS12Full.inc \
-		$(DIR)/hierarchies/h6/sigS1Full.inc \
-		$(DIR)/hierarchies/h6/sigS2Full.inc \
-		$(DIR)/hierarchies/h9q2/sigS12Full.inc \
-		$(DIR)/hierarchies/h9q2/sigS1Full.inc \
-		$(DIR)/hierarchies/h9q2/sigS2Full.inc \
-		$(DIR)/hierarchies/h9/sigS12Full.inc \
-		$(DIR)/hierarchies/h9/sigS1Full.inc \
-		$(DIR)/hierarchies/h9/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h32q2g/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h32q2g/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h32q2g/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h3q22g/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h3q22g/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h3q22g/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h3/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h3/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h3/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h4/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h4/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h4/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h5g1/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h5g1/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h5g1/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h5/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h5/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h5/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6b2qg2/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6b2qg2/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6b2qg2/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6bq22g/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6bq22g/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6bq22g/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6bq2g2/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6bq2g2/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6bq2g2/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6b/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6b/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6b/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6g2/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6g2/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6g2/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h6/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h9q2/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h9q2/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h9q2/sigS2Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h9/sigS12Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h9/sigS1Full.inc \
+		$(LIBH3M_HIERARCHIES_DIR)/h9/sigS2Full.inc \
 
 LIBH3M_HDR := \
 		$(DIR)/H3m_interface.hpp \
-		$(DIR)/HierarchyCalculator.hpp \
-		$(LIBH3M_INC)
+		$(DIR)/HierarchyCalculator.hpp
 
 LIBH3M_MK := \
 		$(DIR)/module.mk
@@ -81,6 +82,62 @@ install-src::
 		install -m u=rw,g=r,o=r $(LIBH3M_SRC) $(LIBH3M_INSTALL_DIR)
 		install -m u=rw,g=r,o=r $(LIBH3M_HDR) $(LIBH3M_INSTALL_DIR)
 		install -m u=rw,g=r,o=r $(LIBH3M_MK) $(LIBH3M_INSTALL_DIR)
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h3
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h32q2g
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h3q22g
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h4
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h5
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h5g1
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h6
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h6b
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h6b2qg2
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq22g
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq2g2
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h6g2
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h9
+		install -d $(LIBH3M_INSTALL_DIR)/hierarchies/h9q2
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h32q2g/sigS12Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h32q2g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h32q2g/sigS1Full.inc   $(LIBH3M_INSTALL_DIR)/hierarchies/h32q2g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h32q2g/sigS2Full.inc   $(LIBH3M_INSTALL_DIR)/hierarchies/h32q2g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h3q22g/sigS12Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h3q22g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h3q22g/sigS1Full.inc   $(LIBH3M_INSTALL_DIR)/hierarchies/h3q22g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h3q22g/sigS2Full.inc   $(LIBH3M_INSTALL_DIR)/hierarchies/h3q22g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h3/sigS12Full.inc      $(LIBH3M_INSTALL_DIR)/hierarchies/h3/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h3/sigS1Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h3/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h3/sigS2Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h3/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h4/sigS12Full.inc      $(LIBH3M_INSTALL_DIR)/hierarchies/h4/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h4/sigS1Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h4/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h4/sigS2Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h4/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h5g1/sigS12Full.inc    $(LIBH3M_INSTALL_DIR)/hierarchies/h5g1/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h5g1/sigS1Full.inc     $(LIBH3M_INSTALL_DIR)/hierarchies/h5g1/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h5g1/sigS2Full.inc     $(LIBH3M_INSTALL_DIR)/hierarchies/h5g1/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h5/sigS12Full.inc      $(LIBH3M_INSTALL_DIR)/hierarchies/h5/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h5/sigS1Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h5/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h5/sigS2Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h5/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6b2qg2/sigS12Full.inc $(LIBH3M_INSTALL_DIR)/hierarchies/h6b2qg2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6b2qg2/sigS1Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h6b2qg2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6b2qg2/sigS2Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h6b2qg2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6bq22g/sigS12Full.inc $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq22g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6bq22g/sigS1Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq22g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6bq22g/sigS2Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq22g/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6bq2g2/sigS12Full.inc $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq2g2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6bq2g2/sigS1Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq2g2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6bq2g2/sigS2Full.inc  $(LIBH3M_INSTALL_DIR)/hierarchies/h6bq2g2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6b/sigS12Full.inc     $(LIBH3M_INSTALL_DIR)/hierarchies/h6b/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6b/sigS1Full.inc      $(LIBH3M_INSTALL_DIR)/hierarchies/h6b/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6b/sigS2Full.inc      $(LIBH3M_INSTALL_DIR)/hierarchies/h6b/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6g2/sigS12Full.inc    $(LIBH3M_INSTALL_DIR)/hierarchies/h6g2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6g2/sigS1Full.inc     $(LIBH3M_INSTALL_DIR)/hierarchies/h6g2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6g2/sigS2Full.inc     $(LIBH3M_INSTALL_DIR)/hierarchies/h6g2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6/sigS12Full.inc      $(LIBH3M_INSTALL_DIR)/hierarchies/h6/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6/sigS1Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h6/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h6/sigS2Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h6/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h9q2/sigS12Full.inc    $(LIBH3M_INSTALL_DIR)/hierarchies/h9q2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h9q2/sigS1Full.inc     $(LIBH3M_INSTALL_DIR)/hierarchies/h9q2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h9q2/sigS2Full.inc     $(LIBH3M_INSTALL_DIR)/hierarchies/h9q2/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h9/sigS12Full.inc      $(LIBH3M_INSTALL_DIR)/hierarchies/h9/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h9/sigS1Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h9/
+		install -m u=rw,g=r,o=r $(LIBH3M_HIERARCHIES_DIR)/h9/sigS2Full.inc       $(LIBH3M_INSTALL_DIR)/hierarchies/h9/
 endif
 
 clean-$(MODNAME)-dep:
