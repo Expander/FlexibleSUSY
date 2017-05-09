0cb4042297b1fd1f008011c008a84c94cdb65f79
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-05-09 15:25:04 +0200

    Bugfix: element-wise matrix product for constraints of the form A*B
    In Mathematica A*B represents an element-wise product.  FS should
    interpret expressions of this form in the same way.  Matrix products
    should be written as MatMul[A,B] or A.B .
    
    This fixes a bug in the MSSMatMGUT, where the GUT boundary condition
    has the form (according to SLHA)
    
       T[Yu] = Auij * Yu
    
    where Auij and Yu are matrices and Auij * Yu must be interpreted as
    element-wise product.

--- meta/CConversion.m
+++ meta/CConversion.m
@@ -37,6 +37,8 @@ AbsSqr::usage="";
 AbsSqrt::usage="";
 FSKroneckerDelta::usage="";
 TensorProd::usage="";
+ElementwiseProd::usage = "represents an elemen-wise product of
+ vectors, matrices or tensors";
 
 HaveSameDimension::usage = "Checks if given types have same
 dimension";
@@ -750,6 +752,16 @@ Format[CConversion`TensorProd[HoldPattern[x_],HoldPattern[y_]],CForm] :=
     Format["(" <> ToString[CForm[HoldForm[x]]] <> ")*(" <>
            ToString[CForm[HoldForm[y]]] <> ").transpose()", OutputForm];
 
+Format[CConversion`ElementwiseProd[HoldPattern[fac_]],CForm] :=
+    Format[ToString[CForm[HoldForm[fac]]], OutputForm];
+
+Format[CConversion`ElementwiseProd[HoldPattern[fac__]],CForm] :=
+    Format[
+        "(" <> StringJoin[
+            Riffle[ToString[CForm[HoldForm[#]]]& /@ {fac}, ").cwiseProduct("]
+         ] <> ")"
+        , OutputForm];
+
 (* Finds all Greek symbols in an expression.
    Note: All arguments of Which and If are evaluated.
  *)
@@ -758,6 +770,19 @@ FindGreekSymbols[expr_] :=
           DeleteDuplicates @ Select[
               Cases[expr, x_Symbol | x_Symbol[__] :> x, {0,Infinity}, Heads->True], GreekQ]
          ];
+
+CConversion`ElementwiseProd[] = 1;
+
+(* finds all vectors, matrices and tensors in a given list *)
+FindMatrices[syms_List] :=
+    Select[syms, Length[Parameters`GetParameterDimensions[#]] > 1 &];
+
+FactorElementwiseProd[fac_] :=
+    Module[{facs, mat},
+           facs = If[Head[fac] === Times, List @@ fac, {fac}];
+           mat = FindMatrices[facs];
+           Times[Sequence @@ Complement[facs, mat]] CConversion`ElementwiseProd[Sequence @@ mat]
+          ];
 
 (* Converts an expression to CForm and expands SARAH symbols
  *
@@ -795,6 +820,7 @@ RValueToCFormString[expr_] :=
                     FlexibleSUSY`BETA[l_,p_]     :> FlexibleSUSY`BETA1[l,p] /.
                     Susyno`LieGroups`conj    -> SARAH`Conj //.
                     conjSimplification /.
+                    Times[fac__] :> FactorElementwiseProd[fac] /.
                     SARAH`Delta[a_,a_]       -> 1 /.
                     Power[a_?NumericQ,n_?NumericQ] :> N[Power[a,n]] /.
                     Sqrt[a_?NumericQ]        :> N[Sqrt[a]] /.
