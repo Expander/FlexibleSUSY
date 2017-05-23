c18b71093755f3df61dd00c9f10b681a027b231b
Dylan Harries <dylan.harries@adelaide.edu.au>
2017-05-23 14:43:15 +0930

    Reuse existing routines for performing Runge-Kutta integration
    to avoid code duplication

--- src/basic_rk_integrator.hpp
+++ src/basic_rk_integrator.hpp
@@ -19,10 +19,6 @@
 /**
  * @file basic_rk_integrator.hpp
  * @brief Integration of ODEs by Runge-Kutta
- * @author Ben Allanach, Alexander Voigt
- *
- * The implementation of the Runge-Kutta routines have been derived
- * from SOFTSUSY [hep-ph/0104145, Comp. Phys. Comm. 143 (2002) 305].
  */
 
 #ifndef BASIC_RK_INTEGRATOR_H
@@ -32,8 +28,7 @@
 #include <cmath>
 #include <functional>
 
-#include "error.hpp"
-#include "logger.hpp"
+#include "rk.hpp"
 
 namespace flexiblesusy {
 
@@ -76,54 +71,7 @@ void Basic_rk_stepper<StateType, Derivs>::runge_kutta_step(
    const StateType& y, const StateType& dydx, double x,
    double h, StateType& yout, StateType& yerr, Derivs derivs) const
 {
-   const double a2 = 0.2;
-   const double a3 = 0.3;
-   const double a4 = 0.6;
-   const double a5 = 1.0;
-   const double a6 = 0.875;
-   const double b21 = 0.2;
-   const double b31 = 3.0 / 40.0;
-   const double b32 = 9.0 / 40.0;
-   const double b41 = 0.3;
-   const double b42 = -0.9;
-   const double b43 = 1.2;
-   const double b51 = -11.0 / 54.0;
-   const double b52 = 2.5;
-   const double b53 = -70.0 / 27.0;
-   const double b54 = 35.0 / 27.0;
-   const double b61 = 1631.0 / 55296.0;
-   const double b62 = 175.0 / 512.0;
-   const double b63 = 575.0 / 13824.0;
-   const double b64 = 44275.0 / 110592.0;
-   const double b65 = 253.0 / 4096.0;
-   const double c1 = 37.0 / 378.0;
-   const double c3 = 250.0 / 621.0;
-   const double c4 = 125.0 / 594.0;
-   const double c6 = 512.0 / 1771.0;
-   const double dc5 = -277.00 / 14336.0;
-   const double dc1 = c1 - 2825.0 / 27648.0;
-   const double dc3 = c3 - 18575.0 / 48384.0;
-   const double dc4 = c4 - 13525.0 / 55296.0;
-   const double dc6 = c6 - 0.25;
-
-   StateType ytemp = b21 * h * dydx + y;
-   const StateType ak2 = derivs(x + a2 * h, ytemp);
-
-   // Allowing piece-wise calculating of ytemp for speed reasons
-   ytemp = y + h * (b31 * dydx + b32 * ak2);
-   const StateType ak3 = derivs(x + a3 * h, ytemp);
-
-   ytemp = y + h * (b41 * dydx + b42 * ak2 + b43 * ak3);
-   const StateType ak4 = derivs(x+a4*h,ytemp);
-
-   ytemp = y + h * (b51 * dydx + b52 * ak2 + b53 * ak3 + b54 * ak4);
-   const StateType ak5 = derivs(x + a5 * h, ytemp);
-
-   ytemp = y + h * (b61 * dydx + b62 * ak2 + b63 * ak3 + b64 * ak4 + b65 * ak5);
-   const StateType ak6 = derivs(x + a6 * h, ytemp);
-
-   yout = y + h * (c1 * dydx + c3 * ak3 + c4 * ak4 + c6 * ak6);
-   yerr = h * (dc1 * dydx + dc3 * ak3 + dc4 * ak4 + dc5 * ak5 + dc6 * ak6);
+   rungeKuttaStep(y, dydx, x, h, yout, yerr, derivs);
 }
 
 /**
@@ -145,53 +93,11 @@ void Basic_rk_stepper<StateType, Derivs>::runge_kutta_step(
  */
 template <typename StateType, typename Derivs>
 double Basic_rk_stepper<StateType,Derivs>::step(
-   StateType& y, const StateType& dydx, double *x, double htry,
+   StateType& y, const StateType& dydx, double* x, double htry,
    double eps, const StateType& yscal, Derivs derivs,
    int& max_step_dir) const
 {
-   const double SAFETY = 0.9;
-   const double PGROW = -0.2;
-   const double PSHRNK = -0.25;
-   const double ERRCON = 1.89e-4;
-   const int n = y.size();
-
-   double errmax;
-   double h = htry;
-   StateType yerr(n);
-   StateType ytemp(n);
-
-   for (;;) {
-      runge_kutta_step(y, dydx, *x, h, ytemp, yerr, derivs);
-      errmax = (yerr / yscal).abs().maxCoeff(&max_step_dir);
-      errmax /= eps;
-      if (!std::isfinite(errmax)) {
-#ifdef ENABLE_VERBOSE
-         ERROR("Basic_rk_stepper::step: non-perturbative running at Q = "
-               << std::exp(*x) << " GeV of parameter y(" << max_step_dir
-               << ") = " << y(max_step_dir) << ", dy(" << max_step_dir
-               << ")/dx = " << dydx(max_step_dir));
-#endif
-         throw NonPerturbativeRunningError(std::exp(*x), max_step_dir,
-                                           y(max_step_dir));
-      }
-      if (errmax <= 1.0) break;
-      const double htemp = SAFETY * h * std::pow(errmax, PSHRNK);
-      h = (h >= 0.0 ? std::max(htemp, 0.1 * h) : std::min(htemp, 0.1 * h));
-      if (*x + h == *x) {
-#ifdef ENABLE_VERBOSE
-         ERROR("At Q = " << std::exp(*x) << " GeV "
-               "stepsize underflow in Basic_rk_stepper::step in parameter y("
-               << max_step_dir << ") = " << y(max_step_dir) << ", dy("
-               << max_step_dir << ")/dx = " << dydx(max_step_dir));
-#endif
-         throw NonPerturbativeRunningError(std::exp(*x), max_step_dir,
-                                           y(max_step_dir));
-      }
-   }
-   *x += h;
-   y = ytemp;
-
-   return errmax > ERRCON ? SAFETY * h * std::pow(errmax,PGROW) : 5.0 * h;
+   return odeStepper(y, dydx, x, htry, eps, yscal, derivs, max_step_dir);
 }
 
 /**
@@ -244,50 +150,16 @@ void Basic_rk_integrator<StateType, Derivs, Stepper>::operator()(
 {
    const double guess = (start - end) * 0.1; // first step size
    const double hmin = (start - end) * tolerance * 1.0e-5;
-   const int nvar = ystart.size();
-   const double TINY = 1.0e-16;
+   const auto rkqs = [this] (
+      StateType& y, const StateType& dydx, double* x, double htry,
+      double eps, const StateType& yscal, Derivs derivs,
+      int& max_step_dir) -> double {
+      return this->stepper.step(y, dydx, x, htry, eps,
+                                yscal, derivs, max_step_dir);
+   };
 
-   double x = start;
-   double h = (end - start) >= 0 ? std::fabs(guess) : -std::fabs(guess);
-
-   StateType yscal(nvar);
-   StateType y(ystart);
-   StateType dydx;
-   int max_step_dir;
-
-   for (int nstp = 0; nstp < max_steps; ++nstp) {
-      dydx = derivs(x, y);
-      yscal = y.abs() + (dydx * h).abs() + TINY;
-
-      if ((x + h - end) * (x + h - start) > 0.0) {
-         h = end - x;
-      }
-
-      const double hnext = stepper.step(y, dydx, &x, h, tolerance,
-                                        yscal, derivs, max_step_dir);
-
-      if ((x - end) * (end - start) >= 0.0) {
-         ystart = y;
-         return;
-      }
-
-      h = hnext;
-
-      if (std::fabs(hnext) <= hmin)
-         break;
-   }
-
-#ifdef ENABLE_VERBOSE
-   ERROR("Basic_rkintegrator: too many steps\n"
-         "********** Q = " << std::exp(x) << " *********");
-   ERROR("max step in direction of " << max_step_dir);
-   for (int i = 0; i < nvar; i++)
-      ERROR("y(" << i << ") = " << y(i) << " dydx(" << i <<
-            ") = " << dydx(i));
-#endif
-
-   throw NonPerturbativeRunningError(std::exp(x), max_step_dir,
-                                     y(max_step_dir));
+   integrateOdes(ystart, start, end, tolerance, guess, hmin,
+                 derivs, rkqs, max_steps);
 }
 
 } // namespace runge_kutta
--- src/rk.hpp
+++ src/rk.hpp
@@ -55,34 +55,54 @@ template <typename ArrayType, typename Derivs>
 void rungeKuttaStep(const ArrayType& y, const ArrayType& dydx, double x,
 		    double h, ArrayType& yout, ArrayType& yerr, Derivs derivs)
 {
-  const double a2 = 0.2,a3 = 0.3,a4 = 0.6,a5 = 1.0,a6 = 0.875,b21 =
-    0.2,b31 = 3.0 / 40.0,b32 = 9.0 / 40.0,b41 = 0.3,b42 = -0.9,b43 = 1.2,
-    b51 = -11.0 / 54.0, b52 = 2.5,b53 = -70.0 / 27.0,b54 = 35.0 / 27.0,
-    b61 = 1631.0 / 55296.0,b62 = 175.0 / 512.0,b63 = 575.0 / 13824.0,
-    b64 = 44275.0 / 110592.0,b65 = 253.0 / 4096.0,c1 = 37.0 / 378.0,
-    c3 = 250.0 / 621.0,c4 = 125.0 / 594.0,c6 = 512.0 / 1771.0,
-    dc5 = -277.00 / 14336.0;
-  const double dc1 = c1-2825.0 / 27648.0,dc3 = c3-18575.0 / 48384.0,
-    dc4 = c4-13525.0 / 55296.0,dc6 = c6-0.25;
+   const double a2 = 0.2;
+   const double a3 = 0.3;
+   const double a4 = 0.6;
+   const double a5 = 1.0;
+   const double a6 = 0.875;
+   const double b21 = 0.2;
+   const double b31 = 3.0 / 40.0;
+   const double b32 = 9.0 / 40.0;
+   const double b41 = 0.3;
+   const double b42 = -0.9;
+   const double b43 = 1.2;
+   const double b51 = -11.0 / 54.0;
+   const double b52 = 2.5;
+   const double b53 = -70.0 / 27.0;
+   const double b54 = 35.0 / 27.0;
+   const double b61 = 1631.0 / 55296.0;
+   const double b62 = 175.0 / 512.0;
+   const double b63 = 575.0 / 13824.0;
+   const double b64 = 44275.0 / 110592.0;
+   const double b65 = 253.0 / 4096.0;
+   const double c1 = 37.0 / 378.0;
+   const double c3 = 250.0 / 621.0;
+   const double c4 = 125.0 / 594.0;
+   const double c6 = 512.0 / 1771.0;
+   const double dc5 = -277.00 / 14336.0;
+   const double dc1 = c1 - 2825.0 / 27648.0;
+   const double dc3 = c3 - 18575.0 / 48384.0;
+   const double dc4 = c4 - 13525.0 / 55296.0;
+   const double dc6 = c6 - 0.25;
 
-  ArrayType ytemp = b21 * h * dydx + y;
-  const ArrayType ak2 = derivs(x + a2 * h, ytemp);
+   ArrayType ytemp = b21 * h * dydx + y;
+   const ArrayType ak2 = derivs(x + a2 * h, ytemp);
 
-  // Allowing piece-wise calculating of ytemp for speed reasons
-  ytemp = y + h * (b31 * dydx + b32 * ak2);
-  const ArrayType ak3 = derivs(x + a3 * h, ytemp);
+   // Allowing piece-wise calculating of ytemp for speed reasons
+   ytemp = y + h * (b31 * dydx + b32 * ak2);
+   const ArrayType ak3 = derivs(x + a3 * h, ytemp);
 
-  ytemp = y + h * (b41 * dydx + b42 * ak2 + b43 * ak3);
-  const ArrayType ak4 = derivs(x+a4*h,ytemp);
+   ytemp = y + h * (b41 * dydx + b42 * ak2 + b43 * ak3);
+   const ArrayType ak4 = derivs(x+a4*h,ytemp);
 
-  ytemp = y + h * (b51 * dydx + b52 * ak2 + b53 * ak3 + b54 * ak4);
-  const ArrayType ak5 = derivs(x + a5 * h, ytemp);
+   ytemp = y + h * (b51 * dydx + b52 * ak2 + b53 * ak3 + b54 * ak4);
+   const ArrayType ak5 = derivs(x + a5 * h, ytemp);
 
-  ytemp = y + h * (b61 * dydx + b62 * ak2 + b63 * ak3 + b64 * ak4 + b65 * ak5);
-  const ArrayType ak6 = derivs(x + a6 * h, ytemp);
+   ytemp = y + h * (b61 * dydx + b62 * ak2 + b63 * ak3 + b64 * ak4 + b65 * ak5);
+   const ArrayType ak6 = derivs(x + a6 * h, ytemp);
 
-  yout = y + h * (c1 * dydx + c3 * ak3 + c4 * ak4 + c6 * ak6);
-  yerr = h * (dc1 * dydx + dc3 * ak3 + dc4 * ak4 + dc5 * ak5 + dc6 * ak6);
+   yout = y + h * (c1 * dydx + c3 * ak3 + c4 * ak4 + c6 * ak6);
+   yerr = h * (dc1 * dydx + dc3 * ak3 + dc4 * ak4 + dc5 * ak5 + dc6 * ak6);
 }
 
 /// organises the variable step-size for Runge-Kutta evolution
@@ -91,41 +111,48 @@ double odeStepper(ArrayType& y, const ArrayType& dydx, double *x, double htry,
                   double eps, const ArrayType& yscal, Derivs derivs,
                   int& max_step_dir)
 {
-  const double SAFETY = 0.9, PGROW = -0.2, PSHRNK = -0.25, ERRCON = 1.89e-4;
-  const int n = y.size();
-  double errmax, h = htry;
-  ArrayType yerr(n), ytemp(n);
+   const double SAFETY = 0.9;
+   const double PGROW = -0.2;
+   const double PSHRNK = -0.25;
+   const double ERRCON = 1.89e-4;
+   const int n = y.size();
+   double errmax;
+   double h = htry;
+   ArrayType yerr(n);
+   ArrayType ytemp(n);
 
-  for (;;) {
-    rungeKuttaStep(y, dydx, *x, h, ytemp, yerr, derivs);
-    errmax = (yerr / yscal).abs().maxCoeff(&max_step_dir);
-    errmax  /= eps;
-    if (!std::isfinite(errmax)) {
+   for (;;) {
+      rungeKuttaStep(y, dydx, *x, h, ytemp, yerr, derivs);
+      errmax = (yerr / yscal).abs().maxCoeff(&max_step_dir);
+      errmax  /= eps;
+      if (!std::isfinite(errmax)) {
 #ifdef ENABLE_VERBOSE
-       ERROR("odeStepper: non-perturbative running at Q = "
-             << std::exp(*x) << " GeV of parameter y(" << max_step_dir
-             << ") = " << y(max_step_dir) << ", dy(" << max_step_dir
-             << ")/dx = " << dydx(max_step_dir));
+         ERROR("odeStepper: non-perturbative running at Q = "
+               << std::exp(*x) << " GeV of parameter y(" << max_step_dir
+               << ") = " << y(max_step_dir) << ", dy(" << max_step_dir
+               << ")/dx = " << dydx(max_step_dir));
 #endif
-       throw NonPerturbativeRunningError(std::exp(*x), max_step_dir, y(max_step_dir));
-    }
-    if (errmax <= 1.0) break;
-    const double htemp = SAFETY * h * std::pow(errmax, PSHRNK);
-    h = (h >= 0.0 ? std::max(htemp, 0.1 * h) : std::min(htemp, 0.1 * h));
-    if (*x + h == *x) {
+         throw NonPerturbativeRunningError(std::exp(*x), max_step_dir, y(max_step_dir));
+      }
+      if (errmax <= 1.0) {
+         break;
+      }
+      const double htemp = SAFETY * h * std::pow(errmax, PSHRNK);
+      h = (h >= 0.0 ? std::max(htemp, 0.1 * h) : std::min(htemp, 0.1 * h));
+      if (*x + h == *x) {
 #ifdef ENABLE_VERBOSE
-       ERROR("At Q = " << std::exp(*x) << " GeV "
-             "stepsize underflow in odeStepper in parameter y("
-             << max_step_dir << ") = " << y(max_step_dir) << ", dy("
-             << max_step_dir << ")/dx = " << dydx(max_step_dir));
+         ERROR("At Q = " << std::exp(*x) << " GeV "
+               "stepsize underflow in odeStepper in parameter y("
+               << max_step_dir << ") = " << y(max_step_dir) << ", dy("
+               << max_step_dir << ")/dx = " << dydx(max_step_dir));
 #endif
-       throw NonPerturbativeRunningError(std::exp(*x), max_step_dir, y(max_step_dir));
-    }
-  }
-  *x += h;
-  y = ytemp;
+         throw NonPerturbativeRunningError(std::exp(*x), max_step_dir, y(max_step_dir));
+      }
+   }
+   *x += h;
+   y = ytemp;
 
-  return errmax > ERRCON ? SAFETY * h * std::pow(errmax,PGROW) : 5.0 * h;
+   return errmax > ERRCON ? SAFETY * h * std::pow(errmax,PGROW) : 5.0 * h;
 }
 
 /// Organises integration of 1st order system of ODEs
@@ -133,43 +160,48 @@ template <typename ArrayType, typename Derivs,
           typename Stepper = decltype(runge_kutta::odeStepper<ArrayType,Derivs>)>
 void integrateOdes(ArrayType& ystart, double from, double to, double eps,
                    double h1, double hmin, Derivs derivs,
-                   Stepper rkqs = runge_kutta::odeStepper<ArrayType,Derivs>)
+                   Stepper rkqs = runge_kutta::odeStepper<ArrayType,Derivs>, int max_steps = 400)
 {
-  const int nvar = ystart.size();
-  const int MAXSTP = 400;
-  const double TINY = 1.0e-16;
-  double x = from, h = sign(h1, to - from);
-  ArrayType yscal(nvar), y(ystart), dydx;
-  int max_step_dir;
+   const int nvar = ystart.size();
+   const double TINY = 1.0e-16;
+   double x = from;
+   double h = sign(h1, to - from);
+   ArrayType yscal(nvar);
+   ArrayType y(ystart);
+   ArrayType dydx;
+   int max_step_dir;
 
-  for (int nstp = 0; nstp < MAXSTP; nstp++) {
-    dydx = derivs(x, y);
-    yscal = y.abs() + (dydx * h).abs() + TINY;
-    if ((x + h - to) * (x + h - from) > 0.0) h = to - x;
+   for (int nstp = 0; nstp < max_steps; ++nstp) {
+      dydx = derivs(x, y);
+      yscal = y.abs() + (dydx * h).abs() + TINY;
+      if ((x + h - to) * (x + h - from) > 0.0) {
+         h = to - x;
+      }
 
-    const double hnext = rkqs(y, dydx, &x, h, eps, yscal, derivs, max_step_dir);
+      const double hnext = rkqs(y, dydx, &x, h, eps, yscal, derivs, max_step_dir);
 
-    if ((x - to) * (to - from) >= 0.0) {
-      ystart = y;
-      return;
-    }
+      if ((x - to) * (to - from) >= 0.0) {
+         ystart = y;
+         return;
+      }
 
-    h = hnext;
+      h = hnext;
 
-    if (std::fabs(hnext) <= hmin)
-      break;
-  }
+      if (std::fabs(hnext) <= hmin) {
+         break;
+      }
+   }
 
 #ifdef ENABLE_VERBOSE
-  ERROR("Bailed out of rk.cpp:too many steps in integrateOdes\n"
-        "********** Q = " << std::exp(x) << " *********");
-  ERROR("max step in direction of " << max_step_dir);
-  for (int i = 0; i < nvar; i++)
-    ERROR("y(" << i << ") = " << y(i) << " dydx(" << i <<
-          ") = " << dydx(i));
+   ERROR("Bailed out of rk.cpp:too many steps in integrateOdes\n"
+         "********** Q = " << std::exp(x) << " *********");
+   ERROR("max step in direction of " << max_step_dir);
+   for (int i = 0; i < nvar; i++)
+      ERROR("y(" << i << ") = " << y(i) << " dydx(" << i <<
+            ") = " << dydx(i));
 #endif
 
-  throw NonPerturbativeRunningError(std::exp(x), max_step_dir, y(max_step_dir));
+   throw NonPerturbativeRunningError(std::exp(x), max_step_dir, y(max_step_dir));
 }
 
 } // namespace runge_kutta
