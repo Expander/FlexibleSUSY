e3b795d98cb8dfd595d0dbe872cbb4a79bc022ee
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-07-20 08:53:16 +0200

    correcting file name in documentation

--- meta/MSSM/extract_MSSM_delta_mb_from_softsusy.m
+++ meta/MSSM/extract_MSSM_delta_mb_from_softsusy.m
@@ -5,7 +5,7 @@
    The GiNaC expression in "dmbas2.expr" has been extracted from
    SOFTSUSY 3.7.4 by adding the following C++ code snippet into the
    file
-   src/two_loop_thresholds/two_loop_archives/tquark_2loop_strong_pole.gar
+   src/two_loop_thresholds/two_loop_archives/bquark_corrections.cpp
    right after the cache has been filled:
 
    if (cache.size() == 1) {
