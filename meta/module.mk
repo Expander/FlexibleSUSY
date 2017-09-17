dff2faf58d102f5d905eccc8f532ec8ea04c61ea
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-09-17 11:07:33 +0200

    do not export auxiliary files, export only useful meta files

--- meta/module.mk
+++ meta/module.mk
@@ -24,9 +24,6 @@ META_MSSM_SRC:= \
 		$(DIR)/MSSM/beta_Yd.m \
 		$(DIR)/MSSM/beta_Ye.m \
 		$(DIR)/MSSM/beta_Yu.m \
-		$(DIR)/MSSM/dmtas2.m \
-		$(DIR)/MSSM/extract_MSSM_beta_functions_from_hep-ph-0308231.m \
-		$(DIR)/MSSM/extract_MSSM_Mt_over_mt_from_softsusy.m \
 		$(DIR)/MSSM/gamma_SdR.m \
 		$(DIR)/MSSM/gamma_SeR.m \
 		$(DIR)/MSSM/gamma_SHd.m \
@@ -34,11 +31,15 @@ META_MSSM_SRC:= \
 		$(DIR)/MSSM/gamma_SlL.m \
 		$(DIR)/MSSM/gamma_SqL.m \
 		$(DIR)/MSSM/gamma_SuR.m \
+		$(DIR)/MSSM/das2.m \
+		$(DIR)/MSSM/dmtauas2.m \
+		$(DIR)/MSSM/bquark_2loop_sqcd_decoupling.m \
+		$(DIR)/MSSM/bquark_2loop_sqcd_pole.m \
 		$(DIR)/MSSM/tquark_1loop_qcd.m \
 		$(DIR)/MSSM/tquark_1loop_strong.m \
 		$(DIR)/MSSM/tquark_2loop_qcd.m \
 		$(DIR)/MSSM/tquark_2loop_strong.m \
-		$(DIR)/MSSM/tquark_to_cpp.m
+		$(DIR)/MMSM/twoloopbubble.m
 
 META_SM_SRC  := \
 		$(DIR)/SM/beta_g1.m \
