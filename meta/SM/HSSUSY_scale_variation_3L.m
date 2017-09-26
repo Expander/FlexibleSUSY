0725f49fa6b969b3bea44a776de4327668e8c3a7
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-09-26 18:03:38 +0200

    take degenerate 2L O(at*as) thresholds into account in the running
    Note: this is incomplete!  The general expressions, also including
    O(at^2), needs to be used!

--- meta/SM/HSSUSY_scale_variation_3L.m
+++ meta/SM/HSSUSY_scale_variation_3L.m
@@ -1,6 +1,58 @@
 (* Created by Wolfram Mathematica 9.0 : www.wolfram.com *)
 -2944*g3^4*gt^4*Log[MS/Q]^3 + 4416*g3^2*gt^6*Log[MS/Q]^3 - 
- 1548*gt^8*Log[MS/Q]^3 - 
+ 1548*gt^8*Log[MS/Q]^3 + 
+ (4*((-14*g3^4*gt^4*Log[MS/Q] + (g3^2*gt^3*(-160*g3^2*gt*Log[MS/Q] + 
+         90*gt^3*Log[MS/Q]))/5)*
+     (((At - \[Mu]/tb)*(24 + (12*(At - \[Mu]/tb))/M3 - (28*(At - \[Mu]/tb)^2)/
+          M3^2 - (At - \[Mu]/tb)^3/M3^3 + (2*(At - \[Mu]/tb)^4)/M3^4))/M3 - 
+      (2*(At - \[Mu]/tb)*(24 - (24*(At - \[Mu]/tb))/M3 - 
+         (4*(At - \[Mu]/tb)^2)/M3^2 + (At - \[Mu]/tb)^3/M3^3)*
+        Log[Sqrt[mq2[3, 3]*mu2[3, 3]]/Q^2])/M3 + 
+      36*Log[Sqrt[mq2[3, 3]*mu2[3, 3]]/Q^2]^2) + 
+    g3^2*gt^4*((2*(5*At^4*tb^4 - 2*At^3*M3*tb^4 - 42*At^2*M3^2*tb^4 + 
+         12*At*M3^3*tb^4 + 12*M3^4*tb^4 - 20*At^3*tb^3*\[Mu] + 
+         6*At^2*M3*tb^3*\[Mu] + 84*At*M3^2*tb^3*\[Mu] - 12*M3^3*tb^3*\[Mu] + 
+         30*At^2*tb^2*\[Mu]^2 - 6*At*M3*tb^2*\[Mu]^2 - 42*M3^2*tb^2*\[Mu]^2 - 
+         20*At*tb*\[Mu]^3 + 2*M3*tb*\[Mu]^3 + 5*\[Mu]^4)*
+        (180*At*gt^2*tb*Log[MS/Q] + 90*At*g3^2*tb^3*Log[MS/Q] + 
+         180*At*gt^2*tb^3*Log[MS/Q] + 160*g3^2*M3*tb^3*Log[MS/Q] - 
+         90*gt^2*\[Mu]*Log[MS/Q] - 90*g3^2*tb^2*\[Mu]*Log[MS/Q] - 
+         90*gt^2*tb^2*\[Mu]*Log[MS/Q]))/(15*M3^5*tb^7) + 
+      (36*Log[Sqrt[mq2[3, 3]*mu2[3, 3]]/Q^2]*
+        (mu2[3, 3]*((-32*g3^2*M3^2*Log[MS/Q])/3 + (2*gt^2*mA^2*Log[MS/Q])/
+            tb^2 + (2*At^2*gt^2*(1 + tb^2)*Log[MS/Q])/tb^2 - 
+           (2*gt^2*(1 + tb^2)*\[Mu]^2*Log[MS/Q])/tb^2 + 
+           (2*gt^2*(1 + tb^2)*Log[MS/Q]*mq2[3, 3])/tb^2 + 
+           (2*gt^2*(1 + tb^2)*Log[MS/Q]*mu2[3, 3])/tb^2) + 
+         mq2[3, 3]*((-32*g3^2*M3^2*Log[MS/Q])/3 + (4*gt^2*mA^2*Log[MS/Q])/
+            tb^2 + (4*At^2*gt^2*(1 + tb^2)*Log[MS/Q])/tb^2 - 
+           (4*gt^2*(1 + tb^2)*\[Mu]^2*Log[MS/Q])/tb^2 + 
+           (4*gt^2*(1 + tb^2)*Log[MS/Q]*mq2[3, 3])/tb^2 + 
+           (4*gt^2*(1 + tb^2)*Log[MS/Q]*mu2[3, 3])/tb^2)))/
+       (mq2[3, 3]*mu2[3, 3]) - 
+      2*(-((At - \[Mu]/tb)*(-3*At^2*tb^2 + 8*At*M3*tb^2 + 24*M3^2*tb^2 + 
+            6*At*tb*\[Mu] - 8*M3*tb*\[Mu] - 3*\[Mu]^2)*
+           (180*At*gt^2*tb*Log[MS/Q] + 90*At*g3^2*tb^3*Log[MS/Q] + 
+            180*At*gt^2*tb^3*Log[MS/Q] + 160*g3^2*M3*tb^3*Log[MS/Q] - 
+            90*gt^2*\[Mu]*Log[MS/Q] - 90*g3^2*tb^2*\[Mu]*Log[MS/Q] - 
+            90*gt^2*tb^2*\[Mu]*Log[MS/Q])*Log[Sqrt[mq2[3, 3]*mu2[3, 3]]/Q^2])/
+         (15*M3^4*tb^5) + (24 - (24*(At - \[Mu]/tb))/M3 - 
+          (4*(At - \[Mu]/tb)^2)/M3^2 + (At - \[Mu]/tb)^3/M3^3)*
+         ((((32*g3^2*M3*Log[MS/Q])/3 + (12*At*gt^2*(1 + tb^2)*Log[MS/Q])/
+              tb^2 - (3*(10*gt^2*\[Mu]*Log[MS/Q] + 10*gt^2*tb^2*\[Mu]*
+                 Log[MS/Q]))/(5*tb^3))*Log[Sqrt[mq2[3, 3]*mu2[3, 3]]/Q^2])/
+           M3 + (At - \[Mu]/tb)*((6*g3^2*Log[MS/Q]*Log[Sqrt[mq2[3, 3]*
+                  mu2[3, 3]]/Q^2])/M3 + (mu2[3, 3]*((-32*g3^2*M3^2*Log[MS/Q])/
+                 3 + (2*gt^2*mA^2*Log[MS/Q])/tb^2 + (2*At^2*gt^2*(1 + tb^2)*
+                  Log[MS/Q])/tb^2 - (2*gt^2*(1 + tb^2)*\[Mu]^2*Log[MS/Q])/
+                 tb^2 + (2*gt^2*(1 + tb^2)*Log[MS/Q]*mq2[3, 3])/tb^2 + 
+                (2*gt^2*(1 + tb^2)*Log[MS/Q]*mu2[3, 3])/tb^2) + 
+              mq2[3, 3]*((-32*g3^2*M3^2*Log[MS/Q])/3 + (4*gt^2*mA^2*
+                  Log[MS/Q])/tb^2 + (4*At^2*gt^2*(1 + tb^2)*Log[MS/Q])/tb^2 - 
+                (4*gt^2*(1 + tb^2)*\[Mu]^2*Log[MS/Q])/tb^2 + 
+                (4*gt^2*(1 + tb^2)*Log[MS/Q]*mq2[3, 3])/tb^2 + 
+                (4*gt^2*(1 + tb^2)*Log[MS/Q]*mu2[3, 3])/tb^2))/
+             (2*M3*mq2[3, 3]*mu2[3, 3])))))))/3 - 
  3*(((gt^2*(-160*g3^2*gt*Log[MS/Q] + 90*gt^3*Log[MS/Q])^2)/100 + 
      2*gt^2*((-160*g3^2*gt*Log[MS/Q] + 90*gt^3*Log[MS/Q])^2/400 + 
        2*gt*(88*g3^4*gt*Log[MS/Q]^2 - 72*g3^2*gt^3*Log[MS/Q]^2 + 
