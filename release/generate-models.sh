e0cc18b221c4770886264942e7f3fb1e3b15de32
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-10-25 08:09:32 +0200

    build more models

--- release/generate-models.sh
+++ release/generate-models.sh
@@ -54,6 +54,7 @@ models_array="             \
    NUTNMSSM,NMSSM          \
    NUTSMSSM,SMSSM          \
    lowNMSSM,NMSSM          \
+   lowNMSSMTanBetaAtMZ,NMSSM \
    SMSSM,SMSSM             \
    UMSSM,UMSSM             \
    E6SSM,E6SSM             \
@@ -62,13 +63,19 @@ models_array="             \
    SM,SM                   \
    HSSUSY,SM               \
    SplitMSSM,SplitMSSM     \
+   THDMII,THDM-II          \
    THDMIIMSSMBC,THDM-II    \
    HTHDMIIMSSMBC,HTHDM-II  \
    HGTHDMIIMSSMBC,HGTHDM-II \
-   MSSMEFTHiggs,MSSM          \
-   NMSSMEFTHiggs,NMSSM        \
-   E6SSMEFTHiggs,E6SSM        \
-   MRSSMEFTHiggs,MRSSM        \
+   MSSMEFTHiggs,MSSM       \
+   NMSSMEFTHiggs,NMSSM     \
+   E6SSMEFTHiggs,E6SSM     \
+   MRSSMEFTHiggs,MRSSM     \
+   CNMSSM,NMSSM            \
+   CE6SSM,E6SSM            \
+   MSSMNoFVatMGUTHimalaya,MSSMNoFV \
+   MSSMNoFVHimalaya,MSSMNoFV \
+   NUHMSSMNoFVHimalaya,MSSMNoFV \
 "
 
 models="`echo $models_array | sed 's/,[a-zA-Z0-9-]*//g'`"
