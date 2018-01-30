084d060a1dca967a2ae046bcc78b3350331c9699
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-01-30 15:27:33 +0100

    update tests

--- test/test_SM_3loop_beta.m
+++ test/test_SM_3loop_beta.m
@@ -29,8 +29,8 @@ Print["testing BetaSM[] ..."];
 
 TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`hyperchargeCoupling]], 3];
 TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`leftCoupling       ]], 3];
-TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`strongCoupling     ]], 3];
-TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`UpYukawa           ]], 3];
+TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`strongCoupling     ]], 4];
+TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`UpYukawa           ]], 4];
 TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`DownYukawa         ]], 3];
 TestEquality[Length[ThreeLoopSM`BetaSM[SARAH`ElectronYukawa     ]], 3];
 TestEquality[Length[ThreeLoopSM`BetaSM[\[Lambda]                ]], 4];
