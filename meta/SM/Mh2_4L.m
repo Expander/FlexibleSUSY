c51af970c75e9add70ab32741a63c18e12f91056
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-02-01 21:41:58 +0100

    correcting normalization of effective 4L potential
    Note: need to correct this also in the C++ code

--- meta/SM/Mh2_4L.m
+++ meta/SM/Mh2_4L.m
@@ -1,7 +1,7 @@
 (* effective potential from (5.3) of [1508.00912] *)
 (* k = 1/(4 Pi)^2 *)
-V4L[h_] := h^4 gs^6 yt^4 k^4 (a4 l^4 + a3 l^3 + a2 l^2 + a1 l + a0) /.
-    { l -> Log[h^2*yt^2/(2*Q^2)] };
+V4L[h_] := gs^6 mt^4 k^4 (a4 l^4 + a3 l^3 + a2 l^2 + a1 l + a0) /.
+    { l -> Log[mt^2/Q^2] } /. { mt -> h yt/Sqrt[2] };
 
 (* tadpole / v *)
 t4L = -1/v D[V4L[h], h] /. { h -> v } /. { v -> Sqrt[2] mt/yt } // Simplify;
