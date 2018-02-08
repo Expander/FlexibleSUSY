9993ec5c81ae9a62d8f26a4366292e49f23b852e
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-02-08 10:15:14 +0100

    use Mathematica to determine whether flags are set
    This catches cases where the FlexibleSUSY model file contains
    expressions of the form
    
      UseHiggs2LoopMSSM = True
    
    or
    
      UseHiggs2LoopSM = UseHiggs3LoopSM = UseHiggs4LoopSM = True
    
    Thanks to Jae-hyeon for pointing this out!

--- config/model_specific_corrections.sh
+++ config/model_specific_corrections.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# This script provides the get_model_specific_corrections() function,
+# which takes a FlexibleSUSY model file [$1] (and optionally the
+# Mathematica kernel command [$2]) and returns the list of
+# model-specific corrections necessary for the build.
+
+#_____________________________________________________________________
+__contains() {
+    # Check if string $1 contains $2
+    if test $# -lt 2 ; then
+        echo "contains: Too few arguments"
+        return 1
+    fi
+    local string="$1"
+    local substring="$2"
+    for f in ${string}; do
+        test "x$f" = "x$substring" && return 0
+    done
+    return 1 # not found
+}
+
 #_____________________________________________________________________
 __make_unique() {
     # removes duplicate words from the string $1 (preserves order)
@@ -7,7 +27,7 @@ __make_unique() {
     local res=""
 
     for s in ${str}; do
-        contains "${res}" "${s}" && continue
+        __contains "${res}" "${s}" && continue
         res="${res} ${s}"
     done
 
@@ -15,28 +35,72 @@ __make_unique() {
 }
 
 #_____________________________________________________________________
+__mma_symbol_is() {
+    local model_file="$1"
+    local math_cmd="$2"
+    local symbol="$3"
+    local value="$4"
+
+    [ -e "${model_file}" ] || {
+        >&2 echo "Error: file \"${model_file}\" does not exist!"
+        return 1
+    }
+
+    command -v "${math_cmd}" >/dev/null 2>&1 || {
+        >&2 echo "Error: Mathematica kernel \"${math_cmd}\" is not executable!"
+        return 1
+    }
+
+    [ -z "${symbol}" ] && {
+        >&2 echo "Error: no symbol given!"
+        return 1
+    }
+
+    [ -z "${value}" ] && {
+        >&2 echo "Error: no value given!"
+        return 1
+    }
+
+    echo "Get[\"${model_file}\"]; Quit[Boole[!(${symbol} === ${value})]]" | "${math_cmd}" > /dev/null 2>&1
+
+    return $?
+}
+
+#_____________________________________________________________________
 get_model_specific_corrections() {
     local model_file="$1"
+    local math_cmd="${2:-math}"
 
     # always build SM, because it is needed for the effective couplings module
     local ho="SM"
 
-    if test -e "$model_file"; then
-        grep "UseHiggs2LoopSM *= *True"    "$model_file" > /dev/null 2>&1 && ho="${ho} SM"
-        grep "UseHiggs3LoopSM *= *True"    "$model_file" > /dev/null 2>&1 && ho="${ho} SM"
-        grep "UseHiggs4LoopSM *= *True"    "$model_file" > /dev/null 2>&1 && ho="${ho} SM"
-        grep "UseSMAlphaS3Loop *= *True"   "$model_file" > /dev/null 2>&1 && ho="${ho} SM"
-        grep "FlexibleEFTHiggs *= *True"   "$model_file" > /dev/null 2>&1 && ho="${ho} SM"
+    [ -e "${model_file}" ] || {
+        >&2 echo "Error: file \"${model_file}\" does not exist!"
+        echo "${ho}"
+        return 1
+    }
 
-        grep "UseHiggs2LoopMSSM *= *True"  "$model_file" > /dev/null 2>&1 && ho="${ho} MSSM_higgs"
-        grep "UseHiggs3LoopMSSM *= *True"  "$model_file" > /dev/null 2>&1 && ho="${ho} MSSM_higgs"
-        grep "UseMSSMAlphaS2Loop *= *True" "$model_file" > /dev/null 2>&1 && ho="${ho} MSSM_thresholds"
-        grep "UseMSSMYukawa2Loop *= *True" "$model_file" > /dev/null 2>&1 && ho="${ho} MSSM_thresholds"
+    command -v "${math_cmd}" >/dev/null 2>&1 || {
+        >&2 echo "Error: Mathematica kernel \"${math_cmd}\" is not executable!"
+        >&2 echo "   Please specify the Mathematica kernel as 2nd argument!"
+        echo "${ho}"
+        return 1
+    }
 
-        grep "UseHiggs2LoopNMSSM *= *True" "$model_file" > /dev/null 2>&1 && ho="${ho} MSSM_higgs NMSSM_higgs"
+    __mma_symbol_is "$model_file" "$math_cmd" "UseHiggs2LoopSM" "True"    && ho="${ho} SM"
+    __mma_symbol_is "$model_file" "$math_cmd" "UseHiggs3LoopSM" "True"    && ho="${ho} SM"
+    __mma_symbol_is "$model_file" "$math_cmd" "UseHiggs4LoopSM" "True"    && ho="${ho} SM"
+    __mma_symbol_is "$model_file" "$math_cmd" "UseSMAlphaS3Loop" "True"   && ho="${ho} SM"
+    __mma_symbol_is "$model_file" "$math_cmd" "FlexibleEFTHiggs" "True"   && ho="${ho} SM"
 
-        grep "UseHiggs3LoopSplit *= *True" "$model_file" > /dev/null 2>&1 && ho="${ho} SplitMSSM"
-    fi
+    __mma_symbol_is "$model_file" "$math_cmd" "UseHiggs2LoopMSSM" "True"  && ho="${ho} MSSM_higgs"
+    __mma_symbol_is "$model_file" "$math_cmd" "UseHiggs3LoopMSSM" "True"  && ho="${ho} MSSM_higgs"
+    __mma_symbol_is "$model_file" "$math_cmd" "UseMSSMAlphaS2Loop" "True" && ho="${ho} MSSM_thresholds"
+    __mma_symbol_is "$model_file" "$math_cmd" "UseMSSMYukawa2Loop" "True" && ho="${ho} MSSM_thresholds"
+
+    __mma_symbol_is "$model_file" "$math_cmd" "UseHiggs2LoopNMSSM" "True" && ho="${ho} MSSM_higgs NMSSM_higgs"
+
+    __mma_symbol_is "$model_file" "$math_cmd" "UseHiggs3LoopSplit" "True" && ho="${ho} SplitMSSM"
 
     echo "$(__make_unique "${ho}")"
 }
