801199c546f685db07f673c161817c042099344e
Jobst Ziebell <Jobst.Ziebell@tu-dresden.de>
2018-03-05 16:51:10 +0100

    Added another cxx helper file

---
+++ src/concatenate.hpp
@@ -0,0 +1,89 @@
+#ifndef H_FS_CONCATENATE
+#define H_FS_CONCATENATE
+
+#include <type_traits>
+
+namespace detail
+{
+template<class T> 
+struct value_type
+{
+  using type = typename T::value_type;
+};
+
+template<class ...Args>
+struct common_value_type
+{
+  using type = typename std::common_type<
+    typename value_type<Args>::type...
+  >::type;
+};
+
+template<int ...Args> struct sum;
+
+template<int V1, int ...Tail>
+struct sum<V1, Tail...>
+{
+  static constexpr int value = V1 + sum<Tail...>::value;
+};
+
+template<> struct sum<>
+{
+  static constexpr int value = 0;
+};
+
+namespace result_of
+{
+template<class ...Args>
+struct concatenate
+{
+  using type = std::array<
+    typename common_value_type<
+      typename std::decay<Args>::type...
+    >::type,
+    sum<std::tuple_size<
+      typename std::decay<Args>::type
+    >::value...>::value
+  >;
+};
+}
+
+template<class ...Args>
+struct concatenate_impl;
+
+template<class T, class ...Args>
+struct concatenate_impl<T, Args...>
+{
+  template<class OutputIterator>
+  void operator()( OutputIterator it, T &&t, Args &&...args )
+  {
+    auto next = std::copy( t.begin(), t.end(), it );
+    concatenate_impl<Args...>{}( next, std::forward<Args>( args )... );
+  }
+};
+
+template<>
+struct concatenate_impl<>
+{
+  template<class OutputIterator>
+  void operator()( OutputIterator it )
+  {}
+};
+}
+
+template<class ...Args>
+typename detail::result_of::concatenate<Args...>::type
+concatenate( Args &&...args )
+{
+  using result_type =
+    typename detail::result_of::concatenate<Args...>::type;
+  result_type result;  
+
+  detail::concatenate_impl<Args...>{}( result.begin(),
+    std::forward<Args>( args )... );
+
+  return result;
+}
+
+#endif
+
