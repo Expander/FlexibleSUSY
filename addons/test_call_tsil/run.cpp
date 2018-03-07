2f4ee8a8cf9e43a7783cd8a0d91d8c280a3307c2
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-03-07 10:38:16 +0100

    remove rule for .c files

--- addons/test_call_tsil/run.cpp
+++ addons/test_call_tsil/run.cpp
@@ -1,11 +1,9 @@
-#include "call_tsil.h"
 #include "call_tsil.hpp"
 #include <iostream>
 
 int main()
 {
-   std::cout << "C  : A0(100,100) = " << call_A(100., 100.) << '\n';
-   std::cout << "C++: A0(100,100) = " << call_A_cpp(100., 100.) << '\n';
+   std::cout << "C++: A0(100,100) = " << call_A(100., 100.) << '\n';
 
    return 0;
 }
