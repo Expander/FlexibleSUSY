8a5dc1c68d4415c0a479605cf66453ca89aa9a62
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-03-22 10:57:19 +0100

    adding test of 0L and 1L spectrum of generated and pre-generated SM

--- test/test_SM_one_loop_spectrum.cpp
+++ test/test_SM_one_loop_spectrum.cpp
@@ -8,11 +8,12 @@
 #include "wrappers.hpp"
 #include "pv.hpp"
 #include "SM_two_scale_model.hpp"
+#include "standard_model.hpp"
 
 using namespace flexiblesusy;
 using namespace passarino_veltman;
 
-BOOST_AUTO_TEST_CASE( test_SM_tree_level_masses )
+BOOST_AUTO_TEST_CASE( test_SM_one_loop_Higgs_masses )
 {
    SM_input_parameters input;
    input.LambdaIN = 0.25;
@@ -116,4 +117,100 @@ BOOST_AUTO_TEST_CASE( test_SM_heavy_top_self_energy )
       }
    }
 
+}
+
+#define CHECK_CLOSE_1(mass,eps)                                         \
+   do {                                                                 \
+      BOOST_CHECK_CLOSE(m.get_physical().mass, sm.get_physical().mass, (eps)); \
+      BOOST_TEST_MESSAGE(#mass " = " << m.get_physical().mass << " = " << sm.get_physical().mass); \
+   } while (false);
+
+#define CHECK_CLOSE_N(mass,eps,N)                                       \
+   do {                                                                 \
+      for (int i = 0; i < N; i++) {                                     \
+         BOOST_CHECK_CLOSE(m.get_physical().mass(i), sm.get_physical().mass(i), (eps)); \
+         BOOST_TEST_MESSAGE(#mass "(" << i << ") = " << m.get_physical().mass(i) << " = " << sm.get_physical().mass(i)); \
+      }                                                                 \
+   } while (false);
+
+BOOST_AUTO_TEST_CASE( test_SM_one_loop_masses )
+{
+   SM_input_parameters input;
+   input.LambdaIN = 0.25;
+   SM<Two_scale> m;
+   standard_model::Standard_model sm;
+   setup_SM_const(m, input);
+   setup_SM_const(sm, input);
+
+   m.do_calculate_sm_pole_masses(true);
+
+   m.calculate_DRbar_masses();
+   sm.calculate_DRbar_masses();
+
+   m.solve_ewsb_one_loop();
+   sm.solve_ewsb_one_loop();
+
+   m.calculate_pole_masses();
+   sm.calculate_pole_masses();
+
+   const double eps = 1e-13;
+
+   CHECK_CLOSE_1(MVP, eps);
+   CHECK_CLOSE_1(MVG, eps);
+   CHECK_CLOSE_1(MVZ, eps);
+   CHECK_CLOSE_1(MVWp, eps);
+   CHECK_CLOSE_1(MAh, eps);
+   CHECK_CLOSE_1(MHp, eps);
+   CHECK_CLOSE_1(Mhh, eps);
+   CHECK_CLOSE_N(MFv, eps, 3);
+   CHECK_CLOSE_N(MFu, eps, 3);
+   CHECK_CLOSE_N(MFd, eps, 3);
+   CHECK_CLOSE_N(MFe, eps, 3);
+
+   const double MW = m.get_physical().MVWp;
+   const double MZ = m.get_physical().MVZ;
+
+   // specific pole mass functions for gauge bosons
+   BOOST_CHECK_CLOSE(m.calculate_MVWp_pole(MW), sm.calculate_MVWp_pole(MW), eps);
+   BOOST_CHECK_CLOSE(m.calculate_MVWp_pole(MW), MW, 0.02);
+
+   BOOST_CHECK_CLOSE(m.calculate_MVZ_pole(MZ) , sm.calculate_MVZ_pole(MZ) , eps);
+   BOOST_CHECK_CLOSE(m.calculate_MVZ_pole(MZ) , MZ, 0.02);
+
+   // specific DR-bar mass functions
+   BOOST_CHECK_CLOSE(m.calculate_MVP_DRbar(0), sm.calculate_MVP_DRbar(0), eps);
+   BOOST_CHECK_CLOSE(m.calculate_MVP_DRbar(0), 0., eps);
+
+   BOOST_CHECK_CLOSE(m.calculate_MVZ_DRbar(MZ), sm.calculate_MVZ_DRbar(MZ), eps);
+   BOOST_CHECK_CLOSE(m.calculate_MVZ_DRbar(MZ), m.get_MVZ(), 0.01);
+
+   BOOST_CHECK_CLOSE(m.calculate_MVWp_DRbar(MW), sm.calculate_MVWp_DRbar(MW), eps);
+   BOOST_CHECK_CLOSE(m.calculate_MVWp_DRbar(MW), m.get_MVWp(), 0.02);
+
+   for (int i = 0; i < 3; i++) {
+      const double eps = 1e-5, eps2 = 0.0002;
+
+      {
+         const double p = 0.;
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFv_DRbar(p,i), sm.calculate_MFv_DRbar(p,i), eps);
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFv_DRbar(p,i), m.get_MFv(i), eps2);
+      }
+      {
+         const double p = m.get_MFe(i);
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFe_DRbar(p,i), sm.calculate_MFe_DRbar(p,i), eps);
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFe_DRbar(p,i), m.get_MFe(i), eps2);
+      }
+      {
+         const double p = m.get_physical().MFu(i);
+         const double eps2 = i < 2 ? 0.4 : 0.006;
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFu_DRbar(p,i), sm.calculate_MFu_DRbar(p,i), eps);
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFu_DRbar(p,i), m.get_MFu(i), eps2);
+      }
+      {
+         const double p = m.get_MFd(i);
+         const double eps2 = i < 2 ? 0.4 : 0.005;
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFd_DRbar(p,i), sm.calculate_MFd_DRbar(p,i), eps);
+         BOOST_CHECK_CLOSE_FRACTION(m.calculate_MFd_DRbar(p,i), m.get_MFd(i), eps2);
+      }
+   }
 }
