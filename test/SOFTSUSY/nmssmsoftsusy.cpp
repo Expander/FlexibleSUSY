35d4f19529dc7be81777891b72bcde072b992691
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-08-14 17:04:46 +0200

    Bugfix: correcting re-scaling of NMSSM u/d-tadpoles to yield singlet tadpole
    Thanks to Sebastian Poegel.
    
    The 2-loop singlet tadpole can be obtained from the u/d-tadpoles by a
    re-scaling, i.e.
    
       t_s = (vd/vs) t_d
    
    where t_s is the singlet tadpole and t_d is the d-type tadpole diagram
    with mass dimension GeV^3 (i.e. not divided by the VEVs).
    
    The functions tadpole_higgs_2loop_at_as_nmssm() and
    tadpole_higgs_2loop_ab_as_nmssm() return the NMSSM tadpoles, divided
    by the corresponding VEVs, i.e. they return
    
       T_d = t_d / vd
       T_u = t_u / vu
       T_s = t_s / vs
    
    Thus, to obtain T_s one has to calculate
    
       T_s = t_s / vs
    
           = (vd/vs) t_d / vs
    
           = (vd/vs)^2 t_d / vd
    
           = (vd/vs)^2 T_d
    
    Before this commit, the incorrect relation
    
       T_s = (vd/vs) T_d       (incorrect)
    
    was used.

--- test/SOFTSUSY/nmssmsoftsusy.cpp
+++ test/SOFTSUSY/nmssmsoftsusy.cpp
@@ -173,8 +173,8 @@ void NmssmSoftsusy::doTadpoles(double mt, double sinthDRbar) {
         	&costau, &scalesq, &amu, &tanb, &vev2, &s1tau, &s2tau);
       
       //rescale T1 to get TS
-      double sss = s1s * vev * cos(atan(tanb)) / s;
-      double ssb = s1b * vev * sin(atan(tanb)) / s;
+      double sss = s1s * sqr(vev * cos(atan(tanb)) / s);
+      double ssb = s1b * sqr(vev * sin(atan(tanb)) / s);
 
       if (!testNan(s1s * s1t * s1b * s1tau * s2s * s2t * s2b * s2tau 
                    * sss * ssb)) {
