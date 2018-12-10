b1a919c0bc53221427362587b775fd0b922a6378
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-12-10 20:25:08 +0100

    adding tests for finiteness of 2-loop SQCD corrections to mt

--- test/test_mssm_twoloop_mt.cpp
+++ test/test_mssm_twoloop_mt.cpp
@@ -18,15 +18,18 @@ BOOST_AUTO_TEST_CASE(test_universal)
       // g3 mt mg mst1 mst2 msusy Xt Q
       { g3, mt, 1000., 1000., 1000., 1000.,                  0., 1000. },
       { g3, mt, 1000., 1000., 1000., 1000.,                  0., 1100. },
-      { g3, mt, 1000., 1000., 1000., 1000.,  std::sqrt(6.)*1000, 1000. },
-      { g3, mt, 1000., 1000., 1000., 1000.,  std::sqrt(6.)*1000, 1100. },
-      { g3, mt, 1000., 1000., 1000., 1000., -std::sqrt(6.)*1000, 1000. },
-      { g3, mt, 1000., 1000., 1000., 1000., -std::sqrt(6.)*1000, 1100. }
+      { g3, mt, 1000.,  900., 1100., 1000.,  std::sqrt(6.)*1000, 1000. },
+      { g3, mt, 1000.,  900., 1100., 1000.,  std::sqrt(6.)*1000, 1100. },
+      { g3, mt, 1000.,  900., 1100., 1000., -std::sqrt(6.)*1000, 1000. },
+      { g3, mt, 1000.,  900., 1100., 1000., -std::sqrt(6.)*1000, 1100. }
    };
 
    std::cout << "# universal SUSY masses" << std::endl;
-   for (const auto p: pars)
-      std::cout << dMt_over_mt_2loop(p) << std::endl;
+   for (const auto p: pars) {
+      const auto dmt = dMt_over_mt_2loop(p);
+      BOOST_CHECK(std::isfinite(dmt));
+      std::cout << dmt << std::endl;
+   }
 }
 
 BOOST_AUTO_TEST_CASE(test_non_universal)
@@ -42,9 +45,20 @@ BOOST_AUTO_TEST_CASE(test_non_universal)
    };
 
    std::cout << "# non-universal SUSY masses" << std::endl;
-   for (const auto p: pars)
-      std::cout << dMt_over_mt_1loop_qcd(p)  << ' '
-                << dMt_over_mt_1loop_susy(p) << ' '
-                << dMt_over_mt_2loop_qcd(p)  << ' '
-                << dMt_over_mt_2loop_susy(p) << std::endl;
+   for (const auto p: pars) {
+      const auto dmt_1L_qcd  = dMt_over_mt_1loop_qcd(p);
+      const auto dmt_1L_sqcd = dMt_over_mt_1loop_susy(p);
+      const auto dmt_2L_qcd  = dMt_over_mt_2loop_qcd(p);
+      const auto dmt_2L_sqcd = dMt_over_mt_2loop_susy(p);
+
+      BOOST_CHECK(std::isfinite(dmt_1L_qcd));
+      BOOST_CHECK(std::isfinite(dmt_1L_sqcd));
+      BOOST_CHECK(std::isfinite(dmt_2L_qcd));
+      BOOST_CHECK(std::isfinite(dmt_2L_sqcd));
+
+      std::cout << dmt_1L_qcd << ' '
+                << dmt_1L_sqcd << ' '
+                << dmt_2L_qcd << ' '
+                << dmt_2L_sqcd << std::endl;
+   }
 }
