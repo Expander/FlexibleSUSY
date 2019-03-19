877053d596558eb627a0d487b70ac855b5106323
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2019-03-19 12:50:11 +0100

    split up doc/module.mk for make install-src

---
+++ doc/manuals/module.mk
@@ -0,0 +1,208 @@
+DIR          := doc/manuals
+MODNAME      := doc-manuals
+MANUALS_MK   := $(DIR)/module.mk
+
+MANUALS_DIR  := $(DIR)
+
+PDF_OUTPUT_DIR  := $(MANUALS_DIR)
+
+MANUALS_INSTALL_DIR := $(INSTALL_DIR)/$(MANUALS_DIR)
+
+MANUALS_EXAMPLES_DIR := $(MANUALS_DIR)/examples-2.0
+
+MANUALS_PLOTS_DIR := $(MANUALS_DIR)/plots
+
+MANUALS_PLOTS_THDMIIMSSMBC := \
+		$(MANUALS_PLOTS_DIR)/THDMIIMSSMBC/THDMIIMSSMBC_Mh_MS_TB_MA-200_Xt-sqrt6.pdf \
+		$(MANUALS_PLOTS_DIR)/THDMIIMSSMBC/THDMIIMSSMBC_Mh_MS_TB_MA-200_Xt-0.pdf
+
+MANUALS_PLOTS_HSSUSY := \
+		$(MANUALS_PLOTS_DIR)/HSSUSY/Mh_MS_EFT_uncertainty.pdf \
+		$(MANUALS_PLOTS_DIR)/HSSUSY/HSSUSY_thresholds.pdf \
+		$(MANUALS_PLOTS_DIR)/HSSUSY/Mh_Xt.pdf \
+		$(MANUALS_PLOTS_DIR)/HSSUSY/Mh_Xt_uncertainty.pdf
+
+MANUALS_PLOTS_FlexibleMW := \
+		$(MANUALS_PLOTS_DIR)/FlexibleMW/scan_MW_M12.pdf \
+		$(MANUALS_PLOTS_DIR)/FlexibleMW/scan_MW_LamTU.pdf
+
+MANUALS_PLOTS_CMSSM_multiple_solutions := \
+		$(MANUALS_PLOTS_DIR)/CMSSM_multiple_solutions/CMSSM_multiple_solutions.pdf
+
+MANUALS_PLOTS_CE6SSM := \
+		$(MANUALS_PLOTS_DIR)/CE6SSM/ce6ssm_tb10-scan-l-k-vs_combined_m0_m12_ccA0.pdf \
+		$(MANUALS_PLOTS_DIR)/CE6SSM/ce6ssm_tb10-scan-l-k-vs_combined_m0_m12_ccmsu1.pdf \
+		$(MANUALS_PLOTS_DIR)/CE6SSM/ce6ssm_tb10-scan-l-k-vs_combined_m0_m12_cclam.pdf \
+		$(MANUALS_PLOTS_DIR)/CE6SSM/ce6ssm_tb10-scan-l-k-vs_combined_m0_m12_cckap.pdf
+
+MANUALS_PLOTS_CSE6SSM := \
+		$(MANUALS_PLOTS_DIR)/CSE6SSM/CSE6SSM_m12_Azero_m0_handwritten.pdf \
+		$(MANUALS_PLOTS_DIR)/CSE6SSM/CSE6SSM_m12_Azero_m0.pdf
+
+MANUALS_PLOTS_CNMSSM := \
+		$(MANUALS_PLOTS_DIR)/CNMSSM/cnmssm_4D-m0sqpos-full-m0_m12_sort-A0.pdf \
+		$(MANUALS_PLOTS_DIR)/CNMSSM/cnmssm_4D-full-squark_mass_m0sqgtr0_m0_m12.pdf \
+		$(MANUALS_PLOTS_DIR)/CNMSSM/cnmssm_4D-m0sqpos-full_mhZoom_lam_m12_sort-mh.pdf \
+		$(MANUALS_PLOTS_DIR)/CNMSSM/cnmssm_4D-m0sqgtr0-full-msu6.pdf
+
+MANUALS_PLOTS_semi_analytic_benchmark := \
+		$(MANUALS_PLOTS_DIR)/semi_analytic_benchmark/i7-4702MQ.pdf
+
+MANUALS_PLOTS_Himalaya := \
+		$(MANUALS_PLOTS_DIR)/Himalaya/scan_Mh_MS_TB-5_Xt-0_delta_alpha.pdf \
+		$(MANUALS_PLOTS_DIR)/Himalaya/scan_Mh_Xt_TB-5_MS-2000.pdf \
+		$(MANUALS_PLOTS_DIR)/Himalaya/scan_Mh_MS_TB-5_Xt-0.pdf \
+		$(MANUALS_PLOTS_DIR)/Himalaya/scan_Mh_Xt_TB-5_MS-2000_delta_alpha.pdf
+
+MANUALS_PLOTS_CMSSMCPV := \
+		$(MANUALS_PLOTS_DIR)/CMSSMCPV/scan_TYu33_Q_m0-500_M12-500_TB-10_A0-500_PhiMu-0_PhiA0-Pi4.pdf \
+		$(MANUALS_PLOTS_DIR)/CMSSMCPV/scan_Mh_PhiMu_m0-500_M12-500_TB-10_A0-0.pdf
+
+MANUALS_PLOTS_FlexibleEFTHiggs := \
+		$(MANUALS_PLOTS_DIR)/FlexibleEFTHiggs/scan_Mh_Xt_TB-5_MS-2000.pdf \
+		$(MANUALS_PLOTS_DIR)/FlexibleEFTHiggs/scan_Mh_MS_TB-5_Xt-0.pdf
+
+MANUALS_PLOTS_CMSSM_solution_regions := \
+		$(MANUALS_PLOTS_DIR)/CMSSM_solution_regions/CMSSM_higgs_mass.pdf \
+		$(MANUALS_PLOTS_DIR)/CMSSM_solution_regions/CMSSM_solution_regions.pdf
+
+MANUALS_PLOTS_semi_analytic_rel_errs := \
+		$(MANUALS_PLOTS_DIR)/semi_analytic_rel_errs/single_step_errors.pdf
+
+MANUALS_PLOTS_MSSMMuBMu := \
+		$(MANUALS_PLOTS_DIR)/MSSMMuBMu/scan_MSSMMuBMu_Mh_Xt_TB-5_MS-2000.pdf \
+		$(MANUALS_PLOTS_DIR)/MSSMMuBMu/scan_MSSMMuBMu_Mh_MS_TB-5_Xt-0.pdf
+
+MANUALS_PLOTS_MSSMCPV := \
+		$(MANUALS_PLOTS_DIR)/MSSMCPV/contour_de_PhiMuTe.pdf \
+		$(MANUALS_PLOTS_DIR)/MSSMCPV/contour_de_PhiMuM12.pdf
+
+MANUALS_FEYNMAN_DIR := $(DIR)/feynman
+
+MANUALS_FEYNMAN := \
+		$(MANUALS_FEYNMAN_DIR)/deltaVBVertex1.pdf \
+		$(MANUALS_FEYNMAN_DIR)/amuFFS.pdf \
+		$(MANUALS_FEYNMAN_DIR)/deltaVBWave.pdf \
+		$(MANUALS_FEYNMAN_DIR)/deltaVBVertex2.pdf \
+		$(MANUALS_FEYNMAN_DIR)/amuFSS.pdf \
+		$(MANUALS_FEYNMAN_DIR)/deltaVBBox1.pdf
+
+MANUALS_EXAMPLES := \
+		$(MANUALS_EXAMPLES_DIR)/scan_FlexibleEFTHiggs_uncertainty.m \
+		$(MANUALS_EXAMPLES_DIR)/scan_HSSUSY.m \
+		$(MANUALS_EXAMPLES_DIR)/scan_HSSUSY_EFT_uncertainty.m \
+		$(MANUALS_EXAMPLES_DIR)/scan_HSSUSY_SM_uncertainty.m
+
+PAPER_PDF_1     := $(PDF_OUTPUT_DIR)/flexiblesusy-1.0.pdf
+PAPER_PDF_2     := $(PDF_OUTPUT_DIR)/flexiblesusy-2.0.pdf
+PAPER_PDF_3     := $(PDF_OUTPUT_DIR)/flexiblesusy-new_features.pdf
+PAPER_PDF       := $(PAPER_PDF_1) $(PAPER_PDF_2) $(PAPER_PDF_3)
+PAPER_SRC_1     := $(DIR)/flexiblesusy-1.0.tex
+PAPER_SRC_2     := \
+		$(DIR)/flexiblesusy-2.0.tex \
+		$(DIR)/flexiblesusy-2.0.bib
+PAPER_SRC_3     := \
+		$(DIR)/flexiblesusy-new_features.tex \
+		$(DIR)/flexiblesusy-new_features.bib
+PAPER_SRC       := $(PAPER_SRC_1) $(PAPER_SRC_2) $(PAPER_SRC_3)
+PAPER_STY       := $(DIR)/JHEP.bst $(DIR)/tikz-uml.sty
+
+LATEX_TMP       := \
+		$(patsubst %.pdf, %.aux, $(PAPER_PDF)) \
+		$(patsubst %.pdf, %.bbl, $(PAPER_PDF)) \
+		$(patsubst %.pdf, %.blg, $(PAPER_PDF)) \
+		$(patsubst %.pdf, %.log, $(PAPER_PDF)) \
+		$(patsubst %.pdf, %.toc, $(PAPER_PDF)) \
+		$(patsubst %.pdf, %.out, $(PAPER_PDF)) \
+		$(patsubst %.pdf, %.spl, $(PAPER_PDF))
+
+.PHONY:         all-$(MODNAME) clean-$(MODNAME) distclean-$(MODNAME) \
+		$(MODNAME)-pdf
+
+clean-$(MODNAME):
+		-rm -f $(LATEX_TMP)
+
+distclean-$(MODNAME): clean-$(MODNAME)
+		-rm -f $(PAPER_PDF)
+
+clean::         clean-$(MODNAME)
+
+distclean::     distclean-$(MODNAME)
+
+ifneq ($(INSTALL_DIR),)
+install-src::
+		install -d $(MANUALS_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(MANUALS_MK) $(MANUALS_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(PAPER_SRC) $(MANUALS_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(PAPER_STY) $(MANUALS_INSTALL_DIR)
+		install -d $(INSTALL_DIR)/$(MANUALS_EXAMPLES_DIR)
+		install -m u=rw,g=r,o=r $(MANUALS_EXAMPLES) $(INSTALL_DIR)/$(MANUALS_EXAMPLES_DIR)
+
+		install -d $(INSTALL_DIR)/$(MANUALS_FEYNMAN_DIR)
+		install -m u=rw,g=r,o=r $(MANUALS_FEYNMAN) $(INSTALL_DIR)/$(MANUALS_FEYNMAN_DIR)
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/THDMIIMSSMBC
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_THDMIIMSSMBC) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/THDMIIMSSMBC
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/HSSUSY
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_HSSUSY) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/HSSUSY
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/FlexibleMW
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_FlexibleMW) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/FlexibleMW
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CMSSM_multiple_solutions
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_CMSSM_multiple_solutions) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CMSSM_multiple_solutions
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CE6SSM
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_CE6SSM) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CE6SSM
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CSE6SSM
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_CSE6SSM) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CSE6SSM
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CNMSSM
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_CNMSSM) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CNMSSM
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/semi_analytic_benchmark
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_semi_analytic_benchmark) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/semi_analytic_benchmark
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/Himalaya
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_Himalaya) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/Himalaya
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CMSSMCPV
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_CMSSMCPV) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CMSSMCPV
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/FlexibleEFTHiggs
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_FlexibleEFTHiggs) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/FlexibleEFTHiggs
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CMSSM_solution_regions
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_CMSSM_solution_regions) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/CMSSM_solution_regions
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/semi_analytic_rel_errs
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_semi_analytic_rel_errs) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/semi_analytic_rel_errs
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/MSSMMuBMu
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_MSSMMuBMu) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/MSSMMuBMu
+
+		install -d $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/MSSMCPV
+		install -m u=rw,g=r,o=r $(MANUALS_PLOTS_MSSMCPV) $(INSTALL_DIR)/$(MANUALS_PLOTS_DIR)/MSSMCPV
+
+endif
+
+$(MODNAME)-pdf: $(PAPER_PDF)
+
+$(PAPER_PDF_1): $(PAPER_SRC_1) $(PAPER_STY)
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-1.0.tex
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-1.0.tex
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-1.0.tex
+
+$(PAPER_PDF_2): $(PAPER_SRC_2) $(PAPER_STY)
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-2.0.tex
+		cd $(MANUALS_DIR) && bibtex flexiblesusy-2.0
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-2.0.tex
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-2.0.tex
+
+$(PAPER_PDF_3): $(PAPER_SRC_3) $(PAPER_STY)
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-new_features.tex
+		cd $(MANUALS_DIR) && bibtex flexiblesusy-new_features
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-new_features.tex
+		cd $(MANUALS_DIR) && pdflatex flexiblesusy-new_features.tex
---
+++ doc/models/module.mk
@@ -0,0 +1,41 @@
+DIR          := doc/models
+MODNAME      := doc-models
+
+DOC_MODELS_DIR := $(DIR)
+
+DOC_MODELS_MK := $(DOC_MODELS_DIR)/module.mk
+
+DOC_MODELS_INSTALL_DIR := $(INSTALL_DIR)/$(DOC_MODELS_DIR)
+
+DOC_MODELS_RST := \
+		$(DOC_MODELS_DIR)/HSSUSY.rst \
+		$(DOC_MODELS_DIR)/MSSMEFTHiggs.rst \
+		$(DOC_MODELS_DIR)/NUHMSSMNoFVHimalaya.rst
+
+DOC_MODELS_IMAGE_DIR := $(DOC_MODELS_DIR)/images
+
+DOC_MODELS_IMAGES := \
+		$(DOC_MODELS_IMAGE_DIR)/HSSUSY_Mh_MS.png \
+		$(DOC_MODELS_IMAGE_DIR)/HSSUSY_Mh_Xt.png \
+		$(DOC_MODELS_IMAGE_DIR)/HSSUSY_tower.svg \
+		$(DOC_MODELS_IMAGE_DIR)/MSSMEFTHiggs_Mh_MS.png \
+		$(DOC_MODELS_IMAGE_DIR)/MSSMEFTHiggs_tower.svg \
+		$(DOC_MODELS_IMAGE_DIR)/NUHMSSMNoFVHimalaya_tower.svg
+
+DOC_MODELS_EXAMPLE_DIR := $(DOC_MODELS_DIR)/examples
+
+DOC_MODELS_EXAMPLES := \
+		$(DOC_MODELS_EXAMPLE_DIR)/HSSUSY_uncertainty_estimate.m \
+		$(DOC_MODELS_EXAMPLE_DIR)/MSSMEFTHiggs_uncertainty_estimate.m \
+		$(DOC_MODELS_EXAMPLE_DIR)/NUHMSSMNoFVHimalaya_uncertainty_estimate.m
+
+ifneq ($(INSTALL_DIR),)
+install-src::
+		install -d $(DOC_MODELS_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(DOC_MODELS_MK) $(DOC_MODELS_INSTALL_DIR)
+		install -m u=rw,g=r,o=r $(DOC_MODELS_RST) $(DOC_MODELS_INSTALL_DIR)
+		install -d $(INSTALL_DIR)/$(DOC_MODELS_IMAGE_DIR)
+		install -m u=rw,g=r,o=r $(DOC_MODELS_IMAGES) $(INSTALL_DIR)/$(DOC_MODELS_IMAGE_DIR)
+		install -d $(INSTALL_DIR)/$(DOC_MODELS_EXAMPLE_DIR)
+		install -m u=rw,g=r,o=r $(DOC_MODELS_EXAMPLES) $(INSTALL_DIR)/$(DOC_MODELS_EXAMPLE_DIR)
+endif
