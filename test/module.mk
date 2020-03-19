6890fb8af86217eace1e7a0803066de6ad26fb96
Alexander Voigt <ahgvoigt@gmail.com>
2020-03-19 11:19:37 +0100

    adding GSLFLAGS to CPPFLAGS to compile the tests

--- test/module.mk
+++ test/module.mk
@@ -1089,7 +1089,7 @@ $(DIR)/test_%.x: $(DIR)/test_%.o
 		$(THREADLIBS) $(GSLLIBS) $(FLIBS) $(SQLITELIBS) $(TSILLIBS)
 
 # add boost and eigen flags for the test object files and dependencies
-$(TEST_OBJ) $(TEST_DEP): CPPFLAGS += -Itest/SOFTSUSY $(MODtest_INC) $(BOOSTFLAGS) $(EIGENFLAGS) $(TSILFLAGS)
+$(TEST_OBJ) $(TEST_DEP): CPPFLAGS += -Itest/SOFTSUSY $(MODtest_INC) $(BOOSTFLAGS) $(EIGENFLAGS) $(GSLFLAGS) $(TSILFLAGS)
 
 ifeq ($(ENABLE_SHARED_LIBS),yes)
 $(LIBTEST): $(LIBTEST_OBJ)
