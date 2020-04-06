Expander/FlexibleSUSY

commit 9c37a95b6260ae0816b1bd4adf7aace0955adace

fixed default HighPoleMassPrecision value in CPV models

meta/FlexibleSUSY.m
@@ -248,7 +248,7 @@ SARAH`RealParameters = { All };
 
 (* precision of pole mass calculation *)
 DefaultPoleMassPrecision = MediumPrecision;
-HighPoleMassPrecision    = {SARAH`HiggsBoson, SARAH`PseudoScalar, SARAH`ChargedHiggs};
+HighPoleMassPrecision    = DeleteCases[{TreeMasses`GetHiggsBoson[], TreeMasses`GetChargedHiggsBoson[], TreeMasses`GetPseudoscalarHiggsBoson[]}, Null];
 MediumPoleMassPrecision  = {};
 LowPoleMassPrecision     = {};
 

commit 0da6d189324de16f321500efb6d942427ae1ff5b

correcting commit 9c37a95b6260ae
The functions TreeMasses`GetHiggsBoson[],
TreeMasses`GetChargedHiggsBoson[] and
TreeMasses`GetPseudoscalarHiggsBoson[] don't give meaningful results
until SARAH`Start[] is called.

Commit 9c37a95b6260ae broke the test

   test/test_CMSSMCPV_spectrum.sh

meta/FlexibleSUSY.m
@@ -248,7 +248,7 @@ SARAH`RealParameters = { All };
 
 (* precision of pole mass calculation *)
 DefaultPoleMassPrecision = MediumPrecision;
-HighPoleMassPrecision    = DeleteCases[{TreeMasses`GetHiggsBoson[], TreeMasses`GetChargedHiggsBoson[], TreeMasses`GetPseudoscalarHiggsBoson[]}, Null];
+HighPoleMassPrecision    = {};
 MediumPoleMassPrecision  = {};
 LowPoleMassPrecision     = {};
 
@@ -506,6 +506,12 @@ CheckBVPSolvers[solvers_List] :=
               Quit[1];
              ];
           ];
+
+(* sets model file variables to default values, after SARAH`Start[] has been called *)
+FSSetDefaultModelFileSettings[] :=
+    Module[{},
+           FlexibleSUSY`HighPoleMassPrecision = DeleteCases[{TreeMasses`GetHiggsBoson[], TreeMasses`GetChargedHiggsBoson[], TreeMasses`GetPseudoscalarHiggsBoson[]}, Null];
+    ];
 
 CheckModelFileSettings[] :=
     Module[{},
@@ -3770,6 +3776,8 @@ MakeFlexibleSUSY[OptionsPattern[]] :=
               Print["   Please run ./createmodel first."];
               Quit[1]];
            CheckSARAHVersion[];
+           (* set default values that need SARAH`Start[] to be called first *)
+           FSSetDefaultModelFileSettings[];
            (* load model file *)
            LoadModelFile[OptionValue[InputFile]];
            Print["FlexibleSUSY model file loaded"];