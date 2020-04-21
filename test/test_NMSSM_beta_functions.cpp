55caadcab4e835646344c3af4373a747478bd012
Alexander Voigt <ahgvoigt@gmail.com>
2020-04-21 14:28:30 +0200

    fix some instability in the NMSSM beta functions

--- test/test_NMSSM_beta_functions.cpp
+++ test/test_NMSSM_beta_functions.cpp
@@ -123,7 +123,7 @@ void test_beta_function_equality(const SoftParsNmssm& a, const NMSSM_soft_parame
    TEST_EQUALITY(beta_a.displayMh1Squared(), beta_b.get_mHd2());
    TEST_CLOSE(beta_a.displayMh2Squared(), beta_b.get_mHu2(), 2.0e-12);
    TEST_EQUALITY(beta_a.displayMsSquared(), beta_b.get_ms2());
-   TEST_EQUALITY(beta_a.displaySoftMassSquared(mQl), beta_b.get_mq2());
+   TEST_CLOSE(beta_a.displaySoftMassSquared(mQl), beta_b.get_mq2(), 2.0e-12);
    TEST_CLOSE(beta_a.displaySoftMassSquared(mUr), beta_b.get_mu2(), 2.0e-12);
    TEST_EQUALITY(beta_a.displaySoftMassSquared(mDr), beta_b.get_md2());
    TEST_EQUALITY(beta_a.displaySoftMassSquared(mLl), beta_b.get_ml2());
