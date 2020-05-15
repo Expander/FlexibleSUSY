cade014c79a3775b36d1a38bdb05fd25b7fd7d1f
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-15 09:59:10 +0200

    build test_SM_cxxdiagrams.cpp only if meta code is enabled
    This fixes a build error in PR #210

--- test/module.mk
+++ test/module.mk
@@ -512,13 +512,17 @@ TEST_SRC += \
 		$(DIR)/test_SM_tree_level_spectrum.cpp \
 		$(DIR)/test_SM_two_loop_spectrum.cpp \
 		$(DIR)/test_SM_three_loop_spectrum.cpp \
-		$(DIR)/test_SM_mw_calculation.cpp \
+		$(DIR)/test_SM_mw_calculation.cpp
+endif
+
+ifeq ($(WITH_SM) $(ENABLE_META),yes yes)
+TEST_SRC += \
 		$(DIR)/test_SM_cxxdiagrams.cpp
 endif
 
-ifeq ($(ENABLE_FEYNARTS) $(ENABLE_FORMCALC),yes yes)
+ifeq ($(ENABLE_FEYNARTS) $(ENABLE_FORMCALC) $(ENABLE_META),yes yes yes)
 ifeq ($(WITH_SM),yes)
- TEST_SRC += \
+TEST_SRC += \
 		$(DIR)/test_SM_npointfunctions.cpp \
 		$(DIR)/test_SM_matching_selfenergy_Fd.cpp
 endif
