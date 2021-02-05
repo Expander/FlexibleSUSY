a49ed41c66751d0513a4a4498892147c0f2b8bfd
Wojciech <wojciech.kotlarski@tu-dresden.de>
2021-02-05 01:37:23 +0100

    bumped FS age

--- meta/FlexibleSUSY.m
+++ meta/FlexibleSUSY.m
@@ -73,7 +73,7 @@ FS`Authors = {"P. Athron", "M. Bach", "D. Harries", "W. Kotlarski",
               "T. Kwasnitza", "J.-h. Park", "T. Steudtner",
               "D. St\[ODoubleDot]ckinger", "A. Voigt", "J. Ziebell"};
 FS`Contributors = {};
-FS`Years   = "2013-2020";
+FS`Years   = "2013-2021";
 FS`References = Get[FileNameJoin[{$flexiblesusyConfigDir,"references"}]];
 
 Print[""];
